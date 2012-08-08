b3b20999ed3e75df19bd2d1d94b69b2730284b0f
BenjaminKlatt <klatt@fzi.de>
2012-08-08 13:41:37 +0000

    Removed RessourceSet based diffing to focus on direct model diffing Externalized model element printing

--- org.splevo.diffing/src/org/splevo/diffing/MatchEngineDiffingService.java
+++ org.splevo.diffing/src/org/splevo/diffing/MatchEngineDiffingService.java
@@ -1,44 +1,20 @@
 package org.splevo.diffing;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.diff.engine.GenericDiffEngine;
-import org.eclipse.emf.compare.diff.metamodel.ComparisonResourceSetSnapshot;
-import org.eclipse.emf.compare.diff.metamodel.DiffElement;
-import org.eclipse.emf.compare.diff.metamodel.DiffFactory;
 import org.eclipse.emf.compare.diff.metamodel.DiffModel;
-import org.eclipse.emf.compare.diff.metamodel.DiffResourceSet;
 import org.eclipse.emf.compare.match.MatchOptions;
 import org.eclipse.emf.compare.match.engine.DefaultMatchScopeProvider;
 import org.eclipse.emf.compare.match.engine.IMatchEngine;
-import org.eclipse.emf.compare.match.metamodel.Match2Elements;
-import org.eclipse.emf.compare.match.metamodel.MatchElement;
 import org.eclipse.emf.compare.match.metamodel.MatchModel;
-import org.eclipse.emf.compare.match.metamodel.MatchResourceSet;
 import org.eclipse.emf.compare.match.metamodel.UnmatchElement;
-import org.eclipse.emf.compare.match.service.MatchService;
 import org.eclipse.emf.compare.util.EMFCompareMap;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.ClassInstanceCreationImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.JavadocImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.MethodInvocationImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.PackageImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.SingleVariableAccessImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.TextElementImpl;
-import org.eclipse.gmt.modisco.java.emf.impl.TypeAccessImpl;
 import org.eclipse.modisco.java.composition.javaapplication.JavaApplication;
-import org.splevo.diffing.emfcompare.diff.transform.DiffTreeTransformer;
 import org.splevo.diffing.emfcompare.merge.KdmMatchEngine;
+import org.splevo.diffing.kdm.JavaModelElementPrinter;
 
 /**
  * Diffing Service that directly builds up the diffing model instead of using a post 
@@ -47,7 +23,7 @@ import org.splevo.diffing.emfcompare.merge.KdmMatchEngine;
  * @author Benjamin Klatt
  *
  */
-public class MatchEngineDiffingService implements DiffingService {
+public class MatchEngineDiffingService {
 
 	// TODO: extend to more than one input model
 	public DiffModel doDiff(JavaApplication leadingModel, JavaApplication integrationModel) throws IOException, InterruptedException {
@@ -60,6 +36,8 @@ public class MatchEngineDiffingService implements DiffingService {
 		final Map<String, Object> matchOptions = new EMFCompareMap<String, Object>();
 		DefaultMatchScopeProvider matchScopeProvider = new DefaultMatchScopeProvider(leadingJavaModel, integrationJavaModel);
 		matchOptions.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER,matchScopeProvider);
+
+		JavaModelElementPrinter elementPrinter = new JavaModelElementPrinter();
 		
 		System.out.println("=======================================================");
 		System.out.println("==================== MATCHING PHASE  ==================");
@@ -74,7 +52,7 @@ public class MatchEngineDiffingService implements DiffingService {
 		for (UnmatchElement unmatchedElement : unmatchedElements) {
 			 System.out.print(unmatchedElement.getSide()+"\t");
 			 System.out.print(unmatchedElement.getElement().getClass().getSimpleName()+"\t");
-			 System.out.print(buildOutput(unmatchedElement.getElement()));
+			 System.out.print(elementPrinter.printElement(unmatchedElement.getElement()));
 			 System.out.println();
 		}
 //		System.out.println("");
@@ -92,211 +70,22 @@ public class MatchEngineDiffingService implements DiffingService {
 		System.out.println("==================== DIFFING PHASE  ===================");
 		System.out.println("=======================================================");
 		
-		DiffModel diff = new GenericDiffEngine().doDiff(matchModel, false);
+		DiffModel diffModel = new GenericDiffEngine().doDiff(matchModel, false);
 		
-		DiffTreeTransformer transformer = new DiffTreeTransformer();
-		EList<DiffElement> differences = transformer.transformElements(diff.getDifferences());
-
-		for (DiffElement diffElement : differences) {
-			System.out.println(diffElement.getKind()+": "+diffElement);
-		}
 		
+//		System.out.println("=======================================================");
+//		System.out.println("==================== POST DIFFING PHASE  ==============");
+//		System.out.println("=======================================================");
+//
+//		DiffModelPostProcessor postProcessor = new DiffModelPostProcessor();
+//		DiffModel enhancedDiffModel = postProcessor.process(diffModel);
 		
 		
 		//DiffNodeTransformer diffSwitch = new DiffNodeTransformer();
 		
-		return diff;
+		return diffModel;
 		
 		
-	}
-
-	@Override
-	public ComparisonResourceSetSnapshot getDiff(List<File> leftASTModels,
-			List<File> rightASTModels) throws IOException, InterruptedException {
-
-		ResourceSet leftResourceSet = loadModelResourceSet(leftASTModels);
-		ResourceSet rightResourceSet = loadModelResourceSet(rightASTModels);
-
-		final MatchResourceSet matchResourceSet = buildMatchRessourceSet(leftResourceSet, rightResourceSet);
-
-		DiffResourceSet diffSet = DiffFactory.eINSTANCE.createDiffResourceSet();
-		
-		for (final MatchModel match : matchResourceSet.getMatchModels()) {
-			
-			System.out.println("==========================");
-			System.out.println("=== UNMATCHED ELEMENTS ===");
-			System.out.println("==========================");
-			EList<UnmatchElement> unmatchedElements = match.getUnmatchedElements();
-			for (UnmatchElement unmatchedElement : unmatchedElements) {
-				 System.out.print(unmatchedElement.getSide()+"\t");
-				 System.out.print(unmatchedElement.getElement().getClass().getSimpleName()+"\t");
-				 System.out.print(buildOutput(unmatchedElement.getElement()));
-				 System.out.println();
-			}
-			System.out.println("");
-			System.out.println("");
-			System.out.println("==========================");
-			System.out.println("=== MATCHED ELEMENTS ===");
-			System.out.println("==========================");
-			EList<MatchElement> matchedElements = match.getMatchedElements();
-			for (MatchElement matchedElement : matchedElements) {
-				printMatchedElement(matchedElement,0);
-			}
-		}
-		
-        // build and return the snapshot
-		ComparisonResourceSetSnapshot snapshot = buildResourceSetComparisionSnapshot(matchResourceSet, diffSet);
-		
-		return snapshot;
-	}
-
-	/**
-	 * Print out a MatchedElement returned from a match engine to the console. 
-	 * @param matchedElement
-	 * @param indentIndex
-	 */
-	private void printMatchedElement(MatchElement matchedElement, int indentIndex) {
-
-		StringBuilder indent = new StringBuilder();
-		for (int i = 0; i < indentIndex; i++) {
-			indent.append(" ");
-		}
-		
-		if(matchedElement instanceof Match2Elements){
-			Match2Elements element = (Match2Elements) matchedElement;
-			System.out.println(indent+"Left\t"+buildOutput(element.getLeftElement()));
-			System.out.println(indent+"Right\t"+buildOutput(element.getRightElement()));
-		} else {
-			System.out.println(indent+matchedElement.getSubMatchElements().toString());
-		}
-		System.out.println(indent+"~~~~~~~~~~~~~~~");
-		
-		for (MatchElement subMatchedElement : matchedElement.getSubMatchElements()) {
-			printMatchedElement(subMatchedElement,indentIndex++);
-		}
-		
-	}
-
-	/**
-	 * Build the AST class specific output
-	 * @param element
-	 * @return The prepared output string
-	 */
-	private String buildOutput(EObject element) {
-		if(element instanceof ClassInstanceCreationImpl){
-			return buildOutput((ClassInstanceCreationImpl) element);
-		} else if(element instanceof PackageImpl){
-			return buildOutput((PackageImpl) element);
-		} else if(element instanceof TextElementImpl){
-			return buildOutput((TextElementImpl) element);
-		} else if(element instanceof MethodInvocationImpl){
-			return buildOutput((MethodInvocationImpl) element);
-		} else if(element instanceof TypeAccessImpl){
-			return buildOutput((TypeAccessImpl) element);
-		} else if(element instanceof JavadocImpl){
-			return buildOutput((JavadocImpl) element);
-		} else if(element instanceof SingleVariableAccessImpl){
-			return buildOutput((SingleVariableAccessImpl) element);
-		} else {
-			return element.toString();
-		}
-	}
-	
-	private String buildOutput(SingleVariableAccessImpl element) {
-		return "SingleVariableAccessImpl "+element.getVariable().getName();
-	}
-	
-	private String buildOutput(JavadocImpl element) {
-		return "JavadocImpl";
-	}
-	
-	private String buildOutput(ClassInstanceCreationImpl element) {
-		return element.getType().getType().getName();
-	}
-	
-	private String buildOutput(TypeAccessImpl element) {
-		return element.getType().getName();
-	}
-	
-	private String buildOutput(PackageImpl element) {
-		return element.getName();
-	}
-	
-	private String buildOutput(TextElementImpl element) {
-		return element.getComments().toString();
-	}
-	
-	private String buildOutput(MethodInvocationImpl element) {
-		return element.getMethod().getName();
-	}
-
-	/**
-	 * Build the match ressource set.
-	 * 
-	 * @param leftResourceSet
-	 * @param rightResourceSet
-	 * @return
-	 * @throws InterruptedException
-	 */
-	private MatchResourceSet buildMatchRessourceSet(
-			ResourceSet leftResourceSet, ResourceSet rightResourceSet)
-			throws InterruptedException {
-		
-		// configure the match engine
-		final Map<String, Object> matchOptions = new EMFCompareMap<String, Object>();
-		DefaultMatchScopeProvider matchScopeProvider = new DefaultMatchScopeProvider(leftResourceSet, rightResourceSet);
-		matchOptions.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER,matchScopeProvider);
-		
-		MatchResourceSet matchResourceSet = MatchService.doResourceSetMatch(leftResourceSet, rightResourceSet,matchOptions);
-		return matchResourceSet;
-	}
-
-	/**
-	 * Create the comparsion resource snapshot combining the match and diff
-	 * references as done by the default emf compare.
-	 * 
-	 * @param matchModel
-	 *            matched model generated by EMF Compare
-	 * @param diffModel
-	 *            diff model generated by EMF Compare
-	 * @return The complete comparsion snapshot.
-	 */
-	protected ComparisonResourceSetSnapshot buildResourceSetComparisionSnapshot(
-			MatchResourceSet matchResourceSet, DiffResourceSet diffSet) {
-		final ComparisonResourceSetSnapshot snapshot = DiffFactory.eINSTANCE
-				.createComparisonResourceSetSnapshot();
-		snapshot.setDate(Calendar.getInstance().getTime());
-		snapshot.setMatchResourceSet(matchResourceSet);
-		snapshot.setDiffResourceSet(diffSet);
-		return snapshot;
-	}
-
-
-	/**
-	 * Loads an Ecore model from the supplied file
-	 * 
-	 * @param modelFiles
-	 *            List of models to load
-	 * @return model resource set instance
-	 * @throws IOException
-	 *             possible load exception
-	 */
-	protected ResourceSet loadModelResourceSet(List<File> modelFiles) throws IOException {
-		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new
-		org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl());
-
-		for (File modelFile : modelFiles) {
-			if (!modelFile.canRead()) {
-				throw new IllegalArgumentException("cannot read model file "
-						+ modelFile.getAbsolutePath());
-			}
-			URI fileUri = URI.createFileURI(modelFile.getPath());
-			Resource resource = resourceSet.createResource(fileUri);
-			resource.load(Collections.emptyMap());
-		}
-	
-		return resourceSet;
 	}
 
 }
---
+++ org.splevo.diffing/src/org/splevo/diffing/kdm/JavaModelElementPrinter.java
@@ -0,0 +1,80 @@
+package org.splevo.diffing.kdm;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmt.modisco.java.ClassInstanceCreation;
+import org.eclipse.gmt.modisco.java.Javadoc;
+import org.eclipse.gmt.modisco.java.MethodInvocation;
+import org.eclipse.gmt.modisco.java.Package;
+import org.eclipse.gmt.modisco.java.SingleVariableAccess;
+import org.eclipse.gmt.modisco.java.TextElement;
+import org.eclipse.gmt.modisco.java.TypeAccess;
+import org.eclipse.gmt.modisco.java.emf.util.JavaSwitch;
+
+/**
+ * A printer that generates a type specific information string
+ * for a supplied model element.
+ * 
+ * It uses an internal EMF switch based type decision for an enhanced performance.
+ * 
+ */
+public class JavaModelElementPrinter {
+	
+	/** The internal printer to be used */
+	ElementPrinter internalPrinter = new ElementPrinter();
+	
+	/**
+	 * Print the supplied element.
+	 * @param eObject The element to be printed
+	 */
+	public String printElement(EObject eObject){
+		return internalPrinter.doSwitch(eObject);
+	}
+	
+	/**
+	 * An emf switch based printer specific to the java application model.
+	 *
+	 */
+	class ElementPrinter extends JavaSwitch<String>{
+
+		@Override
+		public String caseSingleVariableAccess(SingleVariableAccess object) {
+			return "SingleVariableAccessImpl "+object.getVariable().getName();
+		}
+		
+		@Override
+		public String caseJavadoc(Javadoc object) {
+			return "JavadocImpl";
+		}
+		
+		@Override
+		public String caseClassInstanceCreation(ClassInstanceCreation object) {
+			return object.getType().getType().getName();
+		}
+		
+		@Override
+		public String caseTypeAccess(TypeAccess object) {
+			return object.getType().getName();
+		}
+		
+		@Override
+		public String casePackage(Package object) {
+			return object.getName();
+		}
+		
+		@Override
+		public String caseTextElement(TextElement object) {
+			return object.getComments().toString().substring(0, 20);
+		}
+		
+		@Override
+		public String caseMethodInvocation(MethodInvocation object) {
+			return object.getMethod().getName();
+		}
+		
+		@Override
+		public String defaultCase(EObject object) {
+			return object.getClass().getName();
+		}
+	}
+
+}
