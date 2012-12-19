a28bdc0da78fc75312e0885841609f66dbec8579
BenjaminKlatt <klatt@fzi.de>
2012-12-19 23:02:51 +0000

    Enabled CSV Format as option for the refinement model save job

--- org.splevo.ui/src/org/splevo/ui/jobs/SaveRefinementModelJob.java
+++ org.splevo.ui/src/org/splevo/ui/jobs/SaveRefinementModelJob.java
@@ -1,5 +1,7 @@
 package org.splevo.ui.jobs;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,8 +12,13 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.compare.util.ModelUtils;
 import org.splevo.project.SPLevoProject;
+import org.splevo.vpm.refinement.AnalyzerConfiguration;
+import org.splevo.vpm.refinement.Refinement;
 import org.splevo.vpm.refinement.RefinementModel;
+import org.splevo.vpm.variability.Variant;
+import org.splevo.vpm.variability.VariationPoint;
 
+import au.com.bytecode.opencsv.CSVWriter;
 import de.uka.ipd.sdq.workflow.AbstractBlackboardInteractingJob;
 import de.uka.ipd.sdq.workflow.exceptions.JobFailedException;
 import de.uka.ipd.sdq.workflow.exceptions.RollbackFailedException;
@@ -19,70 +26,254 @@ import de.uka.ipd.sdq.workflow.exceptions.UserCanceledException;
 
 /**
  * Job to read the refinement model from the blackboard and save it at a given path.
+ * 
+ * The job accepts a format option to decide how to persist the data.
  */
 public class SaveRefinementModelJob extends AbstractBlackboardInteractingJob<SPLevoBlackBoard> {
 
-	/** The relative path to write the model to. */
-	private String targetPath = null;
-	
-	/** The splevo project to work in. */
-	private SPLevoProject splevoProject = null;
-	
-	/**
-	 * Constructor to set the target path to write the model to. 
-	 * 
-	 * @param splevoProject The project to get the workspace from.
-	 * @param targetPath The eclipse workspace relative path to write to.
-	 */
-	public SaveRefinementModelJob(SPLevoProject splevoProject, String targetPath) {
-		this.splevoProject = splevoProject;
-		this.targetPath = targetPath;
-	}
+    /** The relative path to write the model to. */
+    private String targetPath = null;
 
-	/**
-	 * Runs the long running operation
-	 * 
-	 * @param monitor
-	 *            the progress monitor
-	 */
-	@Override
-	public void execute(IProgressMonitor monitor) throws JobFailedException,
-			UserCanceledException {
+    /** The SPLevo project to work in. */
+    private SPLevoProject splevoProject = null;
 
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		String basePath = workspace.getRoot().getRawLocation().toOSString();
+    /** The format to persist the model. */
+    private SaveRefinementModelJob.FORMAT format = FORMAT.ECORE_XMI;
 
-		RefinementModel refModel = getBlackboard().getRefinementModel();
+    /**
+     * Constructor to set the target path to write the model to.
+     * 
+     * @param splevoProject
+     *            The project to get the workspace from.
+     * @param targetPath
+     *            The eclipse workspace relative path to write to.
+     */
+    public SaveRefinementModelJob(SPLevoProject splevoProject, String targetPath) {
+        this.splevoProject = splevoProject;
+        this.targetPath = targetPath;
+    }
 
-		if(targetPath == null){
-			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
-			Calendar cal = Calendar.getInstance();
-			targetPath = "models/analyses-results/"+dateFormat.format(cal.getTime())+".refinement";
-		}
-		
-		logger.info("Save Refinement Model");
-		try {
-			String modelPath = basePath + splevoProject.getWorkspace() + targetPath;			
-			ModelUtils.save(refModel, modelPath);
-		} catch (IOException e) {
-			throw new JobFailedException("Failed to save refinement model.",e);
-		}
-		
-		// finish run
-		monitor.done();
-	}
+    /**
+     * Constructor to set the target path to write the model to.
+     * 
+     * @param splevoProject
+     *            The project to get the workspace from.
+     * @param targetPath
+     *            The eclipse workspace relative path to write to.
+     * @param format
+     *            To format to save the model in.
+     */
+    public SaveRefinementModelJob(SPLevoProject splevoProject, String targetPath, FORMAT format) {
+        this(splevoProject, targetPath);
+        this.format = format;
+    }
 
-	@Override
-	public void rollback(IProgressMonitor monitor)
-			throws RollbackFailedException {
-		// no rollback possible
-	}
+    /**
+     * Runs the long running operation.
+     * 
+     * @param monitor
+     *            the progress monitor
+     * @throws JobFailedException
+     *             identifies an unexpected termination of the job.
+     * @throws UserCanceledException
+     *             The job has been canceled by the user.
+     */
+    @Override
+    public void execute(IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
 
-	/**
-	 * Get the name of the job. 
-	 */
-	@Override
-	public String getName() {
-		return "Save Refinement Model Job";
-	}
+        RefinementModel refModel = getBlackboard().getRefinementModel();
+
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        String basePath = workspace.getRoot().getRawLocation().toOSString();
+        String resultDirectory = basePath + splevoProject.getWorkspace() + "models/analyses-results/";
+        
+        if (targetPath == null) {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
+            Calendar cal = Calendar.getInstance();
+            targetPath =  dateFormat.format(cal.getTime());
+        }
+        targetPath =  resultDirectory + targetPath;
+
+        switch (this.format) {
+        case CSV:
+            new File(resultDirectory).mkdirs();
+            exportConfigCSV(refModel, targetPath);
+            exportResultCSV(refModel, targetPath);
+            break;
+
+        case ECORE_XMI:
+            exportEcoreXMI(refModel, targetPath);
+            break;
+
+        default:
+            break;
+        }
+
+        // finish run
+        monitor.done();
+    }
+
+    /**
+     * Export csv files for the identified refinements.
+     * 
+     * @param refModel
+     *            The model to export.
+     * @param targetPath
+     *            The path to write to.
+     * @throws JobFailedException
+     *             Failed to write the csv export.
+     */
+    private void exportResultCSV(RefinementModel refModel, String targetPath) throws JobFailedException {
+        String resultFilePath = targetPath + "-results.csv";
+
+        // write the configuration file
+        CSVWriter writer = null;
+        try {
+            writer = new CSVWriter(new FileWriter(resultFilePath));
+            String[] header = new String[] { "Analyzer", "Type", "VP ASTNode", "Leading AST Node",
+                    "Integration AST Node" };
+            writer.writeNext(header);
+            for (AnalyzerConfiguration analyzer : refModel.getAnalyzerConfigurations()) {
+                String analyzerID = analyzer.getAnalyzerID();
+                for (Refinement refinement : analyzer.getRefinements()) {
+                    String type = refinement.getType().toString();
+
+                    for (VariationPoint vp : refinement.getVariationPoints()) {
+
+                        StringBuilder leadingVariant = new StringBuilder("");
+                        StringBuilder integrationVariant = new StringBuilder("");
+
+                        for (Variant v : vp.getVariants()) {
+                            if (v.getLeading()) {
+                                leadingVariant.append(v.getSoftwareEntities().toString());
+                            } else {
+                                integrationVariant.append(v.getSoftwareEntities().toString());
+                            }
+                        }
+
+                        String[] result = new String[] { analyzerID, type, vp.getSoftwareEntity().toString(),
+                                leadingVariant.toString(), integrationVariant.toString() };
+                        writer.writeNext(result);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            throw new JobFailedException("Failed to write csv export", e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    throw new JobFailedException("Failed to save csv file", e);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Export csv files for the analysis configuration.
+     * 
+     * @param refModel
+     *            The model to export.
+     * @param targetPath
+     *            The path to write to.
+     * @throws JobFailedException
+     *             Failed to write the csv export.
+     */
+    private void exportConfigCSV(RefinementModel refModel, String targetPath) throws JobFailedException {
+        String configFilePath = targetPath + "-config.csv";
+
+        // write the configuration file
+        CSVWriter writer = null;
+        try {
+            writer = new CSVWriter(new FileWriter(configFilePath));
+            String[] header = new String[] { "Analyzer", "Key", "Value" };
+            writer.writeNext(header);
+            for (AnalyzerConfiguration analyzer : refModel.getAnalyzerConfigurations()) {
+                String analyzerID = analyzer.getAnalyzerID();
+                String[] analyzerResults = new String[] { analyzerID, "refinements.count",
+                        "" + analyzer.getRefinements().size() };
+                writer.writeNext(analyzerResults);
+                for (String key : analyzer.getSettings().keySet()) {
+                    String[] setting = new String[] { analyzerID, key, analyzer.getSettings().get(key) };
+                    writer.writeNext(setting);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new JobFailedException("Failed to write csv export", e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    throw new JobFailedException("Failed to save csv file", e);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Export the model as xmi ecore file.
+     * 
+     * @param refModel
+     *            The model to export.
+     * @param targetPath
+     *            The target path to write to with an appended ".refinement".
+     * @throws JobFailedException
+     *             Identifies the job could not write the file.
+     */
+    private void exportEcoreXMI(RefinementModel refModel, String targetPath) throws JobFailedException {
+        logger.info("Save Refinement Model");
+        try {
+            String modelPath = targetPath + ".refinement";
+            ModelUtils.save(refModel, modelPath);
+        } catch (IOException e) {
+            throw new JobFailedException("Failed to save refinement model as xmi.", e);
+        }
+    }
+
+    @Override
+    public void rollback(IProgressMonitor monitor) throws RollbackFailedException {
+        // no roll back required
+    }
+
+    /**
+     * Get the name of the job.
+     * 
+     * @return The name of the job.
+     */
+    @Override
+    public String getName() {
+        return "Save Refinement Model Job";
+    }
+
+    /**
+     * Enumeration specifying the formats supported by this job.
+     * 
+     * @author Benjamin Klatt
+     * 
+     */
+    public enum FORMAT {
+
+        /**
+         * The job will store the model as an ecore model in the common XMI format (file extension
+         * .refinement).
+         */
+        ECORE_XMI,
+
+        /**
+         * The job will store the data included in the model as two separate csv files, one
+         * containing the configuration, the other containing the refinement data.
+         */
+        CSV,
+
+        /**
+         * The job will write nothing to the file system.
+         */
+        NONE
+    }
 }
--- org.splevo.ui/src/org/splevo/ui/workflow/VPMAnalysisWorkflowDelegate.java
+++ org.splevo.ui/src/org/splevo/ui/workflow/VPMAnalysisWorkflowDelegate.java
@@ -9,6 +9,8 @@ import org.splevo.ui.jobs.LoadVPMJob;
 import org.splevo.ui.jobs.OpenVPMRefinementBrowserJob;
 import org.splevo.ui.jobs.SPLevoBlackBoard;
 import org.splevo.ui.jobs.SaveRefinementModelJob;
+import org.splevo.ui.jobs.UpdateUIJob;
+import org.splevo.ui.jobs.SaveRefinementModelJob.FORMAT;
 import org.splevo.ui.jobs.VPMAnalysisJob;
 import org.splevo.vpm.refinement.VPMRefinementAnalyzer;
 
@@ -20,82 +22,87 @@ import de.uka.ipd.sdq.workflow.ui.UIBasedWorkflow;
 import de.uka.ipd.sdq.workflow.workbench.AbstractWorkbenchDelegate;
 
 /**
- * A workflow delegate defining the workflow for 
- * the refinement of the variation point model.
+ * A workflow delegate defining the workflow for the refinement of the variation point model.
  */
-public class VPMAnalysisWorkflowDelegate
-		extends
-		AbstractWorkbenchDelegate<VPMAnalysisWorkflowConfiguration, UIBasedWorkflow<Blackboard<?>>> {
-	
-	/** The logger for this class. */
+public class VPMAnalysisWorkflowDelegate extends
+        AbstractWorkbenchDelegate<VPMAnalysisWorkflowConfiguration, UIBasedWorkflow<Blackboard<?>>> {
+
+    /** The logger for this class. */
     private Logger logger = Logger.getLogger(VPMAnalysisWorkflowDelegate.class);
 
-	/** The configuration of the workflow. */
-	private VPMAnalysisWorkflowConfiguration config = null;
+    /** The configuration of the workflow. */
+    private VPMAnalysisWorkflowConfiguration config = null;
 
-	/**
-	 * Constructor requiring a diffing workflow configuration.
-	 * 
-	 * @param config
-	 *            The configuration of the workflow.
-	 */
-	public VPMAnalysisWorkflowDelegate(VPMAnalysisWorkflowConfiguration config) {
-		this.config = config;
-	}
+    /**
+     * Constructor requiring a diffing workflow configuration.
+     * 
+     * @param config
+     *            The configuration of the workflow.
+     */
+    public VPMAnalysisWorkflowDelegate(VPMAnalysisWorkflowConfiguration config) {
+        this.config = config;
+    }
 
-	/**
-	 * Create the workflow
-	 */
-	@Override
-	protected IJob createWorkflowJob(VPMAnalysisWorkflowConfiguration config) {
-		
-		// initialize the basic elements
-		SPLevoProject splevoProject = config.getSplevoProjectEditor().getSplevoProject();
-		OrderPreservingBlackboardCompositeJob<SPLevoBlackBoard> compositeJob =
-				new OrderPreservingBlackboardCompositeJob<SPLevoBlackBoard>();
-		compositeJob.setBlackboard(new SPLevoBlackBoard());
-		
-		// load the latest vpm model
-		LoadVPMJob loadVPMJob = new LoadVPMJob(splevoProject);
-		compositeJob.add(loadVPMJob);
+    /**
+     * Create the workflow.
+     * 
+     * @param config
+     *            The configuration object for this workflow.
+     * @return The prepared job.
+     */
+    @Override
+    protected IJob createWorkflowJob(VPMAnalysisWorkflowConfiguration config) {
 
-		// trigger the configured refinement analysis
-		ParallelBlackboardInteractingCompositeJob<SPLevoBlackBoard> parallelAnalysisJob = new ParallelBlackboardInteractingCompositeJob<SPLevoBlackBoard>();
-		if(config.getAnalyzers().size() < 1){
-			logger.error("No analysis to perform configured.");
-			return null;
-		}
-		for(VPMRefinementAnalyzer analyzerInstance : config.getAnalyzers()){
-			VPMAnalysisJob vpmAnalysisJob = new VPMAnalysisJob(analyzerInstance);
-			parallelAnalysisJob.add(vpmAnalysisJob);
-		}
-		compositeJob.add(parallelAnalysisJob);
-		
-		BuildRefinementModelJob createRefinementModelJob = new BuildRefinementModelJob();
-		compositeJob.add(createRefinementModelJob);
-		
-		IJob openViewerJob = new OpenVPMRefinementBrowserJob(config.getSplevoProjectEditor());
-		compositeJob.add(openViewerJob);
-		
-		SaveRefinementModelJob saveRefinementModelJob = new SaveRefinementModelJob(splevoProject,null);
-		compositeJob.add(saveRefinementModelJob);
+        // initialize the basic elements
+        SPLevoProject splevoProject = config.getSplevoProjectEditor().getSplevoProject();
+        OrderPreservingBlackboardCompositeJob<SPLevoBlackBoard> compositeJob = new OrderPreservingBlackboardCompositeJob<SPLevoBlackBoard>();
+        compositeJob.setBlackboard(new SPLevoBlackBoard());
 
-		// return the prepared workflow
-		return compositeJob;
-	}
+        // load the latest vpm model
+        LoadVPMJob loadVPMJob = new LoadVPMJob(splevoProject);
+        compositeJob.add(loadVPMJob);
 
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// nothing to do here
-	}
+        // trigger the configured refinement analysis
+        ParallelBlackboardInteractingCompositeJob<SPLevoBlackBoard> parallelAnalysisJob = new ParallelBlackboardInteractingCompositeJob<SPLevoBlackBoard>();
+        if (config.getAnalyzers().size() < 1) {
+            logger.error("No analysis to perform configured.");
+            return null;
+        }
+        for (VPMRefinementAnalyzer analyzerInstance : config.getAnalyzers()) {
+            VPMAnalysisJob vpmAnalysisJob = new VPMAnalysisJob(analyzerInstance);
+            parallelAnalysisJob.add(vpmAnalysisJob);
+        }
+        compositeJob.add(parallelAnalysisJob);
 
-	@Override
-	protected boolean useSeparateConsoleForEachJobRun() {
-		return false;
-	}
+        BuildRefinementModelJob createRefinementModelJob = new BuildRefinementModelJob();
+        compositeJob.add(createRefinementModelJob);
 
-	@Override
-	protected VPMAnalysisWorkflowConfiguration getConfiguration() {
-		return this.config;
-	}
+        IJob openViewerJob = new OpenVPMRefinementBrowserJob(config.getSplevoProjectEditor());
+        compositeJob.add(openViewerJob);
+
+        SaveRefinementModelJob saveRefinementModelJob = new SaveRefinementModelJob(splevoProject, null, FORMAT.CSV);
+        compositeJob.add(saveRefinementModelJob);
+
+        // job to update the user interface and see the recently created
+        IJob updateUiJob = new UpdateUIJob(config.getSplevoProjectEditor(), null);
+        compositeJob.add(updateUiJob);
+
+        // return the prepared workflow
+        return compositeJob;
+    }
+
+    @Override
+    public void selectionChanged(IAction action, ISelection selection) {
+        // nothing to do here
+    }
+
+    @Override
+    protected boolean useSeparateConsoleForEachJobRun() {
+        return false;
+    }
+
+    @Override
+    protected VPMAnalysisWorkflowConfiguration getConfiguration() {
+        return this.config;
+    }
 }
