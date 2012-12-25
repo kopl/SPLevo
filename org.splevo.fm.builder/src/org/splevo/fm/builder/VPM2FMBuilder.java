a4dda14684d4a1d555f92fcd9d8aaafbc96e4085
BenjaminKlatt <klatt@fzi.de>
2012-12-25 15:15:03 +0000

    minor code clean up

--- org.splevo.fm.builder/src/org/splevo/fm/builder/VPM2FMBuilder.java
+++ org.splevo.fm.builder/src/org/splevo/fm/builder/VPM2FMBuilder.java
@@ -14,113 +14,116 @@ import org.splevo.vpm.variability.VariationPointGroup;
 import org.splevo.vpm.variability.VariationPointModel;
 
 /**
- * A FeatureModel (FM) builder that derives the FM from an 
- * existing variation point model (VPM).
- *
+ * A FeatureModel (FM) builder that derives the FM from an existing variation point model (VPM).
+ * 
  */
 public class VPM2FMBuilder {
 
-	/**
-	 * Build a feature model based on variation point model.
-	 * 
-	 * The strategy used is very simple and builds one feature per
-	 * VariationPoint and one ChildFeature per Variant.
-	 * The connection is always alternative (OR).
-	 * 
-	 * @param vpm The variation point model to make use of.
-	 * @return The prepared feature model.
-	 */
-	public FeatureModel buildFeatureModel(VariationPointModel vpm){
-		
-		FeatureModel fm = initFeatureModel(vpm);
-		
-		for (VariationPointGroup variationPointGroup : vpm.getVariationPointGroups()) {
+    /**
+     * Build a feature model based on variation point model.
+     * 
+     * The strategy used is very simple and builds one feature per VariationPoint and one
+     * ChildFeature per Variant. The connection is always alternative (OR).
+     * 
+     * @param vpm
+     *            The variation point model to make use of.
+     * @return The prepared feature model.
+     */
+    public FeatureModel buildFeatureModel(VariationPointModel vpm) {
 
-			Feature vpFeature = createVariationPointGroupFeature(variationPointGroup,fm.getRoot());
+        FeatureModel fm = initFeatureModel(vpm);
 
-			Group group = createMandatoryFeatureGroup();
-			vpFeature.getChildren().add(group);
-			
-			Set<String> variantIds = new HashSet<String>();
-			for(VariationPoint variationPoint: variationPointGroup.getVariationPoints()){
-				for(Variant variant : variationPoint.getVariants()){
-					variantIds.add(variant.getVariantId());
-				}
-			}
-			
-			for(String variantId: variantIds){
-				Feature vFeature = createVariantFeature(variantId);
-				group.getFeatures().add(vFeature);
-			}
-			
-		}
-		
-		return fm;
-	}
+        for (VariationPointGroup variationPointGroup : vpm.getVariationPointGroups()) {
 
-	/**
-	 * Create a feature for a supplied variant.
-	 * 
-	 * @param variantId The variant to create the feature for.
-	 * @return
-	 */
-	private Feature createVariantFeature(String variantId) {
+            Feature vpFeature = createVariationPointGroupFeature(variationPointGroup, fm.getRoot());
 
-		Feature feature = FeatureModelFactory.eINSTANCE.createFeature();
-		feature.setName(variantId);
-		feature.setId(EcoreUtil.generateUUID());
-		
-		return feature;
-	}
+            Group group = createMandatoryFeatureGroup();
+            vpFeature.getChildren().add(group);
 
+            Set<String> variantIds = new HashSet<String>();
+            for (VariationPoint variationPoint : variationPointGroup.getVariationPoints()) {
+                for (Variant variant : variationPoint.getVariants()) {
+                    variantIds.add(variant.getVariantId());
+                }
+            }
 
-	/**
-	 * Create a feature for a variation point and attach it with
-	 * a mandatory group to the supplied root.
-	 * 
-	 * @param variationPointGroup The variation point to create the feature for.
-	 * @param parentFeature The feature to attach the group to.
-	 * @return The new feature element.
-	 */
-	private Feature createVariationPointGroupFeature(VariationPointGroup variationPointGroup, Feature parentFeature) {			
-		Group group = createMandatoryFeatureGroup();
-		parentFeature.getChildren().add(group);
-	
-		Feature feature = FeatureModelFactory.eINSTANCE.createFeature();
-		feature.setName(variationPointGroup.getGroupId());
-		feature.setId(EcoreUtil.generateUUID());
-		group.getFeatures().add(feature);
-		
-		return feature;
-	}
+            for (String variantId : variantIds) {
+                Feature vFeature = createVariantFeature(variantId);
+                group.getFeatures().add(vFeature);
+            }
 
-	/**
-	 * Create a mandatory / OR feature group.
-	 * @return The instantiated and configured feature group.
-	 */
-	private Group createMandatoryFeatureGroup() {
-		Group group = FeatureModelFactory.eINSTANCE.createGroup();
-		group.setId(EcoreUtil.generateUUID());
-		group.setLower(1);
-		group.setUpper(1);
-		return group;
-	}
+        }
 
+        return fm;
+    }
 
-	/**
-	 * Initialize the feature model itself.
-	 * @param vpm The vpm to init the fm for.
-	 * @return The initialized fm.
-	 */
-	private FeatureModel initFeatureModel(VariationPointModel vpm) {
-		FeatureModel fm = FeatureModelFactory.eINSTANCE.createFeatureModel();
-		fm.setId(EcoreUtil.generateUUID());
-		fm.setVersion("1.0");
-		Feature rootFeature = FeatureModelFactory.eINSTANCE.createFeature();
-		rootFeature.setName(vpm.getLeadingModel().getJavaModel().getName());
-		rootFeature.setId(EcoreUtil.generateUUID());
-		fm.setRoot(rootFeature);
-		return fm;
-	}
+    /**
+     * Create a feature for a supplied variant.
+     * 
+     * @param variantId
+     *            The variant to create the feature for.
+     * @return The prepared feature
+     */
+    private Feature createVariantFeature(String variantId) {
+
+        Feature feature = FeatureModelFactory.eINSTANCE.createFeature();
+        feature.setName(variantId);
+        feature.setId(EcoreUtil.generateUUID());
+
+        return feature;
+    }
+
+    /**
+     * Create a feature for a variation point and attach it with a mandatory group to the supplied
+     * root.
+     * 
+     * @param variationPointGroup
+     *            The variation point to create the feature for.
+     * @param parentFeature
+     *            The feature to attach the group to.
+     * @return The new feature element.
+     */
+    private Feature createVariationPointGroupFeature(VariationPointGroup variationPointGroup, Feature parentFeature) {
+        Group group = createMandatoryFeatureGroup();
+        parentFeature.getChildren().add(group);
+
+        Feature feature = FeatureModelFactory.eINSTANCE.createFeature();
+        feature.setName(variationPointGroup.getGroupId());
+        feature.setId(EcoreUtil.generateUUID());
+        group.getFeatures().add(feature);
+
+        return feature;
+    }
+
+    /**
+     * Create a mandatory / OR feature group.
+     * 
+     * @return The instantiated and configured feature group.
+     */
+    private Group createMandatoryFeatureGroup() {
+        Group group = FeatureModelFactory.eINSTANCE.createGroup();
+        group.setId(EcoreUtil.generateUUID());
+        group.setLower(1);
+        group.setUpper(1);
+        return group;
+    }
+
+    /**
+     * Initialize the feature model itself.
+     * 
+     * @param vpm
+     *            The vpm to init the fm for.
+     * @return The initialized fm.
+     */
+    private FeatureModel initFeatureModel(VariationPointModel vpm) {
+        FeatureModel fm = FeatureModelFactory.eINSTANCE.createFeatureModel();
+        fm.setId(EcoreUtil.generateUUID());
+        fm.setVersion("1.0");
+        Feature rootFeature = FeatureModelFactory.eINSTANCE.createFeature();
+        rootFeature.setName(vpm.getLeadingModel().getJavaModel().getName());
+        rootFeature.setId(EcoreUtil.generateUUID());
+        fm.setRoot(rootFeature);
+        return fm;
+    }
 
 }
