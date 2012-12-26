b527f3f3f140f8351992985dc1b0ca7a46afd608
BenjaminKlatt <klatt@fzi.de>
2012-12-26 13:03:45 +0000

    code clean up and adaptation to common test infrastructure

--- org.splevo.vpm.refinement.tests/src/org/splevo/vpm/refinement/VPMRefinementServiceTest.java
+++ org.splevo.vpm.refinement.tests/src/org/splevo/vpm/refinement/VPMRefinementServiceTest.java
@@ -3,22 +3,16 @@ package org.splevo.vpm.refinement;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.eclipse.emf.compare.util.ModelUtils;
 import org.eclipse.gmt.modisco.java.ASTNode;
-import org.eclipse.modisco.java.composition.javaapplication.JavaApplication;
 import org.eclipse.modisco.java.composition.javaapplication.JavaNodeSourceRegion;
 import org.junit.Test;
-import org.splevo.diffing.Java2KDMDiffingService;
-import org.splevo.modisco.util.KDMUtil;
 import org.splevo.modisco.util.SourceConnector;
-import org.splevo.vpm.builder.java2kdmdiff.Java2KDMVPMBuilder;
+import org.splevo.tests.SPLevoTestUtil;
 import org.splevo.vpm.refinement.simplelocation.VPLocationAnalyzer;
 import org.splevo.vpm.variability.VariationPointGroup;
 import org.splevo.vpm.variability.VariationPointModel;
@@ -31,14 +25,6 @@ public class VPMRefinementServiceTest extends AbstractTest {
     /** The logger for this class. */
     private Logger logger = Logger.getLogger(VPMRefinementServiceTest.class);
 
-    /** Source path to the native calculator implementation. */
-    private static final File NATIVE_JAVA2KDMMODEL_FILE = new File(
-            "../org.splevo.diffing.tests/testmodels/implementation/gcd/native/_java2kdm.xmi");
-
-    /** Source path to the jscience based calculator implementation. */
-    private static final File JSCIENCE_JAVA2KDMMODEL_FILE = new File(
-            "../org.splevo.diffing.tests/testmodels/implementation/gcd/jscience/_java2kdm.xmi");
-
     /**
      * Test of a basic refinement analysis and application. This test makes use of the basic
      * location based variation point analysis.
@@ -49,7 +35,7 @@ public class VPMRefinementServiceTest extends AbstractTest {
     @Test
     public void testRefinements() throws IOException, InterruptedException {
 
-        VariationPointModel initialVpm = loadVPMModel();
+        VariationPointModel initialVpm = SPLevoTestUtil.loadGCDVPMModel();
         logger.debug("Number of initial variation point groups: " + initialVpm.getVariationPointGroups().size());
 
         VPMRefinementService refinementService = new VPMRefinementService();
@@ -72,36 +58,6 @@ public class VPMRefinementServiceTest extends AbstractTest {
                 assertNotNull(sourceRegion);
             }
         }
-    }
-
-    /**
-     * Load the diffing model.
-     * 
-     * @return The loaded model.
-     * @throws IOException identifies that the vpm could not be loaded.
-     * @throws InterruptedException identifies that the refinement process was interrupted.
-     */
-    private VariationPointModel loadVPMModel() throws IOException, InterruptedException {
-
-        JavaApplication leadingModel = KDMUtil.loadKDMModel(NATIVE_JAVA2KDMMODEL_FILE);
-        JavaApplication integrationModel = KDMUtil.loadKDMModel(JSCIENCE_JAVA2KDMMODEL_FILE);
-        List<String> ignorePackages = new ArrayList<String>();
-        ignorePackages.add("java.lang");
-        ignorePackages.add("java.math");
-        ignorePackages.add("java.io");
-        ignorePackages.add("org.jscience.*");
-        ignorePackages.add("org.jscience.*");
-        ignorePackages.add("javolution.*");
-
-        Java2KDMDiffingService diffingService = new Java2KDMDiffingService();
-        diffingService.getIgnorePackages().addAll(ignorePackages);
-
-        DiffModel diffModel = diffingService.doDiff(integrationModel, leadingModel);
-
-        Java2KDMVPMBuilder java2KDMVPMBuilder = new Java2KDMVPMBuilder();
-        VariationPointModel initialVpm = java2KDMVPMBuilder.buildVPM(diffModel);
-
-        return initialVpm;
     }
 
 }
