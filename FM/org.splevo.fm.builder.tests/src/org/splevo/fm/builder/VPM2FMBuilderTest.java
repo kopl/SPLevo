38ba1042ad4d87dea6c07779e9e7e285fa80f4ed
BenjaminKlatt <klatt@fzi.de>
2012-12-28 00:12:27 +0000

    SPLEVO-3 Create Test Launch Scripts Launch script created and builder test adapted to use GCD example loaded from global test infrastructure

--- FM/org.splevo.fm.builder.tests/src/org/splevo/fm/builder/VPM2FMBuilderTest.java
+++ FM/org.splevo.fm.builder.tests/src/org/splevo/fm/builder/VPM2FMBuilderTest.java
@@ -1,31 +1,20 @@
 package org.splevo.fm.builder;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.eclipse.emf.compare.diff.metamodel.DiffModel;
-import org.eclipse.emf.compare.util.ModelUtils;
 import org.eclipse.featuremodel.FeatureModel;
-import org.eclipse.modisco.java.composition.javaapplication.JavaApplication;
 import org.junit.Test;
-import org.splevo.diffing.Java2KDMDiffingService;
-import org.splevo.modisco.util.KDMUtil;
-import org.splevo.vpm.builder.java2kdmdiff.Java2KDMVPMBuilder;
+import org.splevo.tests.SPLevoTestUtil;
 import org.splevo.vpm.variability.VariationPointModel;
 
 public class VPM2FMBuilderTest extends AbstractTest {
 
 	/** The logger for this test class. */
     private Logger logger = Logger.getLogger(VPM2FMBuilderTest.class);
-
-	/** Source path to the native calculator implementation */
-	private static final File NATIVE_JAVA2KDMMODEL_FILE = new File("../org.splevo.diffing.tests/testmodels/implementation/gcd/native/_java2kdm.xmi");
-	
-	/** Source path to the jscience based calculator implementation */
-	private static final File JSCIENCE_JAVA2KDMMODEL_FILE = new File("../org.splevo.diffing.tests/testmodels/implementation/gcd/jscience/_java2kdm.xmi");
 
 	/**
 	 * Basic test to create a vpm and derive a feature model.
@@ -36,46 +25,15 @@ public class VPM2FMBuilderTest extends AbstractTest {
 	@Test
 	public void testBuildFeatureModel() throws IOException, InterruptedException {
 		
-		VariationPointModel vpm = null;
-		vpm = generateVPM();
-		
-		logger.debug("VPM prepared");
+		VariationPointModel vpm = SPLevoTestUtil.loadGCDVPMModel();
 		
 		VPM2FMBuilder builder = new VPM2FMBuilder();
 		FeatureModel fm = builder.buildFeatureModel(vpm);
 
-		logger.debug("FM created");
+		assertNotNull("No Feature Model Created", fm);
+        assertNotNull("No root feature created", fm.getRoot());
+        assertEquals("Wrong number of top level features", 6, fm.getRoot().getChildren().size());
 		
-		ModelUtils.save(fm, "testresult/gcdFeatureModel.featuremodel");
+		//ModelUtils.save(fm, "testresult/gcdFeatureModel.featuremodel");
 	}
-	
-	/**
-	 * Generate the vpm model to work with.
-	 * 
-	 * @return The prepared vpm.
-	 * @throws IOException
-	 * @throws InterruptedException
-	 */
-	public VariationPointModel generateVPM() throws IOException, InterruptedException {
-
-		JavaApplication leadingModel = KDMUtil.loadKDMModel(NATIVE_JAVA2KDMMODEL_FILE);
-		JavaApplication integrationModel = KDMUtil.loadKDMModel(JSCIENCE_JAVA2KDMMODEL_FILE);
-		List<String> ignorePackages = new ArrayList<String>();
-		ignorePackages.add("java.lang");
-		ignorePackages.add("java.math");
-		ignorePackages.add("java.io");
-		ignorePackages.add("org.jscience.*");
-		ignorePackages.add("org.jscience.*");
-		ignorePackages.add("javolution.*");
-		Java2KDMDiffingService diffingService = new Java2KDMDiffingService();
-		diffingService.getIgnorePackages().addAll(ignorePackages);
-		DiffModel diffModel;
-		diffModel = diffingService.doDiff(leadingModel,integrationModel);
-			
-		Java2KDMVPMBuilder java2KDMVPMBuilder = new Java2KDMVPMBuilder();
-		VariationPointModel vpm = java2KDMVPMBuilder.buildVPM(diffModel);
-		
-		return vpm;
-	}
-
 }
