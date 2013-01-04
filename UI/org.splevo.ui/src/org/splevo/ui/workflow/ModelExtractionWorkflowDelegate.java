163341dc9d50354a861e84774d0e0391ad1c8ff7
BenjaminKlatt <klatt@fzi.de>
2013-01-04 14:02:17 +0000

    Code cleanup for parallel extraction execution

--- UI/org.splevo.ui/src/org/splevo/ui/workflow/ModelExtractionWorkflowDelegate.java
+++ UI/org.splevo.ui/src/org/splevo/ui/workflow/ModelExtractionWorkflowDelegate.java
@@ -2,13 +2,15 @@ package org.splevo.ui.workflow;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.splevo.project.SPLevoProject;
 import org.splevo.ui.jobs.ExtractionJob;
+import org.splevo.ui.jobs.SPLevoBlackBoard;
 import org.splevo.ui.jobs.UpdateUIJob;
 
 import de.uka.ipd.sdq.workflow.Blackboard;
 import de.uka.ipd.sdq.workflow.IJob;
 import de.uka.ipd.sdq.workflow.OrderPreservingCompositeJob;
-import de.uka.ipd.sdq.workflow.ParallelCompositeJob;
+import de.uka.ipd.sdq.workflow.ParallelBlackboardInteractingCompositeJob;
 import de.uka.ipd.sdq.workflow.ui.UIBasedWorkflow;
 import de.uka.ipd.sdq.workflow.workbench.AbstractWorkbenchDelegate;
 
@@ -41,16 +43,17 @@ public class ModelExtractionWorkflowDelegate extends
 
         OrderPreservingCompositeJob compositeJob = new OrderPreservingCompositeJob();
 
-        // init the parallel extraction
-        ExtractionJob leadingExtractionJob = new ExtractionJob(config.getSplevoProjectEditor().getSplevoProject(), true);
-        ExtractionJob integrationExtractionJob = new ExtractionJob(config.getSplevoProjectEditor().getSplevoProject(),
-                false);
-        ParallelCompositeJob parallelExtractionJob = new ParallelCompositeJob();
-        parallelExtractionJob.add(leadingExtractionJob);
-        parallelExtractionJob.add(integrationExtractionJob);
-        compositeJob.add(parallelExtractionJob);
+        // create the parallel extraction
+        SPLevoProject splevoProject = config.getSplevoProjectEditor().getSplevoProject();
+        ExtractionJob leadingExtractionJob = new ExtractionJob(splevoProject, true);
+        ExtractionJob integrationExtractionJob = new ExtractionJob(splevoProject, false);
 
-        // init the ui update job
+        ParallelBlackboardInteractingCompositeJob<SPLevoBlackBoard> compositeExtractionJob = new ParallelBlackboardInteractingCompositeJob<SPLevoBlackBoard>();
+        compositeExtractionJob.add(leadingExtractionJob);
+        compositeExtractionJob.add(integrationExtractionJob);
+        compositeJob.add(compositeExtractionJob);
+        
+        // create the ui update job
         IJob updateUiJob = new UpdateUIJob(config.getSplevoProjectEditor(), "Source Models extracted");
         compositeJob.add(updateUiJob);
 
