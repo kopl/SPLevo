c4a63507563591dccea43a3ef117013d7234f1f4
BenjaminKlatt <klatt@fzi.de>
2013-03-31 06:48:10 +0000

    SPLEVO-55 RefinementService: Merge Processing

--- VPMRefinement/org.splevo.vpm.refinement.tests/src/org/splevo/vpm/refinement/VPMRefinementServiceTest.java
+++ VPMRefinement/org.splevo.vpm.refinement.tests/src/org/splevo/vpm/refinement/VPMRefinementServiceTest.java
@@ -2,6 +2,7 @@ package org.splevo.vpm.refinement;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import org.eclipse.gmt.modisco.java.ASTNode;
 import org.eclipse.gmt.modisco.java.Block;
 import org.eclipse.modisco.java.composition.javaapplication.JavaApplication;
 import org.eclipse.modisco.java.composition.javaapplication.JavaNodeSourceRegion;
+import org.junit.Before;
 import org.junit.Test;
 import org.splevo.modisco.util.SourceConnector;
 import org.splevo.tests.SPLevoTestUtil;
@@ -23,6 +25,20 @@ import org.splevo.vpm.variability.VariationPointModel;
  */
 public class VPMRefinementServiceTest extends AbstractTest {
 
+    /** The initial vpm model to refine. */
+    private VariationPointModel initialVpm = null;
+
+    @Override
+    @Before
+    public void setUp() {
+        try {
+            initialVpm = SPLevoTestUtil.loadGCDVPMModel();
+        } catch (Exception e) {
+            fail("Failed to load initial variation point model");
+        }
+        assertEquals("wrong number of initial variation point groups", 6, initialVpm.getVariationPointGroups().size());
+    }
+
     /**
      * Test of a basic refinement analysis and application. This test makes use of the basic
      * location based variation point analysis.
@@ -33,11 +49,7 @@ public class VPMRefinementServiceTest extends AbstractTest {
      *             identifies that the refinement process was interrupted.
      */
     @Test
-    public void testRefinements() throws IOException, InterruptedException {
-
-        VariationPointModel initialVpm = SPLevoTestUtil.loadGCDVPMModel();
-        
-        assertEquals("wrong number of initial variation point groups", 6, initialVpm.getVariationPointGroups().size());
+    public void testGroupRefinements() throws IOException, InterruptedException {
 
         List<Refinement> refinements = new ArrayList<Refinement>();
         Refinement refinement = RefinementFactory.eINSTANCE.createRefinement();
@@ -59,7 +71,55 @@ public class VPMRefinementServiceTest extends AbstractTest {
         assertEquals("wrong number of variation point groups after refinement", 3, refinedVPM.getVariationPointGroups()
                 .size());
 
-        // ModelUtils.save(refinedVPM, "testresult/gcd-refined.vpm");
+        for (VariationPointGroup vpGroup : ((VariationPointModel) refinedVPM).getVariationPointGroups()) {
+            if (vpGroup.getGroupId().equals("gcd")) {
+                ASTNode astNode = vpGroup.getVariationPoints().get(0).getEnclosingSoftwareEntity();
+                JavaApplication model = refinedVPM.getLeadingModel();
+                SourceConnector sourceConnector = new SourceConnector(model);
+                JavaNodeSourceRegion sourceRegion = sourceConnector.findSourceRegion(astNode);
+                assertNotNull(sourceRegion);
+            }
+        }
+    }
+
+    /**
+     * Test of a basic merge refinement application. 
+     * 
+     * The initial VPM used containes a set of VPs which are contained
+     * in the same Block (the body of the method gcd). All other VPs are 
+     * import statement changes contained in compilation unit.
+     * 
+     * The test creates a merge refinement for the gcd() VPs, 
+     * triggers the refinement service to perform the refinement,
+     * and the reduced number of VPs is checked afterwards.
+     * 
+     * @throws IOException
+     *             identifies that the vpm could not be loaded.
+     * @throws InterruptedException
+     *             identifies that the refinement process was interrupted.
+     */
+    @Test
+    public void testMergeRefinements() throws IOException, InterruptedException {
+
+        List<Refinement> refinements = new ArrayList<Refinement>();
+        Refinement refinement = RefinementFactory.eINSTANCE.createRefinement();
+        refinement.setSource("Manual");
+        refinement.setType(RefinementType.MERGE);
+        for (VariationPointGroup group : initialVpm.getVariationPointGroups()) {
+            for (VariationPoint vp : group.getVariationPoints()) {
+                if (vp.getEnclosingSoftwareEntity() instanceof Block) {
+                    refinement.getVariationPoints().add(vp);
+                }
+
+            }
+        }
+        refinements.add(refinement);
+        assertEquals("wrong number of refinements identified", 1, refinements.size());
+
+        VPMRefinementService refinementService = new VPMRefinementService();
+        VariationPointModel refinedVPM = refinementService.applyRefinements(refinements, initialVpm);
+        assertEquals("wrong number of variation point groups after refinement", 3, refinedVPM.getVariationPointGroups()
+                .size());
 
         for (VariationPointGroup vpGroup : ((VariationPointModel) refinedVPM).getVariationPointGroups()) {
             if (vpGroup.getGroupId().equals("gcd")) {
--- VPMRefinement/org.splevo.vpm.refinement/src/org/splevo/vpm/refinement/VPMRefinementService.java
+++ VPMRefinement/org.splevo.vpm.refinement/src/org/splevo/vpm/refinement/VPMRefinementService.java
@@ -1,7 +1,13 @@
 package org.splevo.vpm.refinement;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.gmt.modisco.java.ASTNode;
+import org.splevo.vpm.variability.Variant;
 import org.splevo.vpm.variability.VariationPoint;
 import org.splevo.vpm.variability.VariationPointGroup;
 import org.splevo.vpm.variability.VariationPointModel;
@@ -11,10 +17,11 @@ import org.splevo.vpm.variability.VariationPointModel;
  */
 public class VPMRefinementService {
 
+    /** The logger for this class. */
+    private Logger logger = Logger.getLogger(VPMRefinementService.class);
+
     /**
      * Apply a list of refinements to a variation point model.
-     * 
-     * TODO Implement refinement application for merge type
      * 
      * @param refinements
      *            The refinements to be applied.
@@ -26,23 +33,96 @@ public class VPMRefinementService {
 
         for (Refinement refinement : refinements) {
 
-            if (refinement.getType().equals(RefinementType.GROUPING)) {
+            switch (refinement.getType()) {
 
-                // The group of the first variation point is kept as surviving
-                // group
-                // The variation points of all others are moved to this group
-                // and the other groups are removed.
-                VariationPointGroup survivingGroup = refinement.getVariationPoints().get(0).getGroup();
-                for (VariationPoint vp : refinement.getVariationPoints()) {
-                    if (!vp.getGroup().equals(survivingGroup)) {
-                        VariationPointGroup oldGroup = vp.getGroup();
-                        vp.setGroup(survivingGroup);
-                        vpm.getVariationPointGroups().remove(oldGroup);
-                    }
-                }
+            case GROUPING:
+                applyGrouping(vpm, refinement);
+                break;
+
+            case MERGE:
+                applyMerging(vpm, refinement);
+                break;
+
+            default:
+                logger.warn("Unsupported refinement type: " + refinement.getType());
+                break;
             }
         }
 
         return vpm;
+    }
+
+    /**
+     * VariationPoints are merged by consolidating their Variant elements and the referenced
+     * software entities in only one of the VariationPoint elements. First, one VariationPoint
+     * element that should survive is selected. Then, for all Variant elements in the
+     * VariationPoints, contributing to the same child feature, the ASTNode references are merged
+     * into one of the Variant elements. It is ensured, that this Variant element is contained in
+     * the surviving VariationPoint. Finally,
+     * 
+     * the remaining empty Variant and VariationPoint elements are removed from the model.
+     * 
+     * @param vpm
+     *            The variation point model to refine.
+     * @param refinement
+     *            The refinement to apply.
+     */
+    private void applyMerging(VariationPointModel vpm, Refinement refinement) {
+
+        VariationPoint survivingVP = refinement.getVariationPoints().get(0);
+
+        HashMap<String, Variant> variantMap = new HashMap<String, Variant>();
+        for (VariationPoint vp : refinement.getVariationPoints()) {
+            for (Variant variant : vp.getVariants()) {
+                if (variantMap.containsKey(variant.getVariantId())) {
+                    EList<ASTNode> swEntities = variant.getSoftwareEntities();
+                    variantMap.get(variant.getVariantId()).getSoftwareEntities().addAll(swEntities);
+                } else {
+                    variantMap.put(variant.getVariantId(), variant);
+                }
+            }
+
+            if (!vp.equals(survivingVP)) {
+                VariationPointGroup oldGroup = vp.getGroup();
+                oldGroup.getVariationPoints().remove(vp);
+            }
+        }
+
+        // clean up empty variation point groups
+        LinkedList<VariationPointGroup> vpGroupsToDelete = new LinkedList<VariationPointGroup>();
+        for (VariationPointGroup oldGroup : vpm.getVariationPointGroups()) {
+            if (oldGroup.getVariationPoints().size() == 0) {
+                vpGroupsToDelete.add(oldGroup);
+            }
+        }
+        vpm.getVariationPointGroups().removeAll(vpGroupsToDelete);
+
+        // ensure only the merged variants are present in the surviving variation point.
+        survivingVP.getVariants().clear();
+        survivingVP.getVariants().addAll(variantMap.values());
+
+    }
+
+    /**
+     * Refine the variation point model by applying a refinement.
+     * 
+     * @param vpm
+     *            The variation point model to refine.
+     * @param refinement
+     *            The refinement to apply.
+     */
+    private void applyGrouping(VariationPointModel vpm, Refinement refinement) {
+        // The group of the first variation point is kept as surviving
+        // group
+        // The variation points of all others are moved to this group
+        // and the other groups are removed.
+        VariationPointGroup survivingGroup = refinement.getVariationPoints().get(0).getGroup();
+        for (VariationPoint vp : refinement.getVariationPoints()) {
+            if (!vp.getGroup().equals(survivingGroup)) {
+                VariationPointGroup oldGroup = vp.getGroup();
+                vp.setGroup(survivingGroup);
+                vpm.getVariationPointGroups().remove(oldGroup);
+            }
+        }
     }
 }
