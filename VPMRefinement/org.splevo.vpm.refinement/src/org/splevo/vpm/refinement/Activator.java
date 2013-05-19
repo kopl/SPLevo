20003c196bffcc328fc0af66f11b59908087e954
BenjaminKlatt <klatt@fzi.de>
2013-05-19 13:41:43 +0000

    Minor code cleanup

--- VPMRefinement/org.splevo.vpm.refinement/src/org/splevo/vpm/refinement/Activator.java
+++ VPMRefinement/org.splevo.vpm.refinement/src/org/splevo/vpm/refinement/Activator.java
@@ -3,26 +3,37 @@ package org.splevo.vpm.refinement;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+/**
+ * The activator controlling the life cycle of the VPM refinement plugin.
+ */
 public class Activator implements BundleActivator {
 
+	/** The context the plugin is started in. */
 	private static BundleContext context;
 
+	/**
+     * Accessory for the plugins runtime context.
+     * 
+     * @return the context
+     */
 	static BundleContext getContext() {
 		return context;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * {@inheritDoc}
 	 */
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
+     * {@inheritDoc}
+     */
+    @Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
