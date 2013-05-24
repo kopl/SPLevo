39bb6e8b13d56c15fc5b58299d272ddfd5e337bf
dkojic <daniel.kojic@gmx.de>
2013-05-24 16:47:28 +0000

    SPLevo SPLEVO-61 Semantic Relationship VPMAnalyzer
    -further work on Analyzer class
    -package info added

--- VPMAnalyzer/org.splevo.vpm.analyzer.semantic/src/org/splevo/vpm/analyzer/semantic/SemanticVPMAnalyzer.java
+++ VPMAnalyzer/org.splevo.vpm.analyzer.semantic/src/org/splevo/vpm/analyzer/semantic/SemanticVPMAnalyzer.java
@@ -2,12 +2,19 @@ package org.splevo.vpm.analyzer.semantic;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.eclipse.gmt.modisco.java.ASTNode;
+import org.graphstream.graph.Node;
 import org.splevo.vpm.analyzer.AbstractVPMAnalyzer;
 import org.splevo.vpm.analyzer.VPMAnalyzerConfigurationType;
 import org.splevo.vpm.analyzer.VPMAnalyzerResult;
 import org.splevo.vpm.analyzer.graph.VPMGraph;
+import org.splevo.vpm.variability.VariationPoint;
 
 public class SemanticVPMAnalyzer extends AbstractVPMAnalyzer{
+	
+	/** The logger for this class. */
+    private Logger logger = Logger.getLogger(SemanticVPMAnalyzer.class);
 	
 	/** The relationship label of the analyzer. */
     public static final String RELATIONSHIP_LABEL_SEMANTIC = "Semantic";
@@ -17,8 +24,19 @@ public class SemanticVPMAnalyzer extends AbstractVPMAnalyzer{
     
 	@Override
 	public VPMAnalyzerResult analyze(VPMGraph vpmGraph) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// Container for the result.
+		VPMAnalyzerResult result = new VPMAnalyzerResult(this);
+		
+		logger.info("Filling index...");
+		fillIndex(vpmGraph);
+		logger.info("Indexing done.");
+
+		logger.info("Analyzing...");
+		findRelationships(result);
+		logger.info("Analysis done.");
+
+		return result;
 	}
 
 	@Override
@@ -49,4 +67,29 @@ public class SemanticVPMAnalyzer extends AbstractVPMAnalyzer{
 		return RELATIONSHIP_LABEL_SEMANTIC;
 	}
 
+	/**
+	 * Writes all necessary data from the {@link VPMGraph} into the Index.
+	 * 
+	 * @param vpmGraph The {@link VPMGraph} containing the information to be indexed. 
+	 */
+	private void fillIndex(VPMGraph vpmGraph){
+		// TODO Auto-generated method stub
+		for (Node node : vpmGraph.getNodeSet()) {
+            VariationPoint vp = node.getAttribute(VPMGraph.VARIATIONPOINT, VariationPoint.class);
+            if (vp != null) {
+                ASTNode astNode = vp.getEnclosingSoftwareEntity();
+                // TODO: find out how to extract code.
+            }
+
+        }
+	}
+	
+	/**
+	 * Finds semantic relationships between the variation points.
+	 * 
+	 * @param result Contains all relationships found.
+	 */
+	private void findRelationships(VPMAnalyzerResult result) {
+		// TODO Auto-generated method stub	
+	}
 }
---
+++ VPMAnalyzer/org.splevo.vpm.analyzer.semantic/src/org/splevo/vpm/analyzer/semantic/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The base package of vpm analyzer for the 
+ * variation point program semantic relationships.
+ */
+package org.splevo.vpm.analyzer.semantic;
