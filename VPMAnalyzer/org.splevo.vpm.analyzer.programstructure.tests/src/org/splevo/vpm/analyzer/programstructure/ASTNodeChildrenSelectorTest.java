7ec02c879adf9430c3d13f059974576ea1d07512
BenjaminKlatt <klatt@fzi.de>
2013-06-26 23:03:09 +0000

    SPLEVO-67 ProgramStructureVPMAnalyzer: Improve analysis strategy minor test additions

---
+++ VPMAnalyzer/org.splevo.vpm.analyzer.programstructure.tests/src/org/splevo/vpm/analyzer/programstructure/ASTNodeChildrenSelectorTest.java
@@ -0,0 +1,34 @@
+package org.splevo.vpm.analyzer.programstructure;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.eclipse.gmt.modisco.java.ASTNode;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
+import org.eclipse.gmt.modisco.java.emf.JavaFactory;
+import org.junit.Test;
+import org.splevo.vpm.analyzer.programstructure.index.ASTNodeChildrenSelector;
+
+/**
+ * Test case to validate children selector model switch.
+ * 
+ * @author Benjamin Klatt
+ * 
+ */
+public class ASTNodeChildrenSelectorTest {
+
+    /**
+     * Create an ast node children selector and prove it agains a list of different ast nodes.
+     */
+    @Test
+    public void testDoSwitchEClassEObject() {
+        ASTNodeChildrenSelector astNodeTraverser = new ASTNodeChildrenSelector();
+        
+        ClassDeclaration classDeclaration = JavaFactory.eINSTANCE.createClassDeclaration();
+        classDeclaration.getBodyDeclarations().add(JavaFactory.eINSTANCE.createMethodDeclaration());
+        
+        List<ASTNode> classDeclarationChilds = astNodeTraverser.doSwitch(classDeclaration);
+        assertEquals("Wrong number of children",1,classDeclarationChilds.size());
+    }
+}
