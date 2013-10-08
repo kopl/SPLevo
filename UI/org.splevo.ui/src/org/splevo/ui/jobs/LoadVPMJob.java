0cc80d2595f62827e4bc38ecd6a8b6346991523d
BenjaminKlatt <klatt@fzi.de>
2013-10-08 10:19:31 +0000

    SPLEVO-123 ClassNotFound during VPM Analysis right after Tool Start

--- UI/org.splevo.ui/src/org/splevo/ui/jobs/LoadVPMJob.java
+++ UI/org.splevo.ui/src/org/splevo/ui/jobs/LoadVPMJob.java
@@ -1,13 +1,18 @@
 package org.splevo.ui.jobs;
 
 import java.io.File;
+import java.util.List;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.compare.util.ModelUtils;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.splevo.modisco.java.vpm.software.softwarePackage;
 import org.splevo.project.SPLevoProject;
+import org.splevo.vpm.builder.DefaultVPMBuilderService;
+import org.splevo.vpm.builder.VPMBuilder;
+import org.splevo.vpm.builder.VPMBuilderService;
 import org.splevo.vpm.variability.VariationPointModel;
 import org.splevo.vpm.variability.variabilityPackage;
 
@@ -19,80 +24,75 @@ import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
 /**
  * Job to load a variation point model into the blackboard.
  */
-public class LoadVPMJob extends
-		AbstractBlackboardInteractingJob<SPLevoBlackBoard> {
+public class LoadVPMJob extends AbstractBlackboardInteractingJob<SPLevoBlackBoard> {
 
-	/** The splevo project to get the vpm info from. */
-	private SPLevoProject splevoProject;
+    /** The splevo project to get the vpm info from. */
+    private SPLevoProject splevoProject;
 
-	/**
-	 * The index of the vpm to be loaded. If this is -1 the latest one will be
-	 * loaded.
-	 */
-	private int targetVPMIndex = -1;
+    /**
+     * The index of the vpm to be loaded. If this is -1 the latest one will be loaded.
+     */
+    private int targetVPMIndex = -1;
 
-	/**
-	 * Constructor to set a reference to the splevoproject.
-	 * 
-	 * @param splevoProject
-	 *            The reference to the splevoproject.
-	 */
-	public LoadVPMJob(SPLevoProject splevoProject) {
-		this.splevoProject = splevoProject;
-		this.targetVPMIndex = -1;
-	}
+    /**
+     * Constructor to set a reference to the splevoproject.
+     * 
+     * @param splevoProject
+     *            The reference to the splevoproject.
+     */
+    public LoadVPMJob(SPLevoProject splevoProject) {
+        this.splevoProject = splevoProject;
+        this.targetVPMIndex = -1;
+    }
 
-	/**
-	 * Constructor to set a reference to the splevo project and the index of the
-	 * vpm to be loaded.
-	 * 
-	 * @param splevoProject
-	 *            The reference to the splevo project.
-	 * @param index
-	 *            The index of the vpm.
-	 */
-	public LoadVPMJob(SPLevoProject splevoProject, int index) {
-		this.splevoProject = splevoProject;
-		this.targetVPMIndex = index;
-	}
+    /**
+     * Constructor to set a reference to the splevo project and the index of the vpm to be loaded.
+     * 
+     * @param splevoProject
+     *            The reference to the splevo project.
+     * @param index
+     *            The index of the vpm.
+     */
+    public LoadVPMJob(SPLevoProject splevoProject, int index) {
+        this.splevoProject = splevoProject;
+        this.targetVPMIndex = index;
+    }
 
-	@Override
-	public void execute(IProgressMonitor monitor) throws JobFailedException,
-			UserCanceledException {
+    @Override
+    public void execute(IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
 
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		String basePath = workspace.getRoot().getRawLocation().toOSString();
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        String basePath = workspace.getRoot().getRawLocation().toOSString();
 
-		logger.info("Load vpm model");
-		variabilityPackage.eINSTANCE.eClass();
+        logger.info("Load vpm model");
+        variabilityPackage.eINSTANCE.eClass();
+        softwarePackage.eINSTANCE.eClass();
 
-		int index = targetVPMIndex;
-		if (targetVPMIndex == -1) {
-			index = splevoProject.getVpmModelPaths().size() - 1;
-		}
-		VariationPointModel vpm;
-		try {
-			File vpmFile = new File(basePath
-					+ splevoProject.getVpmModelPaths().get(index));
-			vpm = (VariationPointModel) ModelUtils.load(vpmFile,
-					new ResourceSetImpl());
-		} catch (Exception e) {
-			throw new JobFailedException("Failed to load vpm model.", e);
-		}
+        int index = targetVPMIndex;
+        if (targetVPMIndex == -1) {
+            index = splevoProject.getVpmModelPaths().size() - 1;
+        }
+        VariationPointModel vpm;
+        try {
+            File vpmFile = new File(basePath + splevoProject.getVpmModelPaths().get(index));
+            vpm = (VariationPointModel) ModelUtils.load(vpmFile, new ResourceSetImpl());
+        } catch (Exception e) {
+            throw new JobFailedException("Failed to load vpm model.", e);
+        }
 
-		logger.info("Put variation point model on the blackboard");
-		getBlackboard().setVariationPointModel(vpm);
+        logger.info("Put variation point model on the blackboard");
+        getBlackboard().setVariationPointModel(vpm);
 
-		// finish run
-		monitor.done();
-	}
+        // finish run
+        monitor.done();
+    }
 
-	@Override
-	public String getName() {
-		return "Load VPM model Job";
-	}
+    @Override
+    public String getName() {
+        return "Load VPM model Job";
+    }
 
-	@Override
-	public void cleanup(IProgressMonitor arg0) throws CleanupFailedException {
-	}
+    @Override
+    public void cleanup(IProgressMonitor arg0) throws CleanupFailedException {
+    }
 }
