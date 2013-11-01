63e9c665fe884c5e424fe70e920257e95e36f5a1
BenjaminKlatt <klatt@fzi.de>
2013-11-01 10:52:47 +0000

    SPLEVO-119 Introduce Technology Abstraction Execution of all registered VPM Builder

--- VPMBuilder/org.splevo.vpm.builder/src/org/splevo/vpm/builder/DefaultVPMBuilderService.java
+++ VPMBuilder/org.splevo.vpm.builder/src/org/splevo/vpm/builder/DefaultVPMBuilderService.java
@@ -13,98 +13,115 @@ import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.splevo.vpm.variability.VariationPointModel;
+import org.splevo.vpm.variability.variabilityFactory;
 
 /**
  * Default service to run the an extractor.
  */
 public class DefaultVPMBuilderService implements VPMBuilderService {
 
-	private Logger logger = Logger.getLogger(DefaultVPMBuilderService.class);
+    private Logger logger = Logger.getLogger(DefaultVPMBuilderService.class);
 
-	/** Regular expressions defining packages to be ignored. */
-	private static final String MSG_DIFFER_NOT_AVAILABLE = "No builder available.";
-	private static final String DIFFER_EXTENSION_POINT_ID = "org.splevo.vpm.builder";
-	private static final String EXTENSION_POINT_ATTR_DIFFER_CLASS = "builder.class";
+    /** Regular expressions defining packages to be ignored. */
+    private static final String MSG_DIFFER_NOT_AVAILABLE = "No builder available.";
+    private static final String DIFFER_EXTENSION_POINT_ID = "org.splevo.vpm.builder";
+    private static final String EXTENSION_POINT_ATTR_DIFFER_CLASS = "builder.class";
 
-	@Override
-	public VariationPointModel buildVPM(DiffModel diffModel, String variantIDLeading, String variantIDIntegration, Map<String, Object> builderOptions)
-            throws VPMBuildException {
+    @Override
+    public VariationPointModel buildVPM(DiffModel diffModel, String variantIDLeading, String variantIDIntegration,
+            Map<String, Object> builderOptions) throws VPMBuildException {
 
-		List<VPMBuilder> builders = getVPMBuilders();
-		if (builders.size() == 0) {
-			throw new VPMBuildException(String.format(MSG_DIFFER_NOT_AVAILABLE));
-		}
+        List<VPMBuilder> builders = getVPMBuilders();
+        if (builders.size() == 0) {
+            throw new VPMBuildException(String.format(MSG_DIFFER_NOT_AVAILABLE));
+        }
 
-		// TODO Implement using all builders.
-		return builders.get(0).buildVPM(diffModel, variantIDLeading, variantIDIntegration);
-	}
+        VariationPointModel vpm = variabilityFactory.eINSTANCE.createVariationPointModel();
 
-	/**
-	 * Load the software model extractor implementations registered for the
-	 * according extension point.
-	 * 
-	 * {@inheritDoc}
-	 */
-	@Override
-	public List<VPMBuilder> getVPMBuilders() {
-		List<VPMBuilder> builders = new LinkedList<VPMBuilder>();
+        for (VPMBuilder vpmBuilder : builders) {
+            VariationPointModel builderVPM = vpmBuilder.buildVPM(diffModel, variantIDLeading, variantIDIntegration);
+            mergeIntoVPM(vpm, builderVPM);
+        }
 
-		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		if (registry == null) {
-			logger.warn("No extension point registry available.");
-			return null;
-		}
-		IExtensionPoint extensionPoint = registry
-				.getExtensionPoint(DIFFER_EXTENSION_POINT_ID);
+        return vpm;
+    }
 
-		if (extensionPoint == null) {
-			logger.warn("No extension point found for the ID "
-					+ DIFFER_EXTENSION_POINT_ID);
-			return null;
-		}
-		IExtension[] extensions = extensionPoint.getExtensions();
-		for (IExtension extension : extensions) {
-			IConfigurationElement[] configurations = extension
-					.getConfigurationElements();
-			for (IConfigurationElement element : configurations) {
-				try {
-					Object o = element
-							.createExecutableExtension(EXTENSION_POINT_ATTR_DIFFER_CLASS);
-					if ((o != null) && (o instanceof VPMBuilder)) {
-					    VPMBuilder differ = (VPMBuilder) o;
-						builders.add(differ);
-					}
-				} catch (CoreException e) {
-					logger.error(
-							"Failed to load VPM builder extension",
-							e);
-				}
-			}
-		}
+    /**
+     * Merge a variation point model into another one. The second vpm will be merged into the first
+     * one.
+     * 
+     * @param enrichVPM
+     *            The VPM to enrich.
+     * @param mergeVPM
+     *            The VPM to merge into the other one.
+     */
+    private void mergeIntoVPM(VariationPointModel enrichVPM, VariationPointModel mergeVPM) {
+        if (mergeVPM != null) {
+            enrichVPM.getSoftwareElements().addAll(mergeVPM.getSoftwareElements());
+            enrichVPM.getVariationPointGroups().addAll(mergeVPM.getVariationPointGroups());
+        }
+    }
 
-		if (differIdsNotUnique(builders)) {
-			logger.warn("Two or more VPM builder with the same id loaded.");
-		}
+    /**
+     * Load the software model extractor implementations registered for the according extension
+     * point.
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public List<VPMBuilder> getVPMBuilders() {
+        List<VPMBuilder> builders = new LinkedList<VPMBuilder>();
 
-		return builders;
-	}
+        IExtensionRegistry registry = Platform.getExtensionRegistry();
+        if (registry == null) {
+            logger.warn("No extension point registry available.");
+            return null;
+        }
+        IExtensionPoint extensionPoint = registry.getExtensionPoint(DIFFER_EXTENSION_POINT_ID);
 
-	/**
-	 * Check if there are two or more builders with the same id.
-	 * 
-	 * @param builders
-	 *            The builders to check.
-	 * @return True if the same id is used more than once. False otherwise.
-	 */
-	private boolean differIdsNotUnique(List<VPMBuilder> builders) {
-		List<String> ids = new LinkedList<String>();
-		for (VPMBuilder builder : builders) {
-			if (ids.contains(builder.getId())) {
-				return true;
-			}
-			ids.add(builder.getId());
-		}
-		return false;
+        if (extensionPoint == null) {
+            logger.warn("No extension point found for the ID " + DIFFER_EXTENSION_POINT_ID);
+            return null;
+        }
+        IExtension[] extensions = extensionPoint.getExtensions();
+        for (IExtension extension : extensions) {
+            IConfigurationElement[] configurations = extension.getConfigurationElements();
+            for (IConfigurationElement element : configurations) {
+                try {
+                    Object o = element.createExecutableExtension(EXTENSION_POINT_ATTR_DIFFER_CLASS);
+                    if ((o != null) && (o instanceof VPMBuilder)) {
+                        VPMBuilder differ = (VPMBuilder) o;
+                        builders.add(differ);
+                    }
+                } catch (CoreException e) {
+                    logger.error("Failed to load VPM builder extension", e);
+                }
+            }
+        }
 
-	}
+        if (differIdsNotUnique(builders)) {
+            logger.warn("Two or more VPM builder with the same id loaded.");
+        }
+
+        return builders;
+    }
+
+    /**
+     * Check if there are two or more builders with the same id.
+     * 
+     * @param builders
+     *            The builders to check.
+     * @return True if the same id is used more than once. False otherwise.
+     */
+    private boolean differIdsNotUnique(List<VPMBuilder> builders) {
+        List<String> ids = new LinkedList<String>();
+        for (VPMBuilder builder : builders) {
+            if (ids.contains(builder.getId())) {
+                return true;
+            }
+            ids.add(builder.getId());
+        }
+        return false;
+
+    }
 }
