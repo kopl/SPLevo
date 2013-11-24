16ee6614d1c533ca27e449a2be5f87b40d2705c0
BenjaminKlatt <klatt@fzi.de>
2013-11-24 22:50:47 +0000

    SPLEVO-144 JaMoPP Diffing Initial implementation of the Diffing module

---
+++ JaMoPPCartridge/org.splevo.jamopp.diffing.editor/src-gen/org/splevo/jamopp/diffing/jamoppdiff/presentation/JaMoPPDiffEditorPlugin.java
@@ -0,0 +1,99 @@
+/**
+ */
+package org.splevo.jamopp.diffing.jamoppdiff.presentation;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.compare.provider.EMFCompareEditPlugin;
+
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+import org.emftext.commons.layout.provider.LayoutEditPlugin;
+
+import org.emftext.language.java.annotations.provider.JavaEditPlugin;
+
+/**
+ * This is the central singleton for the JaMoPPDiff editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class JaMoPPDiffEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final JaMoPPDiffEditorPlugin INSTANCE = new JaMoPPDiffEditorPlugin();
+
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public JaMoPPDiffEditorPlugin() {
+		super(new ResourceLocator[] { EMFCompareEditPlugin.INSTANCE,
+				EcoreEditPlugin.INSTANCE, JavaEditPlugin.INSTANCE,
+				LayoutEditPlugin.INSTANCE, });
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
