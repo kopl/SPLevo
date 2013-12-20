5f3ba9c1cabbc7a70d57039df0cb03681a1ea5cd
BenjaminKlatt <klatt@fzi.de>
2013-12-20 09:41:39 +0000

    SPLEVO-146 JaMoPP VPM Extension Initial JaMoPP VPM

---
+++ JaMoPPCartridge/org.splevo.jamopp.vpm.editor/src-gen/org/splevo/jamopp/vpm/software/presentation/JaMoPPVPMEditorPlugin.java
@@ -0,0 +1,99 @@
+/**
+ */
+package org.splevo.jamopp.vpm.software.presentation;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+import org.eclipse.featuremodel.provider.FeatureModelEditPlugin;
+
+import org.emftext.commons.layout.provider.LayoutEditPlugin;
+
+import org.emftext.language.java.annotations.provider.JavaEditPlugin;
+
+import org.splevo.vpm.variability.provider.vpmEditPlugin;
+
+/**
+ * This is the central singleton for the JaMoPPVPM editor plugin. <!--
+ * begin-user-doc --> <!-- end-user-doc -->
+ * 
+ * @generated
+ */
+public final class JaMoPPVPMEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	public static final JaMoPPVPMEditorPlugin INSTANCE = new JaMoPPVPMEditorPlugin();
+
+	/**
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public JaMoPPVPMEditorPlugin() {
+		super(new ResourceLocator[] { EcoreEditPlugin.INSTANCE,
+				FeatureModelEditPlugin.INSTANCE, JavaEditPlugin.INSTANCE,
+				LayoutEditPlugin.INSTANCE, vpmEditPlugin.INSTANCE, });
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+		 * 
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
