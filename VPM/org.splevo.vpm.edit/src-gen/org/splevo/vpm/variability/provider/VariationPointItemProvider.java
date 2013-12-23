b53cdfdb63734f14f8b7aaf3efae6719f30ce236
BenjaminKlatt <klatt@fzi.de>
2013-12-23 16:07:19 +0000

    SPLEVO-164 SourceLocation Start-/EndPosition in total File

--- VPM/org.splevo.vpm.edit/src-gen/org/splevo/vpm/variability/provider/VariationPointItemProvider.java
+++ VPM/org.splevo.vpm.edit/src-gen/org/splevo/vpm/variability/provider/VariationPointItemProvider.java
@@ -30,154 +30,165 @@ import org.splevo.vpm.variability.variabilityPackage;
  * <!-- end-user-doc -->
  * @generated
  */
-public class VariationPointItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider,
-        IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
-    /**
-     * This constructs an instance from a factory and a notifier.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public VariationPointItemProvider(AdapterFactory adapterFactory) {
-        super(adapterFactory);
-    }
+public class VariationPointItemProvider extends ItemProviderAdapter implements
+		IEditingDomainItemProvider, IStructuredItemContentProvider,
+		ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public VariationPointItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
 
-    /**
-     * This returns the property descriptors for the adapted class.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-        if (itemPropertyDescriptors == null) {
-            super.getPropertyDescriptors(object);
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
 
-            addEnclosingSoftwareEntityPropertyDescriptor(object);
-        }
-        return itemPropertyDescriptors;
-    }
+			addEnclosingSoftwareEntityPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
 
-    /**
-     * This adds a property descriptor for the Enclosing Software Entity feature.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    protected void addEnclosingSoftwareEntityPropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add(createItemPropertyDescriptor(
-                ((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
-                getResourceLocator(),
-                getString("_UI_VariationPoint_enclosingSoftwareEntity_feature"),
-                getString("_UI_PropertyDescriptor_description", "_UI_VariationPoint_enclosingSoftwareEntity_feature",
-                        "_UI_VariationPoint_type"),
-                variabilityPackage.Literals.VARIATION_POINT__ENCLOSING_SOFTWARE_ENTITY, true, false, true, null, null,
-                null));
-    }
+	/**
+	 * This adds a property descriptor for the Enclosing Software Entity feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addEnclosingSoftwareEntityPropertyDescriptor(Object object) {
+		itemPropertyDescriptors
+				.add(createItemPropertyDescriptor(
+						((ComposeableAdapterFactory) adapterFactory)
+								.getRootAdapterFactory(),
+						getResourceLocator(),
+						getString("_UI_VariationPoint_enclosingSoftwareEntity_feature"),
+						getString(
+								"_UI_PropertyDescriptor_description",
+								"_UI_VariationPoint_enclosingSoftwareEntity_feature",
+								"_UI_VariationPoint_type"),
+						variabilityPackage.Literals.VARIATION_POINT__ENCLOSING_SOFTWARE_ENTITY,
+						true, false, true, null, null, null));
+	}
 
-    /**
-     * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-     * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-     * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
-        if (childrenFeatures == null) {
-            super.getChildrenFeatures(object);
-            childrenFeatures.add(variabilityPackage.Literals.VARIATION_POINT__VARIANTS);
-        }
-        return childrenFeatures;
-    }
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(
+			Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures
+					.add(variabilityPackage.Literals.VARIATION_POINT__VARIANTS);
+		}
+		return childrenFeatures;
+	}
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    protected EStructuralFeature getChildFeature(Object object, Object child) {
-        // Check the type of the specified child object and return the proper feature to use for
-        // adding (see {@link AddCommand}) it as a child.
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
 
-        return super.getChildFeature(object, child);
-    }
+		return super.getChildFeature(object, child);
+	}
 
-    /**
-     * This returns VariationPoint.gif.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public Object getImage(Object object) {
-        return overlayImage(object, getResourceLocator().getImage("full/obj16/VariationPoint"));
-    }
+	/**
+	 * This returns VariationPoint.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object,
+				getResourceLocator().getImage("full/obj16/VariationPoint"));
+	}
 
-    /**
-     * This returns the label text for the adapted class.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated not
-     */
-    @Override
-    public String getText(Object object) {
-        StringBuilder label = new StringBuilder();
-        label.append(getString("_UI_VariationPoint_type"));
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated not
+	 */
+	@Override
+	public String getText(Object object) {
+		StringBuilder label = new StringBuilder();
+		label.append(getString("_UI_VariationPoint_type"));
 
-        VariationPoint vp = (VariationPoint) object;
-        SoftwareElement astNode = vp.getEnclosingSoftwareEntity();
-        label.append(" in ");
-        label.append(astNode.getLabel());
+		VariationPoint vp = (VariationPoint) object;
+		SoftwareElement astNode = vp.getEnclosingSoftwareEntity();
+		label.append(" in ");
+		label.append(astNode.getLabel());
 
-        return label.toString();
-    }
+		return label.toString();
+	}
 
-    /**
-     * This handles model notifications by calling {@link #updateChildren} to update any cached
-     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public void notifyChanged(Notification notification) {
-        updateChildren(notification);
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
 
-        switch (notification.getFeatureID(VariationPoint.class)) {
-        case variabilityPackage.VARIATION_POINT__VARIANTS:
-            fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-            return;
-        }
-        super.notifyChanged(notification);
-    }
+		switch (notification.getFeatureID(VariationPoint.class)) {
+		case variabilityPackage.VARIATION_POINT__VARIANTS:
+			fireNotifyChanged(new ViewerNotification(notification,
+					notification.getNotifier(), true, false));
+			return;
+		}
+		super.notifyChanged(notification);
+	}
 
-    /**
-     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-     * that can be created under this object.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
-        super.collectNewChildDescriptors(newChildDescriptors, object);
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(
+			Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
 
-        newChildDescriptors.add(createChildParameter(variabilityPackage.Literals.VARIATION_POINT__VARIANTS,
-                variabilityFactory.eINSTANCE.createVariant()));
-    }
+		newChildDescriptors.add(createChildParameter(
+				variabilityPackage.Literals.VARIATION_POINT__VARIANTS,
+				variabilityFactory.eINSTANCE.createVariant()));
+	}
 
-    /**
-     * Return the resource locator for this item provider's resources.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public ResourceLocator getResourceLocator() {
-        return ((IChildCreationExtender) adapterFactory).getResourceLocator();
-    }
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return ((IChildCreationExtender) adapterFactory).getResourceLocator();
+	}
 
 }
--- VPM/org.splevo.vpm.edit/src-gen/org/splevo/vpm/variability/provider/vpmEditPlugin.java
+++ VPM/org.splevo.vpm.edit/src-gen/org/splevo/vpm/variability/provider/vpmEditPlugin.java
@@ -14,75 +14,76 @@ import org.eclipse.featuremodel.provider.FeatureModelEditPlugin;
  * @generated
  */
 public final class vpmEditPlugin extends EMFPlugin {
-    /**
-     * Keep track of the singleton.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public static final vpmEditPlugin INSTANCE = new vpmEditPlugin();
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final vpmEditPlugin INSTANCE = new vpmEditPlugin();
 
-    /**
-     * Keep track of the singleton.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    private static Implementation plugin;
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
 
-    /**
-     * Create the instance.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public vpmEditPlugin() {
-        super(new ResourceLocator[] { EcoreEditPlugin.INSTANCE, FeatureModelEditPlugin.INSTANCE, });
-    }
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public vpmEditPlugin() {
+		super(new ResourceLocator[] { EcoreEditPlugin.INSTANCE,
+				FeatureModelEditPlugin.INSTANCE, });
+	}
 
-    /**
-     * Returns the singleton instance of the Eclipse plugin.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @return the singleton instance.
-     * @generated
-     */
-    @Override
-    public ResourceLocator getPluginResourceLocator() {
-        return plugin;
-    }
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
 
-    /**
-     * Returns the singleton instance of the Eclipse plugin.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @return the singleton instance.
-     * @generated
-     */
-    public static Implementation getPlugin() {
-        return plugin;
-    }
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
 
-    /**
-     * The actual implementation of the Eclipse <b>Plugin</b>.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public static class Implementation extends EclipsePlugin {
-        /**
-         * Creates an instance.
-         * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
-         * @generated
-         */
-        public Implementation() {
-            super();
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipsePlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
 
-            // Remember the static instance.
-            //
-            plugin = this;
-        }
-    }
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
 
 }
