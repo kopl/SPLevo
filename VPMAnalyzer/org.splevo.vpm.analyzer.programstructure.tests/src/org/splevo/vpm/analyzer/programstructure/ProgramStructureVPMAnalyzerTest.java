2f4bbc555ff330941ad9264ad63d93008302d9b7
BenjaminKlatt <klatt@fzi.de>
2013-12-27 14:56:08 +0000

    minor code cleanup

--- VPMAnalyzer/org.splevo.vpm.analyzer.programstructure.tests/src/org/splevo/vpm/analyzer/programstructure/ProgramStructureVPMAnalyzerTest.java
+++ VPMAnalyzer/org.splevo.vpm.analyzer.programstructure.tests/src/org/splevo/vpm/analyzer/programstructure/ProgramStructureVPMAnalyzerTest.java
@@ -1,13 +1,19 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2013
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.splevo.vpm.analyzer.programstructure;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -20,56 +26,56 @@ import org.splevo.vpm.analyzer.graph.VPMGraph;
 
 /**
  * Test case for the ProgramDependencyVPMAnalyzer.
- * 
+ *
  * @author Benjamin Klatt
  */
 public class ProgramStructureVPMAnalyzerTest extends AbstractTest {
 
     /** The logger for this class. */
     private Logger logger = Logger.getLogger(ProgramStructureVPMAnalyzerTest.class);
-    
+
     /** An instance of the analyzer for general tests. */
     private final ProgramStructureVPMAnalyzer analyzer = new ProgramStructureVPMAnalyzer();
-    
+
     /**
      * Test method for
      * {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#analyze(org.splevo.vpm.analyzer.graph.VPMGraph)}
      * .
-     * 
-     * It actively registers the MoDiscoProgramStructureProvider to enable the test
-     * run as a regular JUnit test without starting the OSGi Bundles.
-     * 
-     * @throws IOException
+     *
+     * It actively registers the MoDiscoProgramStructureProvider to enable the test run as a regular
+     * JUnit test without starting the OSGi Bundles.
+     *
+     * @throws Exception
      *             Identifies the test input graph could not be read.
-     * @throws InterruptedException
-     *             identifies the test has been interrupted.
      */
     @Test
     public void testAnalyze() throws Exception {
-        
+
         Activator.getProgramStructureProviders().add(new MoDiscoJavaProgramStructureProvider());
-        
+
         VPMGraph graph = SPLevoTestUtil.loadGCDVPMGraph();
-        
+
         int originalNodeCount = graph.getNodeCount();
         int originalEdgeCount = graph.getEdgeCount();
-        
+
         VPMAnalyzerResult result = analyzer.analyze(graph);
-        
-        assertNotNull("The analyzer result must not be null",result);
+
+        assertNotNull("The analyzer result must not be null", result);
         assertEquals("The graph's node count should not have been changed.", originalNodeCount, graph.getNodeCount());
         assertEquals("The graph's edge count should not have been changed.", originalEdgeCount, graph.getEdgeCount());
 
         for (VPMEdgeDescriptor edgeDescriptor : result.getEdgeDescriptors()) {
-            logger.debug("Edge Sub Label referred SoftwareElement: "+edgeDescriptor.getRelationshipSubLabel());
+            logger.debug("Edge Sub Label referred SoftwareElement: " + edgeDescriptor.getRelationshipSubLabel());
         }
-        
+
         assertEquals("Wrong edge descriptor count", 10, result.getEdgeDescriptors().size());
-        
+
     }
 
     /**
-     * Test method for {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#getConfigurations()}.
+     * Test method for
+     * {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#getConfigurations()}
+     * .
      */
     @Test
     public void testGetConfigurations() {
@@ -77,7 +83,8 @@ public class ProgramStructureVPMAnalyzerTest extends AbstractTest {
     }
 
     /**
-     * Test method for {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#getName()}.
+     * Test method for
+     * {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#getName()}.
      */
     @Test
     public void testGetName() {
@@ -86,7 +93,9 @@ public class ProgramStructureVPMAnalyzerTest extends AbstractTest {
     }
 
     /**
-     * Test method for {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#getRelationshipLabel()}.
+     * Test method for
+     * {@link org.splevo.vpm.analyzer.programstructure.ProgramStructureVPMAnalyzer#getRelationshipLabel()}
+     * .
      */
     @Test
     public void testGetRelationshipLabel() {
---
+++ VPMAnalyzer/org.splevo.vpm.analyzer.programstructure.tests/src/org/splevo/vpm/analyzer/programstructure/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Test package for the program structure vpm analyzer.
+ */
+package org.splevo.vpm.analyzer.programstructure;
