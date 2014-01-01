ddd9d637b5c67fdebca3f7f4036b08ffba442fa6
BenjaminKlatt <klatt@fzi.de>
2014-01-01 14:09:52 +0000

    SPLEVO-142 JaMoPP Extractor Code cleanup and switched to on demand proxy resolution only

--- JaMoPPCartridge/org.splevo.jamopp.extraction/src/org/splevo/jamopp/extraction/JaMoPPSoftwareModelExtractor.java
+++ JaMoPPCartridge/org.splevo.jamopp.extraction/src/org/splevo/jamopp/extraction/JaMoPPSoftwareModelExtractor.java
@@ -3,23 +3,17 @@ package org.splevo.jamopp.extraction;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.emftext.language.java.JavaClasspath;
 import org.emftext.language.java.JavaPackage;
@@ -58,52 +52,22 @@ public class JaMoPPSoftwareModelExtractor implements SoftwareModelExtractor {
 
         for (URI projectPathURI : projectURIs) {
 
-//            ResourceSet rs = setUpResourceSet();
-//            JavaClasspath cp = JavaClasspath.get(rs);
-
             String projectPath = projectPathURI.toFileString();
             cp.getURIMap().put(projectPathURI, URI.createURI(""));
             File srcFolder = new File(projectPath);
             try {
                 srcFolder.isDirectory();
                 addAllJarFilesToClassPath(srcFolder, cp);
-                loadAllFilesInResourceSet(srcFolder, ".java", targetResourceSet);
+                loadAllJavaFilesInResourceSet(srcFolder, targetResourceSet);
 
             } catch (Exception e) {
                 throw new SoftwareModelExtractionException("Failed to extract software model.", e);
             }
         }
 
-        if (!resolveAllProxies(0, targetResourceSet)) {
-            handleFailedProxyResolution(targetResourceSet);
-        }
+        logger.info("Java and Jar Files added to resource set");
 
         return targetResourceSet;
-    }
-
-    /**
-     * Handle any failed proxy resolution.
-     *
-     * @param rs
-     *            The resource set to prove for any un-handled proxies.
-     */
-    private void handleFailedProxyResolution(ResourceSet rs) {
-        logger.error("Resolution of some Proxies failed...");
-        Iterator<Notifier> it = rs.getAllContents();
-        while (it.hasNext()) {
-            Notifier next = it.next();
-            if (next instanceof EObject) {
-                EObject o = (EObject) next;
-                if (o.eIsProxy()) {
-                    try {
-                        it.remove();
-                    } catch (UnsupportedOperationException e) {
-                        e.printStackTrace();
-                        logger.info("Error during unresolved proxy handling", e);
-                    }
-                }
-            }
-        }
     }
 
     /**
@@ -117,7 +81,6 @@ public class JaMoPPSoftwareModelExtractor implements SoftwareModelExtractor {
      *             Any exception during jar access.
      */
     private void addAllJarFilesToClassPath(File projectDirectory, JavaClasspath classPath) throws IOException {
-
         Collection<File> jarFiles = FileUtils.listFiles(projectDirectory, new String[] { "jar" }, true);
         for (File jarPath : jarFiles) {
             if (jarPath.exists()) {
@@ -127,30 +90,19 @@ public class JaMoPPSoftwareModelExtractor implements SoftwareModelExtractor {
     }
 
     /**
-     * Load a all files as resource in a specific folder and it's sub folders.<br>
-     * This ignores internal directories starting with ".".
+     * Load a all files as resource in a specific folder and it's sub folders.
      *
      * @param rootFolder
      *            The root folder to recursively load all resources from.
-     * @param fileExtension
-     *            The extension of files to load.
      * @param rs
      *            The resource set to add it to.
      * @throws IOException
      *             An exception during resource access.
      */
-    private void loadAllFilesInResourceSet(File rootFolder, String fileExtension, ResourceSet rs) throws IOException {
-        for (File member : rootFolder.listFiles()) {
-            if (member.isFile()) {
-                if (member.getName().endsWith(fileExtension)) {
-                    parseResource(member, rs);
-                }
-            }
-            if (member.isDirectory()) {
-                if (!member.getName().startsWith(".")) {
-                    loadAllFilesInResourceSet(member, fileExtension, rs);
-                }
-            }
+    private void loadAllJavaFilesInResourceSet(File rootFolder, ResourceSet rs) throws IOException {
+        Collection<File> javaFiles = FileUtils.listFiles(rootFolder, new String[] { "java" }, true);
+        for (File javaFile : javaFiles) {
+            parseResource(javaFile, rs);
         }
     }
 
@@ -194,48 +146,6 @@ public class JaMoPPSoftwareModelExtractor implements SoftwareModelExtractor {
      */
     private void loadResource(URI uri, ResourceSet rs) throws IOException {
         rs.getResource(uri, true);
-    }
-
-    /**
-     * Resolve all proxies of the loaded sources.
-     *
-     * @param resourcesProcessedBefore
-     *            The counter of processed resources for statistical reasons.
-     * @param rs
-     *            The resource set to resolve the proxies in.
-     * @return True/False if the resolving was successful or not.
-     */
-    private boolean resolveAllProxies(int resourcesProcessedBefore, ResourceSet rs) {
-        boolean failure = false;
-        List<EObject> eobjects = new LinkedList<EObject>();
-        for (Iterator<Notifier> i = rs.getAllContents(); i.hasNext();) {
-            Notifier next = i.next();
-            if (next instanceof EObject) {
-                eobjects.add((EObject) next);
-            }
-        }
-        int resourcesProcessed = rs.getResources().size();
-        if (resourcesProcessed == resourcesProcessedBefore) {
-            return true;
-        }
-
-        for (EObject next : eobjects) {
-            InternalEObject nextElement = (InternalEObject) next;
-            for (EObject crElement : nextElement.eCrossReferences()) {
-                crElement = EcoreUtil.resolve(crElement, rs);
-                if (crElement.eIsProxy()) {
-                    failure = true;
-                    logger.warn("Can not find referenced element in classpath: "
-                            + ((InternalEObject) crElement).eProxyURI());
-                }
-            }
-        }
-
-        // call this method again, because the resolving might have triggered
-        // loading
-        // of additional resources that may also contain references that need to
-        // be resolved.
-        return !failure && resolveAllProxies(resourcesProcessed, rs);
     }
 
     /**
