de4b6ae54809de66db68702b40b4d92d5c76aab2
BenjaminKlatt <klatt@fzi.de>
2014-01-06 23:02:52 +0000

    SPLEVO-139 Make overall SPLevo Tests technology independent

--- VPMAnalyzer/org.splevo.vpm.analyzer.codelocation.tests/src/org/splevo/vpm/analyzer/codelocation/AbstractTest.java
+++ VPMAnalyzer/org.splevo.vpm.analyzer.codelocation.tests/src/org/splevo/vpm/analyzer/codelocation/AbstractTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.splevo.vpm.analyzer.codelocation;
 
 import org.apache.log4j.BasicConfigurator;
@@ -6,7 +17,7 @@ import org.apache.log4j.PatternLayout;
 import org.junit.Before;
 
 /**
- * Abstract base class for tests. 
+ * Abstract base class for tests.
  */
 public abstract class AbstractTest {
 
--- VPMAnalyzer/org.splevo.vpm.analyzer.codelocation.tests/src/org/splevo/vpm/analyzer/codelocation/CodeLocationVPMAnalyzerTest.java
+++ VPMAnalyzer/org.splevo.vpm.analyzer.codelocation.tests/src/org/splevo/vpm/analyzer/codelocation/CodeLocationVPMAnalyzerTest.java
@@ -1,67 +1,73 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.splevo.vpm.analyzer.codelocation;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.io.IOException;
-
 import org.junit.Test;
 import org.splevo.tests.SPLevoTestUtil;
 import org.splevo.vpm.analyzer.VPMAnalyzer;
 import org.splevo.vpm.analyzer.VPMAnalyzerResult;
-import org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer;
 import org.splevo.vpm.analyzer.graph.VPMGraph;
 
 /**
  * Test case for the CodeStructureVPMAnalyzer.
- * 
- * @author Benjamin Klatt
- *
  */
 public class CodeLocationVPMAnalyzerTest extends AbstractTest {
 
     /**
-     * Test method for {@link org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer#analyze(org.splevo.vpm.analyzer.graph.VPMGraph)}.
-     * @throws InterruptedException identifies the test was interrupted.
-     * @throws IOException Identifies the initial vpm graph could not be loaded.
+     * Test method for
+     * {@link org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer#analyze(org.splevo.vpm.analyzer.graph.VPMGraph)}
+     * .
+     *
+     * @throws Exception
+     *             Identifies the initial vpm graph could not be loaded.
      */
     @Test
     public void testAnalyze() throws Exception {
         VPMGraph graph = SPLevoTestUtil.loadGCDVPMGraph();
-        
+
         int originalNodeCount = graph.getNodeCount();
         int originalEdgeCount = graph.getEdgeCount();
-        
+
         VPMAnalyzer analyzer = new CodeLocationVPMAnalyzer();
         VPMAnalyzerResult result = analyzer.analyze(graph);
-        
-        assertNotNull("The analyzer result must not be null",result);
+
+        assertNotNull("The analyzer result must not be null", result);
         assertEquals("The graph's node count should not have been changed.", originalNodeCount, graph.getNodeCount());
         assertEquals("The graph's edge count should not have been changed.", originalEdgeCount, graph.getEdgeCount());
-        
-        // TODO: Check import add/delete parent node structure
+
         assertEquals("Wrong edge descriptor count", 11, result.getEdgeDescriptors().size());
     }
 
     /**
-     * Test method for {@link org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer#getName()}.
+     * Test method for
+     * {@link org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer#getName()}.
      */
     @Test
     public void testGetName() {
         VPMAnalyzer anlayzer = new CodeLocationVPMAnalyzer();
-        assertNotNull("The analyzer name must not be not.",anlayzer.getName());
+        assertNotNull("The analyzer name must not be not.", anlayzer.getName());
     }
 
     /**
-     * Test method for {@link org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer#getRelationshipLabel()}.
+     * Test method for
+     * {@link org.splevo.vpm.analyzer.codelocation.CodeLocationVPMAnalyzer#getRelationshipLabel()}.
      */
     @Test
     public void testGetRelationshipLabel() {
         VPMAnalyzer anlayzer = new CodeLocationVPMAnalyzer();
-        assertNotNull("The analyzer relationship label must not be not.",anlayzer.getRelationshipLabel());
+        assertNotNull("The analyzer relationship label must not be not.", anlayzer.getRelationshipLabel());
     }
 
 }
