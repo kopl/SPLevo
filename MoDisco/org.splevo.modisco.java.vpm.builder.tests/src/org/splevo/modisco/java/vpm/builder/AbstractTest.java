9e6f6e3a0dc7ebec3858421d08acee3b0a422347
BenjaminKlatt <klatt@fzi.de>
2014-01-07 00:24:38 +0000

    Fixed unit tests

--- MoDisco/org.splevo.modisco.java.vpm.builder.tests/src/org/splevo/modisco/java/vpm/builder/AbstractTest.java
+++ MoDisco/org.splevo.modisco.java.vpm.builder.tests/src/org/splevo/modisco/java/vpm/builder/AbstractTest.java
@@ -3,22 +3,22 @@ package org.splevo.modisco.java.vpm.builder;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.PatternLayout;
-import org.junit.BeforeClass;
+import org.junit.Before;
 
 /**
- * Abstract base class for tests. 
+ * Abstract base class for tests.
  */
 public abstract class AbstractTest {
 
-	/**
-	 * Prepare the test.
-	 * Initializes a log4j logging environment.
-	 */
-	@BeforeClass
-	public static void setUp() {
-		// set up a basic logging configuration for the test environment
-		BasicConfigurator.resetConfiguration();
-		BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
-	}
+    /**
+     * Prepare the test.
+     * Initializes a log4j logging environment.
+     */
+    @Before
+    public void setUp() {
+        // set up a basic logging configuration for the test environment
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
+    }
 
 }
--- MoDisco/org.splevo.modisco.java.vpm.builder.tests/src/org/splevo/modisco/java/vpm/builder/Java2KDMVPMBuilderTest.java
+++ MoDisco/org.splevo.modisco.java.vpm.builder.tests/src/org/splevo/modisco/java/vpm/builder/Java2KDMVPMBuilderTest.java
@@ -3,14 +3,22 @@ package org.splevo.modisco.java.vpm.builder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.eclipse.emf.compare.Comparison;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.splevo.tests.SPLevoTestUtil;
+import org.splevo.diffing.JavaDiffer;
+import org.splevo.modisco.java.diffing.Java2KDMDiffer;
 import org.splevo.vpm.software.SoftwareElement;
 import org.splevo.vpm.variability.VariationPoint;
 import org.splevo.vpm.variability.VariationPointGroup;
@@ -18,28 +26,44 @@ import org.splevo.vpm.variability.VariationPointModel;
 
 /**
  * Test case for the Java2KDM based VPM builder
- * 
+ *
  * This test case directly invokes the diffing service to produce the diffing model the builder
  * should use. It introduces a direct dependency that should be removed later on, but for now, it
  * improves the development cycles time for changes in the diff engine.
- * 
+ *
  */
-public class Java2KDMVPMBuilderTest extends AbstractTest {
+public class Java2KDMVPMBuilderTest {
 
     /** The logger to use for this class. */
     private Logger logger = Logger.getLogger(Java2KDMVPMBuilderTest.class);
 
     private static Comparison comparisonModel;
 
+    /** Source path to the original implementation. */
+    private static final File TEST_DIR_1 = new File("testmodels/gcd/native");
+
+    /** Source path to the modified implementation. */
+    private static final File TEST_DIR_2 = new File("testmodels/gcd/jscience");
+
     /**
      * Load the diff model to process.
-     * 
+     *
      * @throws Exception
      *             Failed to load generate the diff model.
      */
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        comparisonModel = SPLevoTestUtil.loadGCDDiffModel();
+
+        // set up a basic logging configuration for the test environment
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
+
+        // prepare the default differ and options
+        Map<String, Object> diffOptions = new LinkedHashMap<String, Object>();
+        diffOptions.put(JavaDiffer.OPTION_JAVA_IGNORE_PACKAGES, Arrays.asList("java.*", "org.jscience.*", "javolution.*"));
+
+        Java2KDMDiffer differ = new Java2KDMDiffer();
+        comparisonModel = differ.doDiff(TEST_DIR_1.toURI(), TEST_DIR_2.toURI(), diffOptions);
     }
 
     /**
@@ -65,9 +89,9 @@ public class Java2KDMVPMBuilderTest extends AbstractTest {
         Java2KDMVPMBuilder java2KDMVPMBuilder = new Java2KDMVPMBuilder();
         VariationPointModel vpm = java2KDMVPMBuilder.buildVPM(comparisonModel, "LEADING", "INTEGRATION");
         assertNotNull("No VPM initialized", vpm);
-        
+
         Set<SoftwareElement> vpLocations = new LinkedHashSet<SoftwareElement>();
-        
+
         for (VariationPointGroup vpGroup : vpm.getVariationPointGroups()) {
             for (VariationPoint vp : vpGroup.getVariationPoints()) {
                 vpLocations.add(vp.getEnclosingSoftwareEntity());
