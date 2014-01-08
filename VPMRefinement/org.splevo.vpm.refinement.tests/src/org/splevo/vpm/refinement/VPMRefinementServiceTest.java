55be868a80b42d4a4b731f3b9f16d4c102a112f2
BenjaminKlatt <klatt@fzi.de>
2014-01-08 10:56:11 +0000

    SPLEVO-169 Basic Configuration for Differ Extensions Basic configuration infrastructure and java ignore packages migrated

--- VPMRefinement/org.splevo.vpm.refinement.tests/src/org/splevo/vpm/refinement/VPMRefinementServiceTest.java
+++ VPMRefinement/org.splevo.vpm.refinement.tests/src/org/splevo/vpm/refinement/VPMRefinementServiceTest.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
 import org.junit.Before;
 import org.junit.Test;
 import org.splevo.tests.SPLevoTestUtil;
@@ -17,20 +20,25 @@ import org.splevo.vpm.variability.VariationPointModel;
 
 /**
  * Test the vpm refinement service.
- * 
+ *
  * Initial VPM is loaded during test setup. VPM describes changes in the GCD Example Version 1.0: -
  * ImportInsert: LargeInteger - ImportDelete: BigInteger - StatementChange: integerValue1 -
  * StatementChange: integerValue2 - StatementChange: gcd - StatementInsert: return gcd.toString() //
  * LargeInteger.toString(); - StatementDelete: return gcd.toString() // BigInteger.toString();
  */
-public class VPMRefinementServiceTest extends AbstractTest {
+public class VPMRefinementServiceTest {
 
     /** The initial vpm model to refine. */
     private VariationPointModel initialVpm = null;
 
-    @Override
+    /**
+     * Initialize test infrastructure and requirements.
+     */
     @Before
     public void setUp() {
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
+
         try {
             initialVpm = SPLevoTestUtil.loadGCDVPMModel();
         } catch (Exception e) {
@@ -42,7 +50,7 @@ public class VPMRefinementServiceTest extends AbstractTest {
     /**
      * Test of a basic refinement analysis and application. This test makes use of the basic
      * location based variation point analysis.
-     * 
+     *
      * @throws IOException
      *             identifies that the vpm could not be loaded.
      * @throws InterruptedException
@@ -83,13 +91,13 @@ public class VPMRefinementServiceTest extends AbstractTest {
 
     /**
      * Test of a basic merge refinement application.
-     * 
+     *
      * The initial VPM used containes a set of VPs which are contained in the same Block (the body
      * of the method gcd). All other VPs are import statement changes contained in compilation unit.
-     * 
+     *
      * The test creates a merge refinement for the gcd() VPs, triggers the refinement service to
      * perform the refinement, and the reduced number of VPs is checked afterwards.
-     * 
+     *
      * @throws IOException
      *             identifies that the vpm could not be loaded.
      * @throws InterruptedException
