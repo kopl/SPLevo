758e034bdb592b7d9c130f12424672468d19b0ce
BenjaminKlatt <klatt@fzi.de>
2014-01-30 17:38:30 +0000

    SPLEVO-183 ExtensionPoint & Configuration FM Generation Introduced new extension point to register builders for specific feature models. This is the extension point part only and all available builders are triggered.

---
+++ FM/org.splevo.fm.builder/src/org/splevo/fm/builder/FeatureModelBuilder.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.splevo.fm.builder;
+
+import org.splevo.vpm.variability.VariationPointModel;
+
+/**
+ * Interface to build a specific feature model from a provided Comparison model.
+ *
+ * @param <Model>
+ *            The specific type of the model, the builder is able to produce.
+ */
+public interface FeatureModelBuilder<Model> {
+
+    /**
+     * Get the identifier of this builder. This should be unique in your installation.
+     *
+     * @return The string identifying the builder.
+     */
+    public String getId();
+
+    /**
+     * Get a human readable label for the builder.
+     *
+     * @return The label.
+     */
+    public String getLabel();
+
+    /**
+     * Build a feature model according to the specified model type of the concrete builder.
+     *
+     * @param vpm
+     *            The variation point model to derive the feature model from.
+     *
+     * @param rootFeatureName
+     *            The name of the feature models root element.
+     * @return The prepared feature model.
+     */
+    public Model build(VariationPointModel vpm, String rootFeatureName);
+
+    /**
+     * Save a feature model of the builder's specific model type.
+     *
+     * @param featureModel
+     *            The model to save.
+     * @param targetDirectory
+     *            The path to the directory to save to.
+     */
+    public void save(Model featureModel, String targetDirectory);
+}
---
+++ FM/org.splevo.fm.builder/src/org/splevo/fm/builder/FeatureModelBuilderService.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.splevo.fm.builder;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.splevo.vpm.variability.VariationPointModel;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+/**
+ * Service to access available feature model builder and to trigger selected ones for model
+ * generation.
+ */
+public class FeatureModelBuilderService {
+
+    /** Message to provide if a requested builder can not be loaded. */
+    private static final String MSG_BUILDER_NOT_AVAILABLE = "No feature model builder available for the provided id: %1";
+
+    /** The id of the extension point to look up registered builders. */
+    private static final String BUILDER_EXTENSION_POINT_ID = "org.splevo.fm.builder";
+
+    /** The extension attribute specifying the builder class to instantiate. */
+    private static final String EXTENSION_POINT_ATTR_BUILDER_CLASS = "builder.class";
+
+    /** The list of available builders. */
+    private Map<String, FeatureModelBuilder<?>> builder = Maps.newLinkedHashMap();
+
+    private static Logger logger = Logger.getLogger(FeatureModelBuilderService.class);
+
+    // TODO refactor into a plugin activator.
+    /**
+     * Constructor trying to load the available builders from the eclipse registry if available.
+     */
+    public FeatureModelBuilderService() {
+        loadRegisteredFeatureModels();
+    }
+
+    /**
+     * Trigger the feature model generation for the builders identified by the list of IDs provided.
+     *
+     * @param builderIds
+     *            The IDs of the builder to trigger.
+     * @param vpm
+     *            The variation point model to build the feature models for.
+     * @param rootNodeName
+     *            The name of the models root node.
+     * @param targetPath
+     *            The path of the base directory to save the models to. If null is provided the save
+     *            will not be triggered.
+     * @return The list of build feature models.
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public List<Object> buildAndSaveModels(List<String> builderIds, VariationPointModel vpm, String rootNodeName,
+            String targetPath) {
+        List<Object> featureModels = Lists.newArrayList();
+        for (String id : builderIds) {
+            FeatureModelBuilder currentBuilder = getAvailableBuilders().get(id);
+            if (currentBuilder == builder) {
+                logger.warn(String.format(MSG_BUILDER_NOT_AVAILABLE, id));
+                continue;
+            }
+
+            Object featureModel = currentBuilder.build(vpm, rootNodeName);
+            featureModels.add(featureModel);
+            if (targetPath != null) {
+                currentBuilder.save(featureModel, targetPath);
+            }
+        }
+        return featureModels;
+    }
+
+    /**
+     * Try to access the eclipse extension registry and load the extension available.
+     */
+    private void loadRegisteredFeatureModels() {
+        IExtensionRegistry registry = Platform.getExtensionRegistry();
+        if (registry != null) {
+
+            IExtensionPoint extensionPoint = registry.getExtensionPoint(BUILDER_EXTENSION_POINT_ID);
+            if (extensionPoint != null) {
+                IExtension[] extensions = extensionPoint.getExtensions();
+                for (IExtension extension : extensions) {
+                    IConfigurationElement[] configurations = extension.getConfigurationElements();
+                    for (IConfigurationElement element : configurations) {
+                        try {
+                            Object o = element.createExecutableExtension(EXTENSION_POINT_ATTR_BUILDER_CLASS);
+                            if ((o != null) && (o instanceof FeatureModelBuilder<?>)) {
+                                FeatureModelBuilder<?> extractor = (FeatureModelBuilder<?>) o;
+                                builder.put(extractor.getId(), extractor);
+                            }
+                        } catch (CoreException e) {
+                            logger.error("Failed to load feature model builder extension", e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Get all registered feature model builder.
+     *
+     * Tries to access the eclipse extension registry, looks up registered extensions and returns
+     * them.
+     *
+     * @return A map with feature model builder IDs linked to the builder instances.
+     */
+    public Map<String, FeatureModelBuilder<?>> getAvailableBuilders() {
+        return builder;
+    }
+}
