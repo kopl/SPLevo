91d595a5fa85bb2c67d7958e5447be14bcc24805
BenjaminKlatt <klatt@fzi.de>
2014-02-14 12:34:04 +0000

    Enhanced VPM model loading to support reference caching

--- UI/org.splevo.ui/src/org/splevo/ui/jobs/LoadVPMJob.java
+++ UI/org.splevo.ui/src/org/splevo/ui/jobs/LoadVPMJob.java
@@ -1,13 +1,18 @@
 package org.splevo.ui.jobs;
 
 import java.io.File;
+import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.splevo.extraction.DefaultExtractionService;
 import org.splevo.project.SPLevoProject;
 import org.splevo.vpm.VPMUtil;
 import org.splevo.vpm.software.SoftwarePackage;
 import org.splevo.vpm.variability.VariationPointModel;
 import org.splevo.vpm.variability.variabilityPackage;
+
+import com.google.common.collect.Lists;
 
 import de.uka.ipd.sdq.workflow.jobs.AbstractBlackboardInteractingJob;
 import de.uka.ipd.sdq.workflow.jobs.CleanupFailedException;
@@ -29,7 +34,7 @@ public class LoadVPMJob extends AbstractBlackboardInteractingJob<SPLevoBlackBoar
 
     /**
      * Constructor to set a reference to the splevoproject.
-     * 
+     *
      * @param splevoProject
      *            The reference to the splevoproject.
      */
@@ -40,7 +45,7 @@ public class LoadVPMJob extends AbstractBlackboardInteractingJob<SPLevoBlackBoar
 
     /**
      * Constructor to set a reference to the splevo project and the index of the vpm to be loaded.
-     * 
+     *
      * @param splevoProject
      *            The reference to the splevo project.
      * @param index
@@ -62,10 +67,13 @@ public class LoadVPMJob extends AbstractBlackboardInteractingJob<SPLevoBlackBoar
         if (targetVPMIndex == -1) {
             index = splevoProject.getVpmModelPaths().size() - 1;
         }
+
+        ResourceSetImpl resourceSet = initResourceSet();
+
         VariationPointModel vpm;
         try {
             File vpmFile = new File(splevoProject.getVpmModelPaths().get(index));
-            vpm = VPMUtil.loadVariationPointModel(vpmFile);
+            vpm = VPMUtil.loadVariationPointModel(vpmFile, resourceSet);
         } catch (Exception e) {
             throw new JobFailedException("Failed to load vpm model.", e);
         }
@@ -75,6 +83,22 @@ public class LoadVPMJob extends AbstractBlackboardInteractingJob<SPLevoBlackBoar
 
         // finish run
         monitor.done();
+    }
+
+    /**
+     * Initialize the resource set including preparation by the source model extractors for specific
+     * source models.
+     *
+     * @return The initialized resource set.
+     */
+    private ResourceSetImpl initResourceSet() {
+        ResourceSetImpl resourceSet = new ResourceSetImpl();
+        DefaultExtractionService extractionService = new DefaultExtractionService();
+        List<String> sourceModelPaths = Lists.newArrayList();
+        sourceModelPaths.add(splevoProject.getSourceModelPathLeading());
+        sourceModelPaths.add(splevoProject.getSourceModelPathIntegration());
+        extractionService.prepareResourceSet(resourceSet, sourceModelPaths);
+        return resourceSet;
     }
 
     @Override
