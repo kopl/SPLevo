047177ca7efd73ed2565bc312cb06cc2b40df5f2
ChristianBusch <christian.busch2@gmx.de>
2014-02-27 06:51:07 +0000

    SPLEVO-190 Implemented demonstrator for displaying java source file contents in SourceViewers. SelectionChangedListener not yet implemented, so currently the SourceViewers will not update on selection in the TreeViewer.

--- UI/org.splevo.ui/src/org/splevo/ui/wizards/vpmanalysis/VPMRefinementPage.java
+++ UI/org.splevo.ui/src/org/splevo/ui/wizards/vpmanalysis/VPMRefinementPage.java
@@ -1,12 +1,19 @@
 package org.splevo.ui.wizards.vpmanalysis;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.ui.text.IColorManager;
+import org.eclipse.jdt.ui.text.JavaSourceViewerConfiguration;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.preference.PreferenceStore;
 import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.source.IVerticalRuler;
 import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.jface.text.source.VerticalRuler;
@@ -16,12 +23,11 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.editors.text.FileDocumentProvider;
-import org.eclipse.ui.editors.text.TextFileDocumentProvider;
-import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.splevo.ui.refinementbrowser.CompleteRefinementTreeContentProvider;
 import org.splevo.ui.refinementbrowser.CompleteRefinementTreeLabelProvider;
 import org.splevo.ui.refinementbrowser.VPMRefinementBrowserInput;
+
+import com.google.common.io.CharStreams;
 
 /**
  * Wizard page to modify the results of the vpm analysis.
@@ -42,7 +48,6 @@ public class VPMRefinementPage extends WizardPage {
 
     @Override
     public void createControl(final Composite parent) {
-        // TODO Insert actual content.
         final Composite container = new Composite(parent, SWT.NULL);
         this.setControl(container);
         container.setLayout(new FillLayout());
@@ -50,21 +55,85 @@ public class VPMRefinementPage extends WizardPage {
         this.treeViewer = new CheckboxTreeViewer(outerSash);
         this.treeViewer.setContentProvider(new CompleteRefinementTreeContentProvider());
         this.treeViewer.setLabelProvider(new CompleteRefinementTreeLabelProvider());
-        // treeViewer.setInput(refinementBrowserInput.getRefinements());
-        final SashForm editorSash = new SashForm(outerSash, SWT.HORIZONTAL);
+        final SashForm sourceViewersSash = new SashForm(outerSash, SWT.HORIZONTAL);
         final IVerticalRuler leadingRuler = new VerticalRuler(20);
         final IVerticalRuler followingRuler = new VerticalRuler(20);
-        final SourceViewer leadingSourceViewer = new SourceViewer(editorSash, leadingRuler, SWT.V_SCROLL);
-        final SourceViewer followingSourceViewer = new SourceViewer(editorSash, followingRuler, SWT.V_SCROLL);
-        // leadingSourceViewer.setEditable(false);
-        // followingSourceViewer.setEditable(false);
-        final IColorManager colorManager = null;
-        final IPreferenceStore preferenceStore = new PreferenceStore();
-        // SourceViewerConfiguration configuration = new JavaSourceViewerConfiguration(colorManager,
-        // preferenceStore, null, null);
-        // leadingSourceViewer.configure(configuration);
-        IDocument document = new Document();
-        final IDocumentProvider dp = new TextFileDocumentProvider();
+        final SourceViewer leadingSourceViewer = new SourceViewer(sourceViewersSash, leadingRuler, SWT.V_SCROLL);
+        final SourceViewer followingSourceViewer = new SourceViewer(sourceViewersSash, followingRuler, SWT.V_SCROLL);
+
+        Document leadingDocument = null;
+        Document followingDocument = null;
+
+        // Test code. Un-comment for testing. Requires Calculator example in the workspace of the run instance.
+        
+//        IPath leadingPath = new Path("/Calculator-native/src/org/splevo/examples/calculator/CalculatorGCD.java");
+//        IPath followingPath = new Path("/Calculator-JScience/src/org/splevo/examples/calculator/CalculatorGCD.java");
+//
+//        leadingDocument = createDocumentFromPath(leadingPath);
+//        followingDocument = createDocumentFromPath(followingPath);
+//
+//        displayDocument(leadingSourceViewer, leadingDocument);
+//        displayDocument(followingSourceViewer, followingDocument);
+
+    }
+
+    /**
+     * Displays the Document in the SourceViewer. To achieve this, the SourceViewer will also be set
+     * up according to the Document.
+     * 
+     * @param sourceViewer
+     *            The Source viewer to display the Document in.
+     * @param document
+     *            The Document to display.
+     */
+    private void displayDocument(final SourceViewer sourceViewer, Document document) {
+        // TODO: This needs to be modified to also handle other file types correctly.
+        // Left warnings intentionally so it's obvious that this is a hack.
+        // FIXME: Don't use any members of JavaPlugin even though this could result in code duplication.
+        final IColorManager colorManager = JavaPlugin.getDefault().getJavaTextTools().getColorManager();
+        final IPreferenceStore preferenceStore = JavaPlugin.getDefault().getCombinedPreferenceStore();
+
+        sourceViewer.unconfigure();
+        // TODO: This needs to be replaced for non-java files.
+        sourceViewer.configure(new JavaSourceViewerConfiguration(colorManager, preferenceStore, null, null));
+        sourceViewer.setEditable(false);
+        sourceViewer.setDocument(document);
+    }
+
+    /**
+     * Given a path will return a Document with the contents of the file at this path and the
+     * DocumentPartitioner set up accordingly.
+     * 
+     * @param path
+     *            Path of the File to generate a Document from.
+     * @return a Document containing the content of the file at the path.
+     */
+    private Document createDocumentFromPath(IPath path) {
+        Document document;
+        IFile leadingFile = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+        String content = null;
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(leadingFile.getContents(),
+                    leadingFile.getCharset());
+            content = CharStreams.toString(inputStreamReader);
+            inputStreamReader.close();
+        } catch (UnsupportedEncodingException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        } catch (IOException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        } catch (CoreException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        }
+
+        document = new Document(content);
+        // TODO: This needs to be modified to also handle other file types correctly.
+        // FIXME: Don't use any members of JavaPlugin even though this could result in code duplication.
+        JavaPlugin.getDefault().getJavaTextTools().setupJavaDocumentPartitioner(document);
+        return document;
     }
 
     /**
