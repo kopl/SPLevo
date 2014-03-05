17a32bca0b5f9eedbc79945c8aa56a75c09a5b7e
BenjaminKlatt <klatt@fzi.de>
2014-03-05 13:16:29 +0000

    SPLEVO-210 VPM Analyzer Configuration: Add ID to explicitly access a configuration

--- VPMAnalyzer/org.splevo.vpm.analyzer.tests/src/org/splevo/vpm/analyzer/config/VPMAnalyzerConfigurationsTest.java
+++ VPMAnalyzer/org.splevo.vpm.analyzer.tests/src/org/splevo/vpm/analyzer/config/VPMAnalyzerConfigurationsTest.java
@@ -2,78 +2,81 @@ package org.splevo.vpm.analyzer.config;
 
 import static org.junit.Assert.assertSame;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Tests for the {@link VPMAnalyzerConfigurationSet} class.
- * 
+ *
  * @author Daniel Kojic
- * 
+ *
  */
 public class VPMAnalyzerConfigurationsTest {
 
-	/**
-	 * The object under test.
-	 */
-	private VPMAnalyzerConfigurationSet testedObject;
+    /**
+     * Tests whether the map-size is 0 after initialization.
+     */
+    @Test
+    public final void testVPMAnalyzerConfigurations() {
+        VPMAnalyzerConfigurationSet configSet = new VPMAnalyzerConfigurationSet();
+        int size = configSet.getAllConfigurationsByGroupName().size();
+        assertSame("Map size should be 0 after initialization.", 0, size);
+    }
 
-	/**
-	 * Re-initializes the test-object before every test.
-	 */
-	@Before
-	public void beforeTest() {
-		this.testedObject = new VPMAnalyzerConfigurationSet();
-	}
+    /**
+     * Tests whether the map has the correct amount of elements.
+     */
+    @Test
+    public final void testAddConfigurationsAndGetAllConfigurationsByGroupName1() {
+        VPMAnalyzerConfigurationSet configSet = new VPMAnalyzerConfigurationSet();
+        BooleanConfiguration conf1 = new BooleanConfiguration("ID1", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf2 = new BooleanConfiguration("ID2", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf3 = new BooleanConfiguration("ID3", "LABEL", "EXPLANATION", true);
+        configSet.addConfigurations("Test", conf1, conf2, conf3);
 
-	/**
-	 * Tests whether the map-size is 0 after initialization.
-	 */
-	@Test
-	public final void testVPMAnalyzerConfigurations() {
-		int size = testedObject.getAllConfigurationsByGroupName().size();
-		assertSame("Map size should be 0 after initialization.", 0, size);
-	}
+        int size = configSet.getAllConfigurationsByGroupName().size();
+        assertSame("Map size should be 1.", 1, size);
+        int numElements = configSet.getAllConfigurationsByGroupName().get("Test").size();
+        assertSame("This key should have 3 elements.", 3, numElements);
+    }
 
-	/**
-	 * Tests whether the map has the correct amount of elements.
-	 */
-	@Test
-	public final void testAddConfigurationsAndGetAllConfigurationsByGroupName1() {
-		testedObject.addConfigurations("Test", new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true), new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true), new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true));
+    /**
+     * Tests whether the map has the correct amount of elements.
+     */
+    @Test
+    public final void testAddConfigurationsAndGetAllConfigurationsByGroupName2() {
+        VPMAnalyzerConfigurationSet configSet = new VPMAnalyzerConfigurationSet();
+        BooleanConfiguration conf1 = new BooleanConfiguration("ID1", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf2 = new BooleanConfiguration("ID2", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf3 = new BooleanConfiguration("ID3", "LABEL", "EXPLANATION", true);
+        configSet.addConfigurations("Test1", conf1, conf2, conf3);
 
-		int size = testedObject.getAllConfigurationsByGroupName().size();
-		assertSame("Map size should be 1.", 1, size);
-		int numElements = testedObject.getAllConfigurationsByGroupName()
-				.get("Test").size();
-		assertSame("This key should have 3 elements.", 3, numElements);
-	}
+        BooleanConfiguration conf4 = new BooleanConfiguration("ID4", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf5 = new BooleanConfiguration("ID5", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf6 = new BooleanConfiguration("ID6", "LABEL", "EXPLANATION", true);
+        configSet.addConfigurations("Test2", conf4, conf5, conf6);
 
-	/**
-	 * Tests whether the map has the correct amount of elements.
-	 */
-	@Test
-	public final void testAddConfigurationsAndGetAllConfigurationsByGroupName2() {
-		testedObject.addConfigurations("Test1", new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true), new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true), new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true));
-		testedObject.addConfigurations("Test2", new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true), new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true), new BooleanConfiguration(
-				"LABEL", "EXPLANATION", true));
+        int size = configSet.getAllConfigurationsByGroupName().size();
+        assertSame("Map size should be 1.", 2, size);
+        int numElements1 = configSet.getAllConfigurationsByGroupName().get("Test1").size();
+        int numElements2 = configSet.getAllConfigurationsByGroupName().get("Test2").size();
+        assertSame("This key should have 3 elements.", 3, numElements1);
+        assertSame("This key should have 3 elements.", 3, numElements2);
+    }
 
-		int size = testedObject.getAllConfigurationsByGroupName().size();
-		assertSame("Map size should be 1.", 2, size);
-		int numElements1 = testedObject.getAllConfigurationsByGroupName()
-				.get("Test1").size();
-		int numElements2 = testedObject.getAllConfigurationsByGroupName()
-				.get("Test2").size();
-		assertSame("This key should have 3 elements.", 3, numElements1);
-		assertSame("This key should have 3 elements.", 3, numElements2);
-	}
+    /**
+     * Tests whether the map has the correct amount of elements.
+     */
+    @Test
+    public final void testConfigIDUniqueInSet() {
+        VPMAnalyzerConfigurationSet configSet = new VPMAnalyzerConfigurationSet();
+        BooleanConfiguration conf1 = new BooleanConfiguration("ID1", "LABEL", "EXPLANATION", true);
+        BooleanConfiguration conf2 = new BooleanConfiguration("ID1", "LABEL", "EXPLANATION", true);
+        configSet.addConfigurations("Test1", conf1, conf2);
+
+        int size = configSet.getAllConfigurationsByGroupName().size();
+        assertSame("Map size should be 1.", 1, size);
+        int count = configSet.getAllConfigurationsByGroupName().get("Test1").size();
+        assertSame("This key should have 1 elements.", 1, count);
+    }
 
 }
