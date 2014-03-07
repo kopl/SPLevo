3bbb201c1d14042a3840e71214749697bfb78269
BenjaminKlatt <klatt@fzi.de>
2014-03-07 12:48:57 +0000

    SPLEVO-215 Analyzer Configuration Allow for unlimited numeric ranges

--- UI/org.splevo.ui/src/org/splevo/ui/wizards/vpmanalysis/UIConfigurationCompositeFactory.java
+++ UI/org.splevo.ui/src/org/splevo/ui/wizards/vpmanalysis/UIConfigurationCompositeFactory.java
@@ -9,7 +9,6 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.RowData;
@@ -32,251 +31,242 @@ import org.splevo.vpm.analyzer.config.StringConfiguration;
 import org.splevo.vpm.analyzer.config.VPMAnalyzerConfigurationSet;
 
 /**
- * This class has methods to create the configuration panel for the analysis
- * configuration wizard.
- * 
+ * This class has methods to create the configuration panel for the analysis configuration wizard.
+ *
  * @author Daniel Kojic
- * 
+ *
  */
 public class UIConfigurationCompositeFactory {
 
-	/**
-	 * The {@link VPMAnalyzer} to get the configs from.
-	 */
-	private VPMAnalyzer analyzer;
+    /**
+     * The {@link VPMAnalyzer} to get the configs from.
+     */
+    private VPMAnalyzer analyzer;
 
-	/**
-	 * @param analyzer
-	 *            The {@link VPMAnalyzer} to get the configs from.
-	 */
-	public UIConfigurationCompositeFactory(VPMAnalyzer analyzer) {
-		this.analyzer = analyzer;
-	}
+    /**
+     * @param analyzer
+     *            The {@link VPMAnalyzer} to get the configs from.
+     */
+    public UIConfigurationCompositeFactory(VPMAnalyzer analyzer) {
+        this.analyzer = analyzer;
+    }
 
-	/**
-	 * Takes a {@link Composite} and adds the configuration elements as
-	 * children.
-	 * 
-	 * @param parent
-	 *            The {@link Composite}.
-	 */
-	public void createConfigComps(Composite parent) {
-		VPMAnalyzerConfigurationSet configurations = analyzer
-				.getConfigurations();
-		Map<String, List<AbstractVPMAnalyzerConfiguration<?>>> configsByGroupName = configurations
-				.getAllConfigurationsByGroupName();
+    /**
+     * Takes a {@link Composite} and adds the configuration elements as children.
+     *
+     * @param parent
+     *            The {@link Composite}.
+     */
+    public void createConfigComps(Composite parent) {
+        VPMAnalyzerConfigurationSet configurations = analyzer.getConfigurations();
+        Map<String, List<AbstractVPMAnalyzerConfiguration<?>>> configsByGroupName = configurations
+                .getAllConfigurationsByGroupName();
 
-		for (String groupName : configsByGroupName.keySet()) {
-			createConfigurationGroup(parent, groupName,
-					configsByGroupName.get(groupName));
-		}
-	}
+        for (String groupName : configsByGroupName.keySet()) {
+            createConfigurationGroup(parent, groupName, configsByGroupName.get(groupName));
+        }
+    }
 
-	/**
-	 * Creates a named group that has configurations.
-	 * 
-	 * @param parent
-	 *            The parent {@link Composite}.
-	 * @param groupName
-	 *            The name of the group.
-	 * @param configs
-	 *            The UI configuration elements to be added to the group.
-	 */
-	private void createConfigurationGroup(Composite parent, String groupName,
-			List<AbstractVPMAnalyzerConfiguration<?>> configs) {
-		Group group = new Group(parent, SWT.NONE);
-		group.setText(groupName);
-		RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
-		rowLayout.justify = false;
-		rowLayout.fill = false;
-		rowLayout.wrap = false;
-		group.setLayout(rowLayout);
-		FormData formData = new FormData();
-		if (parent.getChildren().length == 1) {
-			formData.top = new FormAttachment(0);
-		} else {
-			Control lastGroup = parent.getChildren()[parent.getChildren().length - 2];
-			formData.top = new FormAttachment(lastGroup, 10);
-		}
-		formData.left = new FormAttachment(0);
-		formData.right = new FormAttachment(100);
-		group.setLayoutData(formData);
+    /**
+     * Creates a named group that has configurations.
+     *
+     * @param parent
+     *            The parent {@link Composite}.
+     * @param groupName
+     *            The name of the group.
+     * @param configs
+     *            The UI configuration elements to be added to the group.
+     */
+    private void createConfigurationGroup(Composite parent, String groupName,
+            List<AbstractVPMAnalyzerConfiguration<?>> configs) {
+        Group group = new Group(parent, SWT.NONE);
+        group.setText(groupName);
+        RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
+        rowLayout.justify = false;
+        rowLayout.fill = false;
+        rowLayout.wrap = false;
+        group.setLayout(rowLayout);
+        FormData formData = new FormData();
+        if (parent.getChildren().length == 1) {
+            formData.top = new FormAttachment(0);
+        } else {
+            Control lastGroup = parent.getChildren()[parent.getChildren().length - 2];
+            formData.top = new FormAttachment(lastGroup, 10);
+        }
+        formData.left = new FormAttachment(0);
+        formData.right = new FormAttachment(100);
+        group.setLayoutData(formData);
 
-		for (AbstractVPMAnalyzerConfiguration<?> config : configs) {
-			Composite parentComp = new Composite(group, SWT.NONE);
-			RowLayout layout = new RowLayout(SWT.HORIZONTAL);
-			layout.center = true;
-			parentComp.setLayout(layout);
-			if (config instanceof BooleanConfiguration) {
-				createBooleanConfigField(parentComp,
-						(BooleanConfiguration) config);
-			} else if (config instanceof StringConfiguration) {
-				createStringConfigField(parentComp,
-						(StringConfiguration) config);
-			} else if (config instanceof ChoiceConfiguration) {
-				createChoiceConfigField(parentComp,
-						(ChoiceConfiguration) config);
-			} else if (config instanceof NumericConfiguration) {
-				createNumericConfigField(parentComp,
-						(NumericConfiguration) config);
-			}
-		}
-	}
+        for (AbstractVPMAnalyzerConfiguration<?> config : configs) {
+            Composite parentComp = new Composite(group, SWT.NONE);
+            RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+            layout.center = true;
+            parentComp.setLayout(layout);
+            if (config instanceof BooleanConfiguration) {
+                createBooleanConfigField(parentComp, (BooleanConfiguration) config);
+            } else if (config instanceof StringConfiguration) {
+                createStringConfigField(parentComp, (StringConfiguration) config);
+            } else if (config instanceof ChoiceConfiguration) {
+                createChoiceConfigField(parentComp, (ChoiceConfiguration) config);
+            } else if (config instanceof NumericConfiguration) {
+                createNumericConfigField(parentComp, (NumericConfiguration) config);
+            }
+        }
+    }
 
-	/**
-	 * Creates the configuration elements for a numeric configuration. Adds them
-	 * to a parent element.
-	 * 
-	 * @param parent
-	 *            The parent.
-	 * @param config
-	 *            The numeric configuration.
-	 */
-	private void createNumericConfigField(Composite parent,
-			final NumericConfiguration config) {
-		addLabel(parent, config.getLabel());
-		final double spinnerTransformationFactor = Math.pow(10,
-				config.getNumFractionalDigits());
-		final Spinner spinner = new Spinner(parent, SWT.NONE);
-		spinner.setIncrement((int) Math.round(config.getStepSize()
-				* spinnerTransformationFactor));
-		spinner.setDigits(config.getNumFractionalDigits());
-		int value = (int) Math.round(config.getDefaultValue()
-				* spinnerTransformationFactor);
-		spinner.setSelection(value);
-		spinner.addModifyListener(new ModifyListener() {
+    /**
+     * Creates the configuration elements for a numeric configuration. Adds them to a parent
+     * element.
+     *
+     * This includes changing the background if the configured value is out of the valid range.
+     *
+     * @param parent
+     *            The parent.
+     * @param config
+     *            The numeric configuration.
+     */
+    private void createNumericConfigField(Composite parent, final NumericConfiguration config) {
+        addLabel(parent, config.getLabel());
+        final double spinnerTransformationFactor = Math.pow(10, config.getNumFractionalDigits());
+        final Spinner spinner = new Spinner(parent, SWT.NONE);
+        spinner.setIncrement((int) Math.round(config.getStepSize() * spinnerTransformationFactor));
+        spinner.setDigits(config.getNumFractionalDigits());
 
-			@Override
-			public void modifyText(ModifyEvent arg0) {
-				double spinnerValue = (double) spinner.getSelection()
-						/ spinnerTransformationFactor;
-				if (spinnerValue >= config.getLowerBoundary()
-						&& spinnerValue <= config.getUpperBoundary()) {
-					config.setCurrentValue(spinnerValue);
-					spinner.setBackground(spinner.getDisplay().getSystemColor(
-							SWT.COLOR_LIST_BACKGROUND));
-				} else {
-					spinner.setBackground(new Color(spinner.getDisplay(), 255,
-							0, 0));
-				}
-			}
-		});
+        int value = (int) Math.round(config.getDefaultValue() * spinnerTransformationFactor);
+        spinner.setSelection(value);
+        spinner.addModifyListener(new ModifyListener() {
 
-		addExplanation(parent, config.getExplanation());
-	}
+            @Override
+            public void modifyText(ModifyEvent arg0) {
+                double spinnerValue = (double) spinner.getSelection() / spinnerTransformationFactor;
 
-	/**
-	 * Creates the configuration elements for a chioce configuration. Adds them
-	 * to a parent element.
-	 * 
-	 * @param parent
-	 *            The parent.
-	 * @param config
-	 *            The choice configuration.
-	 */
-	private void createChoiceConfigField(Composite parent,
-			final ChoiceConfiguration config) {
-		addLabel(parent, config.getLabel());
-		final List<String> labels = config.getAvailableValues();
-		final Combo combo = new Combo(parent, SWT.DROP_DOWN | SWT.READ_ONLY
-				| SWT.SIMPLE);
-		combo.setItems(labels.toArray(new String[0]));
-		combo.select(labels.indexOf(config.getDefaultValue()));
-		combo.addSelectionListener(new SelectionAdapter() {
+                if (isInValidRange(config, spinnerValue)) {
+                    config.setCurrentValue(spinnerValue);
+                    spinner.setBackground(spinner.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
+                } else {
+                    spinner.setBackground(spinner.getDisplay().getSystemColor(SWT.COLOR_DARK_YELLOW));
+                }
+            }
 
-			@Override
-			public void widgetSelected(SelectionEvent arg0) {
-				String selectedValue = labels.get(combo.getSelectionIndex());
-				config.setCurrentValue(selectedValue);
-			}
-		});
-		addExplanation(parent, config.getExplanation());
-	}
+            private boolean isInValidRange(final NumericConfiguration config, double value) {
+                return (value >= config.getLowerBoundary() || config.getUpperBoundary() == -1)
+                        && (value <= config.getUpperBoundary() || config.getUpperBoundary() == -1);
+            }
+        });
 
-	/**
-	 * Creates the configuration elements for a string configuration. Adds them
-	 * to a parent element.
-	 * 
-	 * @param parent
-	 *            The parent.
-	 * @param config
-	 *            The string configuration.
-	 */
-	private void createStringConfigField(Composite parent,
-			final StringConfiguration config) {
-		addLabel(parent, config.getLabel());
-		final StyledText text = new StyledText(parent, SWT.V_SCROLL);
-		text.setLeftMargin(2);
-		text.setRightMargin(2);
-		text.setTopMargin(2);
-		text.setBottomMargin(2);
-		text.setWordWrap(true);
-		text.addModifyListener(new ModifyListener() {
+        addExplanation(parent, config.getExplanation());
+    }
 
-			@Override
-			public void modifyText(ModifyEvent arg0) {
-				config.setCurrentValue(text.getText());
-			}
-		});
-		String defaultText = config.getDefaultValue();
-		text.setText(defaultText);
-		if (defaultText.length() > 30) {
-			text.setLayoutData(new RowData(150, 80));
-		} else {
-			text.setLayoutData(new RowData(150, 16));
-		}
-		addExplanation(parent, config.getExplanation());
-	}
+    /**
+     * Creates the configuration elements for a chioce configuration. Adds them to a parent element.
+     *
+     * @param parent
+     *            The parent.
+     * @param config
+     *            The choice configuration.
+     */
+    private void createChoiceConfigField(Composite parent, final ChoiceConfiguration config) {
+        addLabel(parent, config.getLabel());
+        final List<String> labels = config.getAvailableValues();
+        final Combo combo = new Combo(parent, SWT.DROP_DOWN | SWT.READ_ONLY | SWT.SIMPLE);
+        combo.setItems(labels.toArray(new String[0]));
+        combo.select(labels.indexOf(config.getDefaultValue()));
+        combo.addSelectionListener(new SelectionAdapter() {
 
-	/**
-	 * Creates the configuration elements for a boolean configuration. Adds them
-	 * to a parent element.
-	 * 
-	 * @param parent
-	 *            The parent.
-	 * @param config
-	 *            The boolean configuration.
-	 */
-	private void createBooleanConfigField(Composite parent,
-			final BooleanConfiguration config) {
-		final Button checkButton = new Button(parent, SWT.CHECK);
-		checkButton.setSelection(config.getDefaultValue());
-		checkButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				super.widgetSelected(e);
-				config.setCurrentValue(checkButton.getSelection());
-			}
-		});
-		addLabel(parent, config.getLabel());
-		addExplanation(parent, config.getExplanation());
-	}
+            @Override
+            public void widgetSelected(SelectionEvent arg0) {
+                String selectedValue = labels.get(combo.getSelectionIndex());
+                config.setCurrentValue(selectedValue);
+            }
+        });
+        addExplanation(parent, config.getExplanation());
+    }
 
-	/**
-	 * Adds a label with given text to a composite.
-	 * 
-	 * @param parent The composite to add the label to.
-	 * @param text The text of the label.
-	 */
-	private void addLabel(Composite parent, String text) {
-		Label label = new Label(parent, SWT.WRAP);
-		label.setText(text);
-	}
+    /**
+     * Creates the configuration elements for a string configuration. Adds them to a parent element.
+     *
+     * @param parent
+     *            The parent.
+     * @param config
+     *            The string configuration.
+     */
+    private void createStringConfigField(Composite parent, final StringConfiguration config) {
+        addLabel(parent, config.getLabel());
+        final StyledText text = new StyledText(parent, SWT.V_SCROLL);
+        text.setLeftMargin(2);
+        text.setRightMargin(2);
+        text.setTopMargin(2);
+        text.setBottomMargin(2);
+        text.setWordWrap(true);
+        text.addModifyListener(new ModifyListener() {
 
-	/**
-	 * Adds a Tool-Tip explanation to a given parent composite.
-	 * 
-	 * @param parent The parent.
-	 * @param explanation The textual explanation.
-	 */
-	private void addExplanation(Composite parent, String explanation) {
-		Label explanationLabel = new Label(parent, SWT.NONE);
-		explanationLabel.setImage(PlatformUI.getWorkbench().getSharedImages()
-				.getImage(ISharedImages.IMG_LCL_LINKTO_HELP));
-		explanationLabel.setToolTipText(explanation);
+            @Override
+            public void modifyText(ModifyEvent arg0) {
+                config.setCurrentValue(text.getText());
+            }
+        });
+        String defaultText = config.getDefaultValue();
+        text.setText(defaultText);
+        if (defaultText.length() > 30) {
+            text.setLayoutData(new RowData(150, 80));
+        } else {
+            text.setLayoutData(new RowData(150, 16));
+        }
+        addExplanation(parent, config.getExplanation());
+    }
 
-		if (explanation == null || explanation.length() == 0) {
-			explanationLabel.setVisible(false);
-		}
-	}
+    /**
+     * Creates the configuration elements for a boolean configuration. Adds them to a parent
+     * element.
+     *
+     * @param parent
+     *            The parent.
+     * @param config
+     *            The boolean configuration.
+     */
+    private void createBooleanConfigField(Composite parent, final BooleanConfiguration config) {
+        final Button checkButton = new Button(parent, SWT.CHECK);
+        checkButton.setSelection(config.getDefaultValue());
+        checkButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                super.widgetSelected(e);
+                config.setCurrentValue(checkButton.getSelection());
+            }
+        });
+        addLabel(parent, config.getLabel());
+        addExplanation(parent, config.getExplanation());
+    }
+
+    /**
+     * Adds a label with given text to a composite.
+     *
+     * @param parent
+     *            The composite to add the label to.
+     * @param text
+     *            The text of the label.
+     */
+    private void addLabel(Composite parent, String text) {
+        Label label = new Label(parent, SWT.WRAP);
+        label.setText(text);
+    }
+
+    /**
+     * Adds a Tool-Tip explanation to a given parent composite.
+     *
+     * @param parent
+     *            The parent.
+     * @param explanation
+     *            The textual explanation.
+     */
+    private void addExplanation(Composite parent, String explanation) {
+        Label explanationLabel = new Label(parent, SWT.NONE);
+        explanationLabel.setImage(PlatformUI.getWorkbench().getSharedImages()
+                .getImage(ISharedImages.IMG_LCL_LINKTO_HELP));
+        explanationLabel.setToolTipText(explanation);
+
+        if (explanation == null || explanation.length() == 0) {
+            explanationLabel.setVisible(false);
+        }
+    }
 }
