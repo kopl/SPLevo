fe17c3eeb99bfdf01e20fb0d73b7dfc8dd33f5f3
BenjaminKlatt <klatt@fzi.de>
2014-03-09 06:58:26 +0000

    comments and formatting

--- VPM/org.splevo.vpm/src/org/splevo/vpm/VPMExtension.java
+++ VPM/org.splevo.vpm/src/org/splevo/vpm/VPMExtension.java
@@ -1,16 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.splevo.vpm;
 
 /**
- * An extension of the vpm model providing capabilities to manage its life cycle. 
+ * An extension of the vpm model providing capabilities to manage its life cycle.
  */
 public interface VPMExtension {
 
     /** Initialize the variation point model extension. */
     void init();
-    
-    /** 
+
+    /**
      * Get the name of the extension.
-     * @return The name of the extension which should be unique. 
+     * @return The name of the extension which should be unique.
      */
     String getName();
 }
