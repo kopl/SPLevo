99b7355443220e9cefc592c502a40aff54c07450
BenjaminKlatt <klatt@fzi.de>
2014-03-11 08:59:54 +0000

    SPLEVO-221 Refinement Detection: Performance and scalability

--- VPMAnalyzer/org.splevo.vpm.analyzer/src/org/splevo/vpm/analyzer/refinement/BasicDetectionRule.java
+++ VPMAnalyzer/org.splevo.vpm.analyzer/src/org/splevo/vpm/analyzer/refinement/BasicDetectionRule.java
@@ -11,10 +11,11 @@
  *******************************************************************************/
 package org.splevo.vpm.analyzer.refinement;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 import org.splevo.vpm.analyzer.graph.RelationshipEdge;
@@ -24,22 +25,23 @@ import org.splevo.vpm.refinement.RefinementFactory;
 import org.splevo.vpm.refinement.RefinementType;
 import org.splevo.vpm.variability.VariationPoint;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
 /**
  * A detection rule to derive refinement recommendations from a set of VPMRelationshipEdge labels.
- *
- * @author Benjamin Klatt
- *
  */
 public class BasicDetectionRule implements DetectionRule {
+
+    private Logger logger = Logger.getLogger(BasicDetectionRule.class);
 
     /** The edges that must be match. */
     private List<String> edgeLabels = null;
 
     /** The type of refinement to create in case of a match. */
     private RefinementType refinementType = null;
-
-    /** A cache for the edges already processed. */
-    private final List<Edge> processedEdges = new ArrayList<Edge>();
 
     /**
      * Constructor requiring to set the edge labels to match and the resulting refinement type.
@@ -54,105 +56,174 @@ public class BasicDetectionRule implements DetectionRule {
         this.refinementType = refinementType;
     }
 
+    /**
+     * <p>
+     * Detect refinements based on analyzing the edge labels of the relationships stored in a VPM
+     * graph.
+     * </p>
+     * <p>
+     * For each subgraph identified by connected edges with the labels configured for the detection
+     * rule, a refinement of the configured type is created.
+     * </p>
+     *
+     * The detection algorithm checks all edges if they match all search edge labels. Not matching
+     * ones are ignored.<br>
+     * In case of a match:
+     * <ul>
+     * <li>Get the nodes of edges</li>
+     * <li>Check if they are already contained in a subgraph</li>
+     * <li>If the nodes are:
+     * <ul>
+     * <li>In the same subgraph: do nothing</li>
+     * <li>In different subgraphs: merge the subgraphs</li>
+     * <li>Only one is in a subgraph: add the other node to the same</li>
+     * <li>None is in a subgraph: Create a new subgraph</li>
+     * </ul>
+     * </li>
+     * <li>Finally, create a refinement per subgraph</li>
+     * </ul>
+     *
+     * For an efficient processing three indexes are used:
+     * <ol>
+     * <li>Node to subgraph: For fast lookup of subgraph membership of the edge's nodes</li>
+     * <li>Subgraph to node: For fast subgraph merging</li>
+     * <li>Subgraph to edge: For fast refinement creation per subgraph</li>
+     * </ol>
+     * Building refinement per subgraph is done based on the edges to enable further interpretation
+     * of relationship information stored to the edges. For example: Which terms lead to a semantic
+     * relationship.
+     *
+     * DesignDecision: Refinement creation based on edges to access further relationship information
+     * stored with edges
+     *
+     * DesignDecision: Flat edge based subgraph analysis using indexes instead of deep search to
+     * improve performance.
+     *
+     * DesignDecision: No recursive deep search as this has lead to stack overflows in the past due
+     * to to large graphs
+     *
+     *
+     * @param vpmGraph
+     *            The graph to analyze.
+     * @return The detected refinements.
+     */
     @Override
     public List<Refinement> detect(final VPMGraph vpmGraph) {
 
-        final List<Refinement> refinements = new ArrayList<Refinement>();
+        List<Refinement> refinements = Lists.newArrayList();
 
-        for (final Edge edge : vpmGraph.getEachEdge()) {
+        Map<Node, Integer> nodeSubgraphIndex = Maps.newLinkedHashMap();
+        ArrayListMultimap<Integer, Node> subgraphNodeIndex = ArrayListMultimap.create();
+        ArrayListMultimap<Integer, RelationshipEdge> subgraphEdgeIndex = ArrayListMultimap.create();
 
-            if (this.processedEdges.contains(edge)) {
-                continue;
+        int nextSubgraphId = 0;
+
+        for (Edge edge : vpmGraph.getEachEdge()) {
+            RelationshipEdge relationshipEdge = (RelationshipEdge) edge;
+            if (match(relationshipEdge)) {
+
+                Node sourceNode = relationshipEdge.getSourceNode();
+                Node targetNode = relationshipEdge.getTargetNode();
+
+                Integer sourceNodeSubgraphId = nodeSubgraphIndex.get(sourceNode);
+                Integer targetNodeSubgraphId = nodeSubgraphIndex.get(targetNode);
+
+                // decide about subgraph
+                Integer subgraphId;
+                if (sourceNodeSubgraphId != null && targetNodeSubgraphId != null) {
+                    if (!sourceNodeSubgraphId.equals(targetNodeSubgraphId)) {
+                        mergeSubGraphNodes(nodeSubgraphIndex, subgraphNodeIndex, sourceNodeSubgraphId,
+                                targetNodeSubgraphId);
+                        mergeSubgraphEdges(subgraphEdgeIndex, sourceNodeSubgraphId, targetNodeSubgraphId);
+                    }
+                    continue;
+
+                } else if (sourceNodeSubgraphId != null) {
+                    subgraphId = sourceNodeSubgraphId;
+
+                } else if (targetNodeSubgraphId != null) {
+                    subgraphId = targetNodeSubgraphId;
+
+                } else {
+                    subgraphId = new Integer(nextSubgraphId++);
+                }
+
+                // index
+                nodeSubgraphIndex.put(sourceNode, subgraphId);
+                nodeSubgraphIndex.put(targetNode, subgraphId);
+                subgraphNodeIndex.put(subgraphId, sourceNode);
+                subgraphNodeIndex.put(subgraphId, targetNode);
+                subgraphEdgeIndex.put(subgraphId, relationshipEdge);
             }
+        }
 
-            List<RelationshipEdge> subgraphEdges = new ArrayList<RelationshipEdge>();
-            detectSubgraph((RelationshipEdge) edge, subgraphEdges);
+        logger.info("Subgraph Count (NodeIndex): " + subgraphNodeIndex.keySet().size());
+        logger.info("Subgraph Count (EdgeIndex): " + subgraphEdgeIndex.keySet().size());
 
-            // build refinement
-            if (subgraphEdges.size() > 0) {
-                Refinement refinement = buildRefinementForEdges(subgraphEdges);
-                refinements.add(refinement);
-            }
-
+        for (Integer subgraphId : subgraphEdgeIndex.keySet()) {
+            List<RelationshipEdge> subgraphEdges = subgraphEdgeIndex.get(subgraphId);
+            Refinement refinement = buildRefinementForEdges(subgraphEdges);
+            refinements.add(refinement);
         }
 
         return refinements;
     }
 
-    /**
-     * Build the subgraph of edges matching the detection rule criteria. It also takes care that
-     * processed edges are registered to not been processed twice. Especially in an undirected
-     * graph, this would lead into problems.
-     *
-     * @param edge
-     *            The edge to check.
-     * @param subgraphEdges
-     *            The edge list to put matched edges into.
-     */
-    private void detectSubgraph(RelationshipEdge edge, List<RelationshipEdge> subgraphEdges) {
-        processedEdges.add(edge);
-        if (match(edge)) {
-            subgraphEdges.add(edge);
-
-            // recursively process subsequent edges
-            for (RelationshipEdge subsequentEdge : subsequentEdges(edge)) {
-                if (processedEdges.contains(subsequentEdge)) {
-                    continue;
-                }
-                detectSubgraph(subsequentEdge, subgraphEdges);
-            }
-        }
+    private void mergeSubgraphEdges(ArrayListMultimap<Integer, RelationshipEdge> subgraphEdgeIndex,
+            Integer sourceNodeSubGraphId, Integer targetNodeSubGraphId) {
+        List<RelationshipEdge> edgesToMerge = subgraphEdgeIndex.get(targetNodeSubGraphId);
+        subgraphEdgeIndex.putAll(sourceNodeSubGraphId, edgesToMerge);
+        subgraphEdgeIndex.removeAll(targetNodeSubGraphId);
     }
 
-    /**
-     * Get the subsequent edges for the source and target nodes of the edge.
-     *
-     * @param edge
-     *            The edge to get the subsequent edges for.
-     * @return The list of edges on the next level.
-     */
-    private List<RelationshipEdge> subsequentEdges(RelationshipEdge edge) {
-        List<RelationshipEdge> edges = new ArrayList<RelationshipEdge>();
-        Collection<RelationshipEdge> sourceEdges = edge.getSourceNode().getEdgeSet();
-        Collection<RelationshipEdge> targetEdges = edge.getTargetNode().getEdgeSet();
-
-        edges.addAll(sourceEdges);
-        edges.addAll(targetEdges);
-
-        return edges;
+    private void mergeSubGraphNodes(Map<Node, Integer> nodeSubgraphIndex,
+            ArrayListMultimap<Integer, Node> subgraphNodeIndex, Integer sourceNodeSubGraphId,
+            Integer targetNodeSubGraphId) {
+        List<Node> nodesToMove = subgraphNodeIndex.get(targetNodeSubGraphId);
+        for (Node node : nodesToMove) {
+            nodeSubgraphIndex.put(node, sourceNodeSubGraphId);
+        }
+        subgraphNodeIndex.putAll(sourceNodeSubGraphId, nodesToMove);
+        subgraphNodeIndex.removeAll(targetNodeSubGraphId);
     }
 
     /**
      * Build a refinement for an edge.
      *
-     * @param edges
-     *            The edges to build the refinement for.
-     * @return The refinemnt.
+     * @param subgraphEdges
+     *            The edges connecting the subgraph to build a refinement for.
+     * @return The prepared refinement.
      */
-    private Refinement buildRefinementForEdges(List<RelationshipEdge> edges) {
+    private Refinement buildRefinementForEdges(List<RelationshipEdge> subgraphEdges) {
 
         Refinement refinement = RefinementFactory.eINSTANCE.createRefinement();
         refinement.setType(this.getRefinementType());
 
-        for (RelationshipEdge edge : edges) {
-            addVariationPoint(refinement, edge.getSourceNode());
-            addVariationPoint(refinement, edge.getTargetNode());
+        Set<Node> nodes = Sets.newLinkedHashSet();
+
+        for (RelationshipEdge edge : subgraphEdges) {
+            nodes.add(edge.getSourceNode());
+            nodes.add(edge.getTargetNode());
         }
 
-        // TODO: handle analyzer configuration for refinement.
-        StringBuilder source = new StringBuilder();
-        source.append("Detection Rule: ");
-        source.append(edgeLabels.toString());
-        source.append("\n");
+        for (Node node : nodes) {
+            addVariationPoint(refinement, node);
+        }
 
-        refinement.setSource("Detection Rule " + edgeLabels.toString());
+        refinement.setSource(String.format("Detection Rule: ", edgeLabels.toString()));
 
         return refinement;
     }
 
     /**
-     * Add the variation point of a node to the refinement. It is ensured, that no vp is added
-     * twice.
+     * Add the variation point of a node to the refinement.
+     *
+     * DesignDecision: No duplicate checking to improve performance
+     *
+     * Note: Adding two nodes with the same VariationPoint would lead to assign the variation point
+     * twice to the refinement. There is no duplicate checking done here, because this has shown to
+     * lead to a significant performance loss at this point.
+     *
      *
      * @param refinement
      *            The refinement to add the vp to.
@@ -161,9 +232,7 @@ public class BasicDetectionRule implements DetectionRule {
      */
     private void addVariationPoint(Refinement refinement, Node node) {
         VariationPoint vp = node.getAttribute(VPMGraph.VARIATIONPOINT, VariationPoint.class);
-        if (!refinement.getVariationPoints().contains(vp)) {
-            refinement.getVariationPoints().add(vp);
-        }
+        refinement.getVariationPoints().add(vp);
     }
 
     /**
