8441b7c26c7a2c2778cea79a51257c4eca413b7f
BenjaminKlatt <klatt@fzi.de>
2014-03-22 20:14:07 +0000

    minor code cleanup

--- VPMRefinement/org.splevo.vpm.refinement.edit/src/org/splevo/vpm/refinement/provider/RefinementItemProvider.java
+++ VPMRefinement/org.splevo.vpm.refinement.edit/src/org/splevo/vpm/refinement/provider/RefinementItemProvider.java
@@ -1,7 +1,15 @@
-/**
- */
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt
+ *******************************************************************************/
 package org.splevo.vpm.refinement.provider;
-
 
 import java.util.Collection;
 import java.util.List;
@@ -24,37 +32,31 @@ import org.splevo.vpm.refinement.RefinementPackage;
 import org.splevo.vpm.refinement.RefinementType;
 
 /**
- * This is the item provider adapter for a {@link org.splevo.vpm.refinement.Refinement} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the item provider adapter for a {@link org.splevo.vpm.refinement.Refinement} object. <!--
+ * begin-user-doc --> <!-- end-user-doc -->
+ *
  * @generated
  */
-public class RefinementItemProvider
-	extends ItemProviderAdapter
-	implements
-		IEditingDomainItemProvider,
-		IStructuredItemContentProvider,
-		ITreeItemContentProvider,
-		IItemLabelProvider,
-		IItemPropertySource {
-	/**
-     * This constructs an instance from a factory and a notifier.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+public class RefinementItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider,
+        IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+    /**
+     * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
      * @generated
      */
-	public RefinementItemProvider(AdapterFactory adapterFactory) {
+    public RefinementItemProvider(AdapterFactory adapterFactory) {
         super(adapterFactory);
     }
 
-	/**
-     * This returns the property descriptors for the adapted class.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
      * @generated
      */
-	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+    @Override
+    public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
         if (itemPropertyDescriptors == null) {
             super.getPropertyDescriptors(object);
 
@@ -65,147 +67,131 @@ public class RefinementItemProvider
         return itemPropertyDescriptors;
     }
 
-	/**
-     * This adds a property descriptor for the Type feature.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * This adds a property descriptor for the Type feature. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
      * @generated
      */
-	protected void addTypePropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_Refinement_type_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_Refinement_type_feature", "_UI_Refinement_type"),
-                 RefinementPackage.Literals.REFINEMENT__TYPE,
-                 true,
-                 false,
-                 false,
-                 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-                 null,
-                 null));
-    }
-
-	/**
-     * This adds a property descriptor for the Variation Points feature.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-     * @generated
-     */
-	protected void addVariationPointsPropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_Refinement_variationPoints_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_Refinement_variationPoints_feature", "_UI_Refinement_type"),
-                 RefinementPackage.Literals.REFINEMENT__VARIATION_POINTS,
-                 true,
-                 false,
-                 true,
-                 null,
-                 null,
-                 null));
-    }
-
-	/**
-     * This adds a property descriptor for the Source feature.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    protected void addSourcePropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_Refinement_source_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_Refinement_source_feature", "_UI_Refinement_type"),
-                 RefinementPackage.Literals.REFINEMENT__SOURCE,
-                 true,
-                 false,
-                 false,
-                 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-                 null,
-                 null));
+    protected void addTypePropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(
+                ((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(), getResourceLocator(),
+                getString("_UI_Refinement_type_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_Refinement_type_feature", "_UI_Refinement_type"),
+                RefinementPackage.Literals.REFINEMENT__TYPE, true, false, false,
+                ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
     }
 
     /**
-     * This returns the label text for the adapted class.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * This adds a property descriptor for the Variation Points feature. <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     *
      * @generated
      */
-	@Override
-	public String getText(Object object) {
-        RefinementType labelValue = ((Refinement)object).getType();
-        String label = labelValue == null ? null : labelValue.toString();
-        return label == null || label.length() == 0 ?
-            getString("_UI_Refinement_type") :
-            getString("_UI_Refinement_type") + " " + label;
+    protected void addVariationPointsPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(
+                ((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+                getResourceLocator(),
+                getString("_UI_Refinement_variationPoints_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_Refinement_variationPoints_feature",
+                        "_UI_Refinement_type"), RefinementPackage.Literals.REFINEMENT__VARIATION_POINTS, true, false,
+                true, null, null, null));
     }
 
-	/**
-	 * This returns an icon for the variant element.
-	 * Depending on the leading flag, the icon is changed.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated not
-	 */
-	@Override
-	public Object getImage(Object object) {
-		if(object instanceof Refinement){
-			if(((Refinement) object).getType().equals(RefinementType.GROUPING)){
-				return overlayImage(object, getResourceLocator().getImage("full/obj16/Grouping"));
-			} else {
-				return overlayImage(object, getResourceLocator().getImage("full/obj16/Merge"));
-			}
-		} else {
-			return super.getImage(object);
-		}
-	}
-
-	/**
-     * This handles model notifications by calling {@link #updateChildren} to update any cached
-     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * This adds a property descriptor for the Source feature. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
      * @generated
      */
-	@Override
-	public void notifyChanged(Notification notification) {
+    protected void addSourcePropertyDescriptor(Object object) {
+        itemPropertyDescriptors
+                .add(createItemPropertyDescriptor(
+                        ((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+                        getResourceLocator(),
+                        getString("_UI_Refinement_source_feature"),
+                        getString("_UI_PropertyDescriptor_description", "_UI_Refinement_source_feature",
+                                "_UI_Refinement_type"), RefinementPackage.Literals.REFINEMENT__SOURCE, true, false,
+                        false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+    }
+
+    /**
+     * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
+     * -->
+     *
+     * @generated
+     */
+    @Override
+    public String getText(Object object) {
+        RefinementType labelValue = ((Refinement) object).getType();
+        String label = labelValue == null ? null : labelValue.toString();
+        return label == null || label.length() == 0 ? getString("_UI_Refinement_type")
+                : getString("_UI_Refinement_type") + " " + label;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     *
+     * This returns an icon for the refinement depending on the type of refinement.
+     *
+     * {@inheritDoc}
+     *
+     * <!-- end-user-doc -->
+     *
+     * @generated not
+     */
+    @Override
+    public Object getImage(Object object) {
+        if (object instanceof Refinement) {
+            if (((Refinement) object).getType().equals(RefinementType.GROUPING)) {
+                return overlayImage(object, getResourceLocator().getImage("full/obj16/Grouping"));
+            } else {
+                return overlayImage(object, getResourceLocator().getImage("full/obj16/Merge"));
+            }
+        } else {
+            return super.getImage(object);
+        }
+    }
+
+    /**
+     * This handles model notifications by calling {@link #updateChildren} to update any cached
+     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}
+     * . <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    public void notifyChanged(Notification notification) {
         updateChildren(notification);
 
         switch (notification.getFeatureID(Refinement.class)) {
-            case RefinementPackage.REFINEMENT__TYPE:
-            case RefinementPackage.REFINEMENT__SOURCE:
-                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-                return;
+        case RefinementPackage.REFINEMENT__TYPE:
+        case RefinementPackage.REFINEMENT__SOURCE:
+            fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+            return;
         }
         super.notifyChanged(notification);
     }
 
-	/**
-     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-     * that can be created under this object.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that
+     * can be created under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
      * @generated
      */
-	@Override
-	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+    @Override
+    protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
         super.collectNewChildDescriptors(newChildDescriptors, object);
     }
 
-	/**
-     * Return the resource locator for this item provider's resources.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * Return the resource locator for this item provider's resources. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
      * @generated
      */
-	@Override
-	public ResourceLocator getResourceLocator() {
+    @Override
+    public ResourceLocator getResourceLocator() {
         return RefinementEditPlugin.INSTANCE;
     }
 
---
+++ VPMRefinement/org.splevo.vpm.refinement.edit/src/org/splevo/vpm/refinement/provider/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * EMF Provider package for the refinement model.
+ */
+package org.splevo.vpm.refinement.provider;
+
