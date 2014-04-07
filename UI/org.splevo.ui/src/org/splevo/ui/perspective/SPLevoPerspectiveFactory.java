e1f28bb12dc65dbbd11e0c4dd952618e96d1469c
BenjaminKlatt <klatt@fzi.de>
2014-04-07 18:55:41 +0000

    SPLEVO-252 SPLevo Perspective

---
+++ UI/org.splevo.ui/src/org/splevo/ui/perspective/SPLevoPerspectiveFactory.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt
+ *******************************************************************************/
+package org.splevo.ui.perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * A factory for the SPLevo default perspective.
+ */
+public class SPLevoPerspectiveFactory implements IPerspectiveFactory {
+
+    @Override
+    public void createInitialLayout(IPageLayout layout) {
+    }
+
+}
