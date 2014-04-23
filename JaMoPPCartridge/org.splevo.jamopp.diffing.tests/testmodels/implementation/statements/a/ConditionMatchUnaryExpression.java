35b20ec17874749f15b747299c4fef46288b9753
BenjaminKlatt <klatt@fzi.de>
2014-04-23 15:35:41 +0000

    SPLEVO-280 Condition Expression Matching Incomplete - Added ConditionalOrExpression comparison - Tests for unary expression support

---
+++ JaMoPPCartridge/org.splevo.jamopp.diffing.tests/testmodels/implementation/statements/a/ConditionMatchUnaryExpression.java
@@ -0,0 +1,18 @@
+package org.splevo.tests.statements;
+
+import java.util.List;
+
+public class ConditionMatchUnaryExpression {
+
+    public String doSth(List<String> a) {
+
+        if(!(String.CASE_INSENSITIVE_ORDER.equals(a.iterator().next()))
+                && a.size() == ("test".length() + 1)) {
+            return "existing conditional";
+        }
+
+        for (int i = 0; i < 10 && i >= 0; i++) {
+            System.out.println(""+i);
+        }
+    }
+}
