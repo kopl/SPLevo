a92cc673ab4dce6fa496adf4ff77014a5c25fc2c
BenjaminKlatt <klatt@fzi.de>
2014-04-24 22:05:16 +0000

    SPLEVO-282 Support for Constructor Changes

--- JaMoPPCartridge/org.splevo.jamopp.diffing.editor/src-gen/org/splevo/jamopp/diffing/jamoppdiff/presentation/JaMoPPDiffEditorPlugin.java
+++ JaMoPPCartridge/org.splevo.jamopp.diffing.editor/src-gen/org/splevo/jamopp/diffing/jamoppdiff/presentation/JaMoPPDiffEditorPlugin.java
@@ -32,77 +32,76 @@ import org.emftext.language.java.annotations.provider.JavaEditPlugin;
  * @generated
  */
 public final class JaMoPPDiffEditorPlugin extends EMFPlugin {
-	/**
-	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static final JaMoPPDiffEditorPlugin INSTANCE = new JaMoPPDiffEditorPlugin();
+    /**
+     * Keep track of the singleton.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public static final JaMoPPDiffEditorPlugin INSTANCE = new JaMoPPDiffEditorPlugin();
 
-	/**
-	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	private static Implementation plugin;
+    /**
+     * Keep track of the singleton.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private static Implementation plugin;
 
-	/**
-	 * Create the instance.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public JaMoPPDiffEditorPlugin() {
-		super(new ResourceLocator[] { EMFCompareEditPlugin.INSTANCE,
-				EcoreEditPlugin.INSTANCE, JavaEditPlugin.INSTANCE,
-				LayoutEditPlugin.INSTANCE, });
-	}
+    /**
+     * Create the instance.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public JaMoPPDiffEditorPlugin() {
+        super(new ResourceLocator[] { EMFCompareEditPlugin.INSTANCE, EcoreEditPlugin.INSTANCE, JavaEditPlugin.INSTANCE,
+                LayoutEditPlugin.INSTANCE, });
+    }
 
-	/**
-	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return the singleton instance.
-	 * @generated
-	 */
-	@Override
-	public ResourceLocator getPluginResourceLocator() {
-		return plugin;
-	}
+    /**
+     * Returns the singleton instance of the Eclipse plugin.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @return the singleton instance.
+     * @generated
+     */
+    @Override
+    public ResourceLocator getPluginResourceLocator() {
+        return plugin;
+    }
 
-	/**
-	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return the singleton instance.
-	 * @generated
-	 */
-	public static Implementation getPlugin() {
-		return plugin;
-	}
+    /**
+     * Returns the singleton instance of the Eclipse plugin.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @return the singleton instance.
+     * @generated
+     */
+    public static Implementation getPlugin() {
+        return plugin;
+    }
 
-	/**
-	 * The actual implementation of the Eclipse <b>Plugin</b>.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static class Implementation extends EclipseUIPlugin {
-		/**
-		 * Creates an instance.
-		 * <!-- begin-user-doc -->
-		 * <!-- end-user-doc -->
-		 * @generated
-		 */
-		public Implementation() {
-			super();
+    /**
+     * The actual implementation of the Eclipse <b>Plugin</b>.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public static class Implementation extends EclipseUIPlugin {
+        /**
+         * Creates an instance.
+         * <!-- begin-user-doc -->
+         * <!-- end-user-doc -->
+         * @generated
+         */
+        public Implementation() {
+            super();
 
-			// Remember the static instance.
-			//
-			plugin = this;
-		}
-	}
+            // Remember the static instance.
+            //
+            plugin = this;
+        }
+    }
 
 }
