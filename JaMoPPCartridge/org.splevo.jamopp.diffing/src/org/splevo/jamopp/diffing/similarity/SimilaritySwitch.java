82bebc62db58b895c0dc76386b7a32e6b1b0db89
BenjaminKlatt <klatt@fzi.de>
2014-04-25 05:56:09 +0000

    SPLEVO-283 Support Instanceof Condition Match

--- JaMoPPCartridge/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/SimilaritySwitch.java
+++ JaMoPPCartridge/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/SimilaritySwitch.java
@@ -43,6 +43,8 @@ import org.emftext.language.java.expressions.ConditionalOrExpressionChild;
 import org.emftext.language.java.expressions.EqualityExpression;
 import org.emftext.language.java.expressions.EqualityExpressionChild;
 import org.emftext.language.java.expressions.Expression;
+import org.emftext.language.java.expressions.InstanceOfExpression;
+import org.emftext.language.java.expressions.InstanceOfExpressionChild;
 import org.emftext.language.java.expressions.RelationExpression;
 import org.emftext.language.java.expressions.RelationExpressionChild;
 import org.emftext.language.java.expressions.UnaryExpression;
@@ -102,8 +104,11 @@ import org.emftext.language.java.statements.SynchronizedBlock;
 import org.emftext.language.java.statements.Throw;
 import org.emftext.language.java.statements.util.StatementsSwitch;
 import org.emftext.language.java.types.ClassifierReference;
+import org.emftext.language.java.types.Int;
 import org.emftext.language.java.types.NamespaceClassifierReference;
+import org.emftext.language.java.types.PrimitiveType;
 import org.emftext.language.java.types.Type;
+import org.emftext.language.java.types.TypeReference;
 import org.emftext.language.java.types.util.TypesSwitch;
 import org.emftext.language.java.variables.AdditionalLocalVariable;
 import org.emftext.language.java.variables.Variable;
@@ -512,6 +517,25 @@ public class SimilaritySwitch extends ComposedSwitch<Boolean> {
             // check expression equality
             UnaryExpressionChild child1 = exp1.getChild();
             UnaryExpressionChild child2 = exp2.getChild();
+            return similarityChecker.isSimilar(child1, child2);
+        }
+
+        @Override
+        public Boolean caseInstanceOfExpression(InstanceOfExpression exp1) {
+
+            InstanceOfExpression exp2 = (InstanceOfExpression) compareElement;
+
+            // check type equality
+            TypeReference typeReference1 = exp1.getTypeReference();
+            TypeReference typeReference2 = exp2.getTypeReference();
+            Boolean typeSimilarity = similarityChecker.isSimilar(typeReference1, typeReference2);
+            if (typeSimilarity == Boolean.FALSE) {
+                return Boolean.FALSE;
+            }
+
+            // check expression equality
+            InstanceOfExpressionChild child1 = exp1.getChild();
+            InstanceOfExpressionChild child2 = exp2.getChild();
             return similarityChecker.isSimilar(child1, child2);
         }
 
@@ -1431,13 +1455,49 @@ public class SimilaritySwitch extends ComposedSwitch<Boolean> {
         }
 
         @Override
+        public Boolean caseTypeReference(TypeReference ref1) {
+
+            TypeReference ref2 = (TypeReference) compareElement;
+
+            Boolean targetSimilarity = similarityChecker.isSimilar(ref1.getTarget(), ref2.getTarget());
+            if (targetSimilarity == Boolean.FALSE) {
+                return Boolean.FALSE;
+            }
+
+            return Boolean.TRUE;
+        }
+
+        @Override
         public Boolean caseNamespaceClassifierReference(NamespaceClassifierReference ref1) {
 
             NamespaceClassifierReference ref2 = (NamespaceClassifierReference) compareElement;
 
             String namespace1 = Strings.nullToEmpty(ref1.getNamespacesAsString());
             String namespace2 = Strings.nullToEmpty(ref2.getNamespacesAsString());
-            return (namespace1.equals(namespace2));
+            if (!namespace1.equals(namespace2)) {
+                return Boolean.FALSE;
+            }
+
+            ClassifierReference pureRef1 = ref1.getPureClassifierReference();
+            ClassifierReference pureRef2 = ref2.getPureClassifierReference();
+
+            return similarityChecker.isSimilar(pureRef1, pureRef2);
+        }
+
+        /**
+         * Primitive types are always similar as their class similarity is assumed before by the
+         * outer {@link SimilarityChecker}.
+         *
+         * Note: The fall back to the default case is not sufficient here, as the common
+         * TypeReference case would be used before, leading to a loop.
+         *
+         * @param type
+         *            The primitive type object.
+         * @return TRUE
+         */
+        @Override
+        public Boolean casePrimitiveType(PrimitiveType type) {
+            return Boolean.TRUE;
         }
 
         /**
