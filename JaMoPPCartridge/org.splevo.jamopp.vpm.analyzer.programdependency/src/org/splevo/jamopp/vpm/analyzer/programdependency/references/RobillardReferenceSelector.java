7c2966757e047c8ab3964bc500568a0f2d83c66d
BenjaminKlatt <klatt@fzi.de>
2014-05-05 22:41:35 +0000

    SPLEVO-265 JaMoPP Program Dependency Analyzer: Robillard References Mode Implemented call, read, and write reference logic

--- JaMoPPCartridge/org.splevo.jamopp.vpm.analyzer.programdependency/src/org/splevo/jamopp/vpm/analyzer/programdependency/references/RobillardReferenceSelector.java
+++ JaMoPPCartridge/org.splevo.jamopp.vpm.analyzer.programdependency/src/org/splevo/jamopp/vpm/analyzer/programdependency/references/RobillardReferenceSelector.java
@@ -11,11 +11,13 @@
  *******************************************************************************/
 package org.splevo.jamopp.vpm.analyzer.programdependency.references;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.emftext.language.java.commons.Commentable;
 import org.splevo.jamopp.util.JaMoPPElementUtil;
+import org.splevo.vpm.analyzer.VPMAnalyzerUtil;
 
 /**
  * A reference selector implementing the references defined by:<br>
@@ -23,14 +25,62 @@ import org.splevo.jamopp.util.JaMoPPElementUtil;
  * "Concern graphs: finding and describing concerns using structural program dependencies."<br>
  * In Proceedings of the 24th International Conference on Software Engineering. ICSE 2002 (pp.
  * 406–416).<br>
+ *
+ * It considers the following reference types, while interpreting the "body of a method", also as
+ * single statements implementing a variant of a variation point.
+ * <table>
+ *
+ * <tr>
+ * <td>(calls,m1,m2)</td>
+ * <td>The body of method m1 contains a call that can bind (statically or dynamically) to m2.</td>
+ * </tr>
+ * <tr>
+ * <td>(reads,m,f)</td>
+ * <td>The body of method m contains an instruction that reads (uses) a value from field f.</td>
+ * </tr>
+ * <tr>
+ * <td>(writes,m,f)</td>
+ * <td>The body of method contains an instruction that writes (defines) a value to field f.</td>
+ * </tr>
+ * <tr>
+ * <td>(checks,m,c)</td>
+ * <td>The body of method m checks the class of an object, or casts an object, to c.</td>
+ * </tr>
+ * <tr>
+ * <td>(creates,m,c)</td>
+ * <td>The body of method m creates an object of class c.</td>
+ * </tr>
+ * <tr>
+ * <td>(declares,c,{m|f})</td>
+ * <td>Class c declares method m or declares field f.</td>
+ * </tr>
+ * <tr>
+ * <td>(superclass,c1,c2)</td>
+ * <td>Class c1 is the superclass of c2.</td>
+ * </tr>
+ * </table>
  */
 public class RobillardReferenceSelector implements ReferenceSelector {
 
     private static Logger logger = Logger.getLogger(RobillardReferenceSelector.class);
 
+    private RobillardReferenceSelectorSwitch selectorSwitch = new RobillardReferenceSelectorSwitch();
+
     @Override
     public List<Commentable> getReferencedElements(Commentable commentable) {
-        return new DefaultReferenceSelector().getReferencedElements(commentable);
+        if (VPMAnalyzerUtil.isNullOrProxy(commentable)) {
+            return new ArrayList<Commentable>();
+        }
+
+        List<Commentable> referencedElements = selectorSwitch.doSwitch(commentable);
+
+        for (Commentable element : referencedElements) {
+            if (VPMAnalyzerUtil.isNullOrProxy(element)) {
+                referencedElements.remove(element);
+            }
+        }
+
+        return referencedElements;
     }
 
     /**
@@ -50,6 +100,8 @@ public class RobillardReferenceSelector implements ReferenceSelector {
         if (JaMoPPElementUtil.isParentOf(source2, target)) {
             return false;
         }
+
+
 
         logger.info("Robillard Reference Selector invoked");
 
--- JaMoPPCartridge/org.splevo.jamopp.vpm.analyzer.programdependency/src/org/splevo/jamopp/vpm/analyzer/programdependency/references/RobillardReferenceSelectorSwitch.java
+++ JaMoPPCartridge/org.splevo.jamopp.vpm.analyzer.programdependency/src/org/splevo/jamopp/vpm/analyzer/programdependency/references/RobillardReferenceSelectorSwitch.java
@@ -15,41 +15,114 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.ComposedSwitch;
+import org.emftext.language.java.classifiers.Class;
+import org.emftext.language.java.classifiers.util.ClassifiersSwitch;
 import org.emftext.language.java.commons.Commentable;
-import org.emftext.language.java.imports.Import;
-import org.emftext.language.java.imports.util.ImportsSwitch;
+import org.emftext.language.java.expressions.AssignmentExpression;
+import org.emftext.language.java.expressions.CastExpression;
+import org.emftext.language.java.expressions.Expression;
+import org.emftext.language.java.expressions.util.ExpressionsSwitch;
+import org.emftext.language.java.instantiations.NewConstructorCall;
+import org.emftext.language.java.instantiations.util.InstantiationsSwitch;
 import org.emftext.language.java.members.ClassMethod;
+import org.emftext.language.java.members.Constructor;
+import org.emftext.language.java.members.Field;
+import org.emftext.language.java.members.Member;
 import org.emftext.language.java.members.Method;
 import org.emftext.language.java.members.util.MembersSwitch;
+import org.emftext.language.java.references.IdentifierReference;
+import org.emftext.language.java.references.MethodCall;
+import org.emftext.language.java.references.ReferenceableElement;
+import org.emftext.language.java.references.util.ReferencesSwitch;
+import org.emftext.language.java.statements.Condition;
+import org.emftext.language.java.statements.ExpressionStatement;
+import org.emftext.language.java.statements.LocalVariableStatement;
+import org.emftext.language.java.statements.Return;
 import org.emftext.language.java.statements.Statement;
+import org.emftext.language.java.statements.StatementListContainer;
+import org.emftext.language.java.statements.util.StatementsSwitch;
+import org.emftext.language.java.types.TypeReference;
+import org.emftext.language.java.variables.LocalVariable;
+import org.splevo.vpm.analyzer.VPMAnalyzerUtil;
 
 import com.google.common.collect.Lists;
 
 /**
  * Switch to decide about which elements to return as referenced elements for a given JaMoPP
  * element.
+ *
+ * Robillar et al. consider references between methods, fields and classes. For methods, they
+ * consider "references defined in the method body", which means statements and expressions.
+ *
+ * According to these rules, this switch collects classes, fields, and methods. For the later, the
+ * contained statements and expressions respectively the elements referenced by them are indexed.
+ * The JaMoPP model explicitly classifies constructors. They are assumed to be treat the same way as
+ * methods. For fields, also the additional fields must be respected.
  */
 public class RobillardReferenceSelectorSwitch extends ComposedSwitch<List<Commentable>> {
 
     /** The logger for this class. */
-    @SuppressWarnings("unused")
     private Logger logger = Logger.getLogger(RobillardReferenceSelectorSwitch.class);
 
     /** Constructor to set up the sub switches. */
     public RobillardReferenceSelectorSwitch() {
+        addSwitch(new ClassifiersReferencedElementsSwitch(this));
         addSwitch(new MembersReferencedElementsSwitch(this));
-        addSwitch(new ImportReferencedElementsSwitch());
+        addSwitch(new StatementsReferencedElementsSwitch(this));
+        addSwitch(new ReferencesReferencedElementsSwitch(this));
+        addSwitch(new InstantiationsReferencedElementsSwitch(this));
+        addSwitch(new ExpressionsReferencedElementsSwitch(this));
     }
 
-    /** Switch to decide about referenced elements for member elements. */
-    private class ImportReferencedElementsSwitch extends ImportsSwitch<List<Commentable>> {
-
-        @Override
-        public List<Commentable> caseImport(Import object) {
-            return Lists.newArrayList((Commentable) object);
+    /**
+     * Wrapper for the super method implementation ensuring that at least an empty list is always
+     * returned.
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Commentable> doSwitch(EObject eObject) {
+        List<Commentable> elements = null;
+        if (!VPMAnalyzerUtil.isNullOrProxy(eObject)) {
+            elements = super.doSwitch(eObject);
         }
 
+        if (elements == null) {
+            elements = Lists.newArrayList();
+        }
+
+        return elements;
+    }
+
+    /**
+     * Switch to decide about referenced elements for member elements.
+     */
+    private class ClassifiersReferencedElementsSwitch extends ClassifiersSwitch<List<Commentable>> {
+
+        private RobillardReferenceSelectorSwitch parentSwitch;
+
+        public ClassifiersReferencedElementsSwitch(RobillardReferenceSelectorSwitch parentSwitch) {
+            this.parentSwitch = parentSwitch;
+        }
+
+        @Override
+        public List<Commentable> caseClass(Class classObject) {
+
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) classObject);
+
+            for (Member member : classObject.getMembers()) {
+                refElements.addAll(parentSwitch.doSwitch(member));
+            }
+
+            TypeReference extendsReference = classObject.getExtends();
+            if (extendsReference != null) {
+                refElements.add(extendsReference.getTarget());
+            }
+
+            return refElements;
+        }
     }
 
     /**
@@ -64,6 +137,18 @@ public class RobillardReferenceSelectorSwitch extends ComposedSwitch<List<Commen
         }
 
         /**
+         * Return the constructor itself and scan the included statements for additional references.
+         */
+        @Override
+        public List<Commentable> caseConstructor(Constructor constructor) {
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) constructor);
+            for (Statement statement : constructor.getStatements()) {
+                refElements.addAll(parentSwitch.doSwitch(statement));
+            }
+            return refElements;
+        }
+
+        /**
          * Return the method itself and scan the included statements for additional references.
          */
         @Override
@@ -72,13 +157,171 @@ public class RobillardReferenceSelectorSwitch extends ComposedSwitch<List<Commen
             for (Statement statement : method.getStatements()) {
                 refElements.addAll(parentSwitch.doSwitch(statement));
             }
-
             return refElements;
         }
 
         @Override
         public List<Commentable> caseMethod(Method object) {
             return Lists.newArrayList((Commentable) object);
+        }
+
+        @Override
+        public List<Commentable> caseField(Field field) {
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) field);
+            refElements.addAll(field.getAdditionalFields());
+            return refElements;
+        }
+    }
+
+    /**
+     * Switch to decide about referenced elements for statement elements.
+     */
+    private class StatementsReferencedElementsSwitch extends StatementsSwitch<List<Commentable>> {
+
+        private RobillardReferenceSelectorSwitch parentSwitch;
+
+        public StatementsReferencedElementsSwitch(RobillardReferenceSelectorSwitch parentSwitch) {
+            this.parentSwitch = parentSwitch;
+        }
+
+        @Override
+        public List<Commentable> caseReturn(Return returnStmt) {
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) returnStmt);
+            refElements.addAll(parentSwitch.doSwitch(returnStmt.getReturnValue()));
+            return refElements;
+        }
+
+        @Override
+        public List<Commentable> caseCondition(Condition condition) {
+            ArrayList<Commentable> refElements = Lists.newArrayList();
+            refElements.addAll(parentSwitch.doSwitch(condition.getCondition()));
+            refElements.addAll(parentSwitch.doSwitch(condition.getElseStatement()));
+            refElements.addAll(parentSwitch.doSwitch(condition.getStatement()));
+            return refElements;
+        }
+
+        @Override
+        public List<Commentable> caseStatementListContainer(StatementListContainer container) {
+            ArrayList<Commentable> refElements = Lists.newArrayList();
+            for (Statement statement : container.getStatements()) {
+                refElements.addAll(parentSwitch.doSwitch(statement));
+            }
+            return refElements;
+        }
+
+        @Override
+        public List<Commentable> caseExpressionStatement(ExpressionStatement stmt) {
+            return parentSwitch.doSwitch(stmt.getExpression());
+        }
+
+        @Override
+        public List<Commentable> caseLocalVariableStatement(LocalVariableStatement lvs) {
+            ArrayList<Commentable> refElements = Lists.newArrayList();
+
+            LocalVariable variable = lvs.getVariable();
+            if (variable != null) {
+                // variables themselves are not considered by robillards algorithm
+                // refElements.add(lvs.getVariable());
+                refElements.addAll(parentSwitch.doSwitch(variable.getTypeReference()));
+                refElements.addAll(parentSwitch.doSwitch(variable.getInitialValue()));
+                // variables themselves are not considered by robillards algorithm
+                // refElements.addAll(variable.getAdditionalLocalVariables());
+            } else {
+                logger.warn("VariableStatement without variable: " + lvs);
+            }
+
+            return refElements;
+        }
+
+
+    }
+
+    /**
+     * Switch to decide about referenced elements for reference elements.
+     */
+    private class ExpressionsReferencedElementsSwitch extends ExpressionsSwitch<List<Commentable>> {
+
+        private RobillardReferenceSelectorSwitch parentSwitch;
+
+        public ExpressionsReferencedElementsSwitch(RobillardReferenceSelectorSwitch parentSwitch) {
+            this.parentSwitch = parentSwitch;
+        }
+
+        @Override
+        public List<Commentable> caseAssignmentExpression(AssignmentExpression exp) {
+            ArrayList<Commentable> refElements = Lists.newArrayList();
+            refElements.addAll(parentSwitch.doSwitch(exp.getChild()));
+            refElements.addAll(parentSwitch.doSwitch(exp.getValue()));
+            return refElements;
+        }
+
+        @Override
+        public List<Commentable> caseCastExpression(CastExpression exp) {
+            ArrayList<Commentable> refElements = Lists.newArrayList();
+            refElements.add(exp.getAlternativeType());
+            return refElements;
+        }
+
+    }
+
+    /**
+     * Switch to decide about referenced elements for reference elements.
+     */
+    private class ReferencesReferencedElementsSwitch extends ReferencesSwitch<List<Commentable>> {
+
+        private RobillardReferenceSelectorSwitch parentSwitch;
+
+        public ReferencesReferencedElementsSwitch(RobillardReferenceSelectorSwitch parentSwitch) {
+            this.parentSwitch = parentSwitch;
+        }
+
+        /**
+         * Register the called method as well as possible content from the arguments, as they might
+         * contain additional identifier references or method calls.
+         */
+        @Override
+        public List<Commentable> caseMethodCall(MethodCall methodCall) {
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) methodCall.getTarget());
+            for (Expression expression : methodCall.getArguments()) {
+                refElements.addAll(parentSwitch.doSwitch(expression));
+            }
+            return refElements;
+        }
+
+        @Override
+        public List<Commentable> caseIdentifierReference(IdentifierReference reference) {
+
+            ReferenceableElement target = reference.getTarget();
+            if (!(target instanceof Field || target instanceof org.emftext.language.java.classifiers.Class || target instanceof Method)) {
+                return Lists.newArrayList();
+            }
+
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) target);
+            if (reference.getNext() != null) {
+                refElements.addAll(parentSwitch.doSwitch(reference.getNext()));
+            }
+            return refElements;
+        }
+    }
+
+    /**
+     * Switch to decide about referenced elements for instantiating elements.
+     */
+    private class InstantiationsReferencedElementsSwitch extends InstantiationsSwitch<List<Commentable>> {
+
+        private RobillardReferenceSelectorSwitch parentSwitch;
+
+        public InstantiationsReferencedElementsSwitch(RobillardReferenceSelectorSwitch parentSwitch) {
+            this.parentSwitch = parentSwitch;
+        }
+
+        @Override
+        public List<Commentable> caseNewConstructorCall(NewConstructorCall call) {
+            ArrayList<Commentable> refElements = Lists.newArrayList((Commentable) call.getType());
+            for (Expression expression : call.getArguments()) {
+                refElements.addAll(parentSwitch.doSwitch(expression));
+            }
+            return refElements;
         }
     }
 }
