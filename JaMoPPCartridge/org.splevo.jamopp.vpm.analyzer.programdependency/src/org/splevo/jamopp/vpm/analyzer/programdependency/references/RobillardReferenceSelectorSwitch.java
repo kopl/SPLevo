1ca7a63c1cae5c464d71db173caa503e9535a361
BenjaminKlatt <klatt@fzi.de>
2014-05-27 07:32:18 +0000

    SPLEVO-301 JaMoPPProgramDependencyAnalyzer: Extended Robillard Mode Import clean up

--- JaMoPPCartridge/org.splevo.jamopp.vpm.analyzer.programdependency/src/org/splevo/jamopp/vpm/analyzer/programdependency/references/RobillardReferenceSelectorSwitch.java
+++ JaMoPPCartridge/org.splevo.jamopp.vpm.analyzer.programdependency/src/org/splevo/jamopp/vpm/analyzer/programdependency/references/RobillardReferenceSelectorSwitch.java
@@ -39,7 +39,6 @@ import org.emftext.language.java.members.AdditionalField;
 import org.emftext.language.java.members.ClassMethod;
 import org.emftext.language.java.members.Constructor;
 import org.emftext.language.java.members.Field;
-import org.emftext.language.java.members.InterfaceMethod;
 import org.emftext.language.java.members.Member;
 import org.emftext.language.java.members.Method;
 import org.emftext.language.java.members.util.MembersSwitch;
