166b68f021a42e0571a081371fd397c8fa2ab428
radoslav-yankov <radoslav.yankov1990@gmail.com>
2014-05-29 20:47:17 +0000

    Added new functionality - the wizard can be finished if the two Variant name fields are filled in.

--- UI/org.splevo.ui.wizard.consolidation/src/org/splevo/ui/wizard/consolidation/NewConsolidationProjectWizard.java
+++ UI/org.splevo.ui.wizard.consolidation/src/org/splevo/ui/wizard/consolidation/NewConsolidationProjectWizard.java
@@ -11,9 +11,12 @@
  *******************************************************************************/
 package org.splevo.ui.wizard.consolidation;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -21,12 +24,21 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+import org.splevo.project.ProjectFactory;
+import org.splevo.project.SPLevoProject;
+import org.splevo.project.SPLevoProjectUtil;
+import org.splevo.ui.editors.SPLevoProjectEditor;
 import org.splevo.ui.nature.SPLevoNature;
 
 /**
@@ -39,6 +51,8 @@ public class NewConsolidationProjectWizard extends Wizard implements INewWizard,
     private static Logger logger = Logger.getLogger(NewConsolidationProjectWizard.class);
 
     private WizardNewProjectCreationPage projectCreationPage;
+    private ProjectsSelectionWizardPage projectsSelectionWizardPage;
+    
     private IConfigurationElement configurationElement;
 
     /**
@@ -56,9 +70,11 @@ public class NewConsolidationProjectWizard extends Wizard implements INewWizard,
         projectCreationPage.setTitle("Consolidation Project");
         projectCreationPage
                 .setDescription("Create a new project for consolidating project copies into a variable product line.");
+        
+        projectsSelectionWizardPage = new ProjectsSelectionWizardPage();
        
         addPage(projectCreationPage);
-        addPage(new ProjectsSelectionWizardPage());
+        addPage(projectsSelectionWizardPage);
     }
 
     @Override
@@ -71,13 +87,34 @@ public class NewConsolidationProjectWizard extends Wizard implements INewWizard,
 
         try {
             IProject project = createBaseProject(name, nonDefaultLocation);
+                     
+            String splevoProjectFileName = project.getName() + "." + SPLevoProjectUtil.SPLEVO_FILE_EXTENSION;
+            File filePath = new File(project.getFullPath().toString() + File.separator + splevoProjectFileName);            
+           
+            SPLevoProject projectConfiguration = ProjectFactory.eINSTANCE.createSPLevoProject();
+            projectConfiguration.setWorkspace("/" + project.getName() + "/");
+            projectConfiguration.setName(project.getName());   
+            projectConfiguration.setVariantNameLeading(projectsSelectionWizardPage.getLeadingProjectsVariantName());
+            projectConfiguration.setVariantNameIntegration(projectsSelectionWizardPage.getIntegrationProjectsVariantName());
+            
+            try {
+                SPLevoProjectUtil.save(projectConfiguration, filePath);
+                project.refreshLocal(-1, new NullProgressMonitor());
+                IFile file = project.getFile(splevoProjectFileName);
+                IFileEditorInput inputFile = new FileEditorInput(file);
+                IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+                page.openEditor(inputFile, SPLevoProjectEditor.ID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
             addNature(project);
         } catch (CoreException e) {
             logger.error("Failed to create consolidation project", e);
         }
 
         BasicNewProjectResourceWizard.updatePerspective(configurationElement);
-        return true;
+        return projectCreationPage.isPageComplete();
     }
 
     @Override
