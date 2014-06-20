f69a0cb45bd54746d20bda59b650528594f0704b
Daniel Kojic <daniel.kojic@gmx.de>
2014-06-19 17:41:02 -0700

    ConflictingFiles

--- JaMoPPCartridge/org.splevo.jamopp.refactoring.java.ifelse.tests/testcode/Imports/integration/TestClass.java
+++
@@ -1,9 +0,0 @@
-package org.splevo.refactoring.tests.testcases;
-
-import java.util.LinkedList;
-
-public class TestClass {
-
-    protected void someMethod() {
-    }
-}
--- JaMoPPCartridge/org.splevo.jamopp.refactoring.java.ifelse.tests/testcode/Statement_EntireMethod/leading/TestClass.java
+++
@@ -1,7 +0,0 @@
-package org.splevo.refactoring.tests.testcases;
-
-public class TestClass {
-
-    protected void someMethod() {
-    }
-}
