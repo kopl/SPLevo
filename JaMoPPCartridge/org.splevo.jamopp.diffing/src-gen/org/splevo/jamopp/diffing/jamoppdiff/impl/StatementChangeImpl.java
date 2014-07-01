eed8c6ad3ea520111aeaec917fc24d7fdde4a344
BenjaminKlatt <klatt@fzi.de>
2014-07-01 19:01:59 +0200

    Fix Stackoverflow in standlone installation

--- JaMoPPCartridge/org.splevo.jamopp.diffing/src-gen/org/splevo/jamopp/diffing/jamoppdiff/impl/StatementChangeImpl.java
+++ JaMoPPCartridge/org.splevo.jamopp.diffing/src-gen/org/splevo/jamopp/diffing/jamoppdiff/impl/StatementChangeImpl.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Benjamin Klatt - initial API and implementation and/or initial documentation
  */
@@ -158,6 +158,9 @@ public class StatementChangeImpl extends JaMoPPDiffImpl implements StatementChan
         switch (featureID) {
         case JaMoPPDiffPackage.STATEMENT_CHANGE__CHANGED_STATEMENT:
             return changedStatement != null;
+        }
+        if(featureID == 13) {
+            System.out.println(JaMoPPDiffPackage.STATEMENT_CHANGE__CHANGED_STATEMENT);
         }
         return super.eIsSet(featureID);
     }
