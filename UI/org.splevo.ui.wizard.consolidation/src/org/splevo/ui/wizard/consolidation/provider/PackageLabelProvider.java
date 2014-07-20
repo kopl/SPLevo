fdaca31a81a88563371c7685d5bb96a67fa35432
radoslav-yankov <radoslav.yankov1990@gmail.com>
2014-07-20 12:52:56 +0200

    SPLEVO-308 Consolidation Wizard: Package Scope Definition Page: Label provider for packages.

---
+++ UI/org.splevo.ui.wizard.consolidation/src/org/splevo/ui/wizard/consolidation/provider/PackageLabelProvider.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt
+ *******************************************************************************/
+package org.splevo.ui.wizard.consolidation.provider;
+
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.ui.ISharedImages;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.swt.graphics.Image;
+
+/**
+ * Label provider for packages
+ * 
+ * @author Radoslav Yankov 
+ */
+public class PackageLabelProvider extends ColumnLabelProvider {
+    @Override
+    public String getText(Object element) {
+        if (element instanceof IPackageFragment) {
+            return ((IPackageFragment) element).getElementName();
+        }
+        return null;
+    }
+
+    @Override
+    public Image getImage(Object element) {
+        return JavaUI.getSharedImages().getImage(ISharedImages.IMG_OBJS_PACKAGE);
+    }
+}
