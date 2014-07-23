681b114fb7c4a21aab2ed9903e6555793d7d8891
BenjaminKlatt <klatt@fzi.de>
2014-07-23 22:12:27 +0200

    Unit test to represent enumeration enumconstant connection and eContainer setting

---
+++ JaMoPPCartridge/org.splevo.jamopp.refactoring.java.ifelse.tests/src/org/splevo/jamopp/refactoring/java/ifelse/optor/tests/RefactoringTest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2014
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Klatt
+ *******************************************************************************/
+package org.splevo.jamopp.refactoring.java.ifelse.optor.tests;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
+import org.emftext.language.java.classifiers.ClassifiersFactory;
+import org.emftext.language.java.classifiers.Enumeration;
+import org.emftext.language.java.members.EnumConstant;
+import org.emftext.language.java.members.MembersFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class RefactoringTest {
+
+    /**
+     * Prepare the test. Initializes a log4j logging environment.
+     */
+    @BeforeClass
+    public static void setUp() {
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
+    }
+
+    @Test
+    public void testConnectEnumeration() {
+
+        Enumeration myEnumeration = ClassifiersFactory.eINSTANCE.createEnumeration();
+        EnumConstant constant = MembersFactory.eINSTANCE.createEnumConstant();
+        myEnumeration.getConstants().add(constant);
+
+        assertThat(constant.eContainer(), notNullValue());
+    }
+}
