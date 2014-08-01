e67806a97f64a77c5f1681b04405c708603a3c1d
radoslav-yankov <radoslav.yankov1990@gmail.com>
2014-08-01 22:31:05 +0200

    SPLEVO-308 Consolidation Wizard: Package Scope Definition Page: added javadoc.

--- UI/org.splevo.ui.wizard.consolidation/src/org/splevo/ui/wizard/consolidation/provider/PackagesTreeContentProvider.java
+++ UI/org.splevo.ui.wizard.consolidation/src/org/splevo/ui/wizard/consolidation/provider/PackagesTreeContentProvider.java
@@ -21,7 +21,7 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 /**
- * Content provider for the packages tree viewer.
+ * Tree content provider which returns java packages ordered by name (ascending).
  * 
  * @author Radoslav Yankov
  */
@@ -36,7 +36,7 @@ public class PackagesTreeContentProvider implements ITreeContentProvider {
      *            All java packages which are in the class path of the chosen projects.
      */
     public void setJavaPackages(SortedSet<IPackageFragment> javaPackages) {
-        this.javaPackages = javaPackages;
+        this.javaPackages = javaPackages;        
     }
 
     @Override
@@ -47,6 +47,13 @@ public class PackagesTreeContentProvider implements ITreeContentProvider {
     public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
     }
 
+    /**
+     * Return the root packages of the hierarchy.
+     * 
+     * @param inputElement
+     *            An array which contains all root packages.
+     * @return All root packages.
+     */
     @Override
     public Object[] getElements(Object inputElement) {
         if (inputElement instanceof IPackageFragment[]) {
@@ -55,6 +62,13 @@ public class PackagesTreeContentProvider implements ITreeContentProvider {
         return new Object[0];
     }
 
+    /**
+     * Return all direct sub packages of the given package.
+     * 
+     * @param parentElement
+     *            The package whose sub packages have to be found.
+     * @return Array with all sub packages of the given package.
+     */
     @Override
     public Object[] getChildren(Object parentElement) {
         if (parentElement instanceof IPackageFragment) {
@@ -68,6 +82,13 @@ public class PackagesTreeContentProvider implements ITreeContentProvider {
         return new Object[0];
     }
 
+    /**
+     * Get the parent package of the given package.
+     * 
+     * @param element
+     *            The package whose parent is to be found.
+     * @return The parent package of the given package.
+     */
     @Override
     public Object getParent(Object element) {
         if (element instanceof IPackageFragment) {
@@ -80,6 +101,13 @@ public class PackagesTreeContentProvider implements ITreeContentProvider {
         return new Object[0];
     }
 
+    /**
+     * Check if the given package has at least one sub package.
+     * 
+     * @param element
+     *            The package which has to be checked
+     * @return true if the given package has at least one sub package, otherwise false.
+     */
     @Override
     public boolean hasChildren(Object element) {
         if (element instanceof IPackageFragment) {
@@ -100,7 +128,7 @@ public class PackagesTreeContentProvider implements ITreeContentProvider {
     private IPackageFragment[] getSubPackages(IPackageFragment parentPackage) {
         List<IPackageFragment> subPackages = new ArrayList<IPackageFragment>();
 
-        for (IPackageFragment javaPackage : javaPackages) {
+        for (IPackageFragment javaPackage : javaPackages) {            
             if (javaPackage.getElementName().matches(parentPackage.getElementName() + "\\.\\w+")) {
                 subPackages.add(javaPackage);
             }
