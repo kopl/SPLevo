b6aef98ec3e33a3ac5d00a9a345093a87eeed444
radoslav-yankov <radoslav.yankov1990@gmail.com>
2014-08-02 14:14:47 +0200

    SPLEVO-385 Consolidation Wizard: Unit tests: added tests for all methods

--- UI/org.splevo.ui.wizard.consolidation.tests/src/org/splevo/ui/wizard/consolidation/tests/PackagesTreeContentProviderTest.java
+++ UI/org.splevo.ui.wizard.consolidation.tests/src/org/splevo/ui/wizard/consolidation/tests/PackagesTreeContentProviderTest.java
@@ -17,15 +17,13 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Iterator;
 import java.util.SortedSet;
-import java.util.TreeSet;
 
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.junit.Test;
 import org.splevo.ui.wizard.consolidation.provider.PackagesTreeContentProvider;
-import org.splevo.ui.wizard.consolidation.util.PackagesComparator;
-
 
 /**
  * Unit test for the functions of the {@link PackagesTreeContentProvider}.
@@ -33,52 +31,113 @@ import org.splevo.ui.wizard.consolidation.util.PackagesComparator;
  * @author Radoslav Yankov
  */
 public class PackagesTreeContentProviderTest {
-    
+
     /**
      * Test if the method getElements() returns a correct result.
      */
     @Test
     public void testGetElements() {
         ITreeContentProvider packagesTreeContentProvider = new PackagesTreeContentProvider();
-        
+
         IPackageFragment javaPackage1 = mock(IPackageFragment.class);
         IPackageFragment javaPackage2 = mock(IPackageFragment.class);
-        
-        IPackageFragment[] rootPackages = {javaPackage1, javaPackage2};
-        
+
+        IPackageFragment[] rootPackages = { javaPackage1, javaPackage2 };
+
         Object[] result = packagesTreeContentProvider.getElements(rootPackages);
-                                
+
         assertThat("returned root packages", (IPackageFragment[]) result, is(rootPackages));
     }
-    
+
     /**
      * Test if the method getChildren() returns a correct result.
      */
+    @SuppressWarnings("unchecked")
     @Test
     public void testGetChildren() {
         PackagesTreeContentProvider packagesTreeContentProvider = new PackagesTreeContentProvider();
-        
-        IPackageFragment parentPackage = mock(IPackageFragment.class);        
+
+        IPackageFragment parentPackage = mock(IPackageFragment.class);
         when(parentPackage.getElementName()).thenReturn("org.splevo");
-        
+
         IPackageFragment javaPackage1 = mock(IPackageFragment.class);
         when(javaPackage1.getElementName()).thenReturn("org.splevo.wizard");
-        
+
         IPackageFragment javaPackage2 = mock(IPackageFragment.class);
         when(javaPackage2.getElementName()).thenReturn("org.splevo.test");
-        
-//        PackagesComparator packagesComparator = mock(PackagesComparator.class);
-//        when(packagesComparator.compare(javaPackage1, javaPackage2)).thenReturn(2);
-                       
-        SortedSet<IPackageFragment> javaPackages = new TreeSet<IPackageFragment>(new PackagesComparator());   
-        javaPackages.add(javaPackage1);
-        javaPackages.add(javaPackage2);
-                    
+
+        SortedSet<IPackageFragment> javaPackages = mock(SortedSet.class);
+        Iterator<IPackageFragment> iterator = mock(Iterator.class);
+
+        when(javaPackages.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(iterator.next()).thenReturn(parentPackage).thenReturn(javaPackage1).thenReturn(javaPackage2);
+
         packagesTreeContentProvider.setJavaPackages(javaPackages);
-                        
-        Object[] result = packagesTreeContentProvider.getChildren(parentPackage);        
-        IPackageFragment[] subPackages = {javaPackage2, javaPackage1};                
-                       
+
+        Object[] result = packagesTreeContentProvider.getChildren(parentPackage);
+        IPackageFragment[] subPackages = { javaPackage1, javaPackage2 };
+
         assertThat("returned sub packages", (IPackageFragment[]) result, is(subPackages));
-    }     
+    }
+
+    /**
+     * Test if the method getParent() returns a correct result.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testGetParent() {
+        PackagesTreeContentProvider packagesTreeContentProvider = new PackagesTreeContentProvider();
+
+        IPackageFragment childPackage = mock(IPackageFragment.class);
+        when(childPackage.getElementName()).thenReturn("org.splevo.wizard");
+
+        IPackageFragment parentPackage = mock(IPackageFragment.class);
+        when(parentPackage.getElementName()).thenReturn("org.splevo");
+
+        IPackageFragment anotherPackage = mock(IPackageFragment.class);
+        when(anotherPackage.getElementName()).thenReturn("org.splevo.ui");
+
+        SortedSet<IPackageFragment> javaPackages = mock(SortedSet.class);
+        Iterator<IPackageFragment> iterator = mock(Iterator.class);
+
+        when(javaPackages.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(iterator.next()).thenReturn(childPackage).thenReturn(parentPackage).thenReturn(anotherPackage);
+
+        packagesTreeContentProvider.setJavaPackages(javaPackages);
+
+        assertThat("returned parent package", (IPackageFragment) packagesTreeContentProvider.getParent(childPackage),
+                is(parentPackage));
+    }
+
+    /**
+     * Test if the method hasChildren() returns a correct result.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testHasChildren() {
+        PackagesTreeContentProvider packagesTreeContentProvider = new PackagesTreeContentProvider();
+
+        IPackageFragment javaPackage1 = mock(IPackageFragment.class);
+        when(javaPackage1.getElementName()).thenReturn("org.splevo");
+
+        IPackageFragment javaPackage2 = mock(IPackageFragment.class);
+        when(javaPackage2.getElementName()).thenReturn("org.splevo.wizard");
+
+        IPackageFragment javaPackage3 = mock(IPackageFragment.class);
+        when(javaPackage3.getElementName()).thenReturn("org.splevo.test");
+
+        SortedSet<IPackageFragment> javaPackages = mock(SortedSet.class);
+        Iterator<IPackageFragment> iterator = mock(Iterator.class);
+
+        when(javaPackages.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(iterator.next()).thenReturn(javaPackage1).thenReturn(javaPackage2).thenReturn(javaPackage3);
+
+        packagesTreeContentProvider.setJavaPackages(javaPackages);
+
+        assertThat("returned result", packagesTreeContentProvider.hasChildren(javaPackage1), is(true));
+        assertThat("returned result", packagesTreeContentProvider.hasChildren(javaPackage2), is(false));
+    }
 }
