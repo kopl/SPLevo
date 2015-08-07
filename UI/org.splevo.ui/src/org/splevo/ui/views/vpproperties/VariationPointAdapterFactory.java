bf2dd8b81e3985a2266384046dad5c091d2998e6
thomcz <thomcz@users.noreply.github.com>
2015-08-07 14:24:26 +0200

    Splevo-349 Added a properties view to show and edit selected VP Groups properties

--- UI/org.splevo.ui/src/org/splevo/ui/views/vpproperties/VariationPointAdapterFactory.java
+++ UI/org.splevo.ui/src/org/splevo/ui/views/vpproperties/VariationPointAdapterFactory.java
@@ -7,13 +7,14 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Benjamin Klatt
+ *    Benjamin Klatt, Thomas Czogalik
  *******************************************************************************/
 package org.splevo.ui.views.vpproperties;
 
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.splevo.vpm.variability.VariationPoint;
+import org.splevo.vpm.variability.VariationPointGroup;
 
 /**
  * Factory to create adapter for content access.
@@ -23,8 +24,12 @@ public class VariationPointAdapterFactory implements IAdapterFactory {
     @Override
     @SuppressWarnings("rawtypes")
     public Object getAdapter(Object adaptableObject, Class adapterType) {
-        if (adapterType == IPropertySource.class && adaptableObject instanceof VariationPoint) {
-            return new VariationPointPropertySource((VariationPoint) adaptableObject);
+        if (adapterType == IPropertySource.class) {
+            if (adaptableObject instanceof VariationPoint) {
+                return new VariationPointPropertySource((VariationPoint) adaptableObject);
+            } else if (adaptableObject instanceof VariationPointGroup) {
+                return new VariationPointGroupPropertySource((VariationPointGroup) adaptableObject);
+            }
         }
         return null;
     }
