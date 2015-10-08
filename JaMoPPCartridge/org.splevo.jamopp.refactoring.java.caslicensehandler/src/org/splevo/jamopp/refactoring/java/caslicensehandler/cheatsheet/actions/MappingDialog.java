604bfe96a4009c7247c2d3cc3ebad32954891094
thomcz <thomcz@users.noreply.github.com>
2015-10-08 15:43:37 +0200

    Splevo-464 improved the design of the license constant mapping dialog of the CASLicenseHandler cheat sheet

--- JaMoPPCartridge/org.splevo.jamopp.refactoring.java.caslicensehandler/src/org/splevo/jamopp/refactoring/java/caslicensehandler/cheatsheet/actions/MappingDialog.java
+++ JaMoPPCartridge/org.splevo.jamopp.refactoring.java.caslicensehandler/src/org/splevo/jamopp/refactoring/java/caslicensehandler/cheatsheet/actions/MappingDialog.java
@@ -1,147 +1,194 @@
 package org.splevo.jamopp.refactoring.java.caslicensehandler.cheatsheet.actions;
 
-import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnViewer;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ComboBoxViewerCellEditor;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.PlatformUI;
 import org.splevo.vpm.variability.Variant;
 
-import com.google.common.base.Strings;
 
 /**
  * The user can assign a variant to a given license with the mapping dialog.
  */
 public class MappingDialog extends TitleAreaDialog {
-	
-  private Iterator<Variant> variants = null;
-  private Variant currentVariant = null;
-  private Text variantTextField = null;
-  private Combo combo = null;
-  private CASLicenseHandlerConfiguration config = null;
-	
-  /**
-	 * The constructor.
-	 * @param parentShell
-	 * 				represents the parent shell.
-	 */
-  public MappingDialog(Shell parentShell) {
-    super(parentShell);
-    this.config = CASLicenseHandlerConfiguration.getInstance();
-    this.variants = config.getVariationPoint().getVariants().iterator();
-    this.currentVariant = this.variants.next();
-  }
 
-  @Override
-  protected Control createDialogArea(Composite parent) {
-	Composite container = (Composite) super.createDialogArea(parent);
-	
-    initButtons(container);
-    initComboBox(container);
-    initLabel(container);
-    initTextField(container);
-    this.setTitle("MappingDialog");
+    private List<Variant> variants;
+    private CASLicenseHandlerConfiguration config;
+    private ComboBoxViewerCellEditor cellEditor;
+
+    /**
+     * The constructor.
+     * 
+     * @param parentShell
+     *            represents the parent shell.
+     */
+    public MappingDialog(Shell parentShell) {
+        super(parentShell);
+        this.config = CASLicenseHandlerConfiguration.getInstance();
+        this.variants = config.getVariationPoint().getVariants();
+    }
     
-    return container;
-  }
-  
-  private void initButtons(Composite container) {
-	  Button nextButton = new Button(container, SWT.PUSH);
-	    nextButton.setLayoutData(new GridData(SWT.END, SWT.RIGHT, false,
-	        false));
-	    nextButton.setText("Next");
-	    nextButton.addSelectionListener(new SelectionAdapter() {
-	      @Override
-	      public void widgetSelected(SelectionEvent e) {	  
-	    	  String license = combo.getText();
-	    		  
-	    	  if (Strings.isNullOrEmpty(license)) {
-	    		  MessageDialog.openInformation(new Shell(), "Information", "No license was specified");
-	    		  return;
-	    	  }
-	    		  
-	    	  if (!updateVariantToLicenseMapper(license)) {
-	    		  MessageDialog.openInformation(new Shell(), "Information", "The license is already assigned to a variant. Please choose a different.");
-	    		  return;
-	    	  }
-	    		  
-	    	  prepareNextAssignment();
-	      }
-	    });
-	    
-	    Button addLicenseButton = new Button(container, SWT.PUSH);
-	    addLicenseButton.setLayoutData(new GridData(SWT.END, SWT.RIGHT, false,
-	        false));
-	    addLicenseButton.setText("Add");
-	    addLicenseButton.addSelectionListener(new SelectionAdapter() {
-	      @Override
-	      public void widgetSelected(SelectionEvent e) {
-	    	  AddLicenseDialog dialog = new AddLicenseDialog(
-	    			  PlatformUI.getWorkbench().getDisplay().getActiveShell(), 
-	    			  currentVariant.getId());
-	    	  
-	    	  if (Window.OK == dialog.open()) {
-	    	  	  combo.setItems(config.getAllLicenses());
-	    	  	  combo.select(0);
-	    	  	  
-	    	  	  prepareNextAssignment();
-	    	  }
-	      }
-	    });
-  }
-  
-  private void initComboBox(Composite container) {
-	  combo = new Combo(container, SWT.READ_ONLY);
-	  combo.setBounds(50, 50, 150, 65);
-	  combo.setItems(config.getAllLicenses());
-	  combo.select(0);
-  }
-  
-  private void initLabel(Composite container) {
-	  Label label = new Label(container, SWT.NULL);
-	  label.setText("Variant:");
-  }
-  
-  private void initTextField(Composite container) {
-	  variantTextField = new Text(container, SWT.SINGLE | SWT.BORDER);
-	  variantTextField.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
-	  updateTextField();
-  }
-  
-  private void updateTextField() {
-	  variantTextField.setText(this.currentVariant.getId());
-  }
-  
-  private boolean updateVariantToLicenseMapper(String license) {
-	  return config.addVariantLicensePair(currentVariant.getId(), license);
-  }
-  
-  private void prepareNextAssignment() {
-	  if (this.variants.hasNext()) {
-		  this.currentVariant = this.variants.next();
-		  updateTextField();
-	  } else {
-		  MessageDialog.openInformation(this.getShell(), "Information", "All Variants are assigned");
-		  close();
-	  }
-  }
- 
-  @Override
-  protected Point getInitialSize() {
-    return new Point(450, 300);
-  }
+    @Override
+    public void create() {
+        super.create();
+        setTitle("Mapping Dialog");
+        setMessage("Assign a variant to a given license");
+    }
 
-} 
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite container = (Composite) super.createDialogArea(parent);
+        
+        GridLayout gridLayout = new GridLayout(1, false);
+        container.setLayout(gridLayout);
+        
+        TableLayout tableLayout = new TableLayout();
+        tableLayout.addColumnData(new ColumnWeightData(1));
+        tableLayout.addColumnData(new ColumnWeightData(1));
+        
+        Table table = new Table(container, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
+        table.setLinesVisible(true);
+        table.setHeaderVisible(true);
+        table.setLayout(tableLayout);
+        
+        TableViewer tableViewer = new TableViewer(table);
+        tableViewer.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        
+        TableViewerColumn labelColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+        labelColumn.getColumn().setText("Variant");
+        
+        TableViewerColumn valueColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+        valueColumn.getColumn().setText("License Constant");
+
+        EditingSupport exampleEditingSupport = new TableEditingSupport(valueColumn.getViewer());
+        valueColumn.setEditingSupport(exampleEditingSupport);
+
+        tableViewer.setContentProvider(new ArrayContentProvider());
+        tableViewer.setLabelProvider(new VariantTableLabelProvider());
+        tableViewer.setInput(variants);
+        
+        Button newLicense = new Button(container, SWT.PUSH);
+        newLicense.setText("New License Constant");
+        newLicense.addMouseListener(new MouseListener() {
+            
+            @Override
+            public void mouseUp(MouseEvent e) {
+                AddLicenseDialog dialog = new AddLicenseDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell());
+                if (dialog.open() == Window.OK) {
+                    cellEditor.setInput(config.getAllLicenses());
+                }
+            }
+            
+            @Override
+            public void mouseDown(MouseEvent e) { }
+            
+            @Override
+            public void mouseDoubleClick(MouseEvent e) { }
+        });
+
+        return container;
+    }
+
+    /**
+     * Provides table with labels for each column.
+     */
+    private final class VariantTableLabelProvider extends LabelProvider implements ITableLabelProvider {
+
+        @Override
+        public Image getColumnImage(Object element, int columnIndex) {
+            return null;
+        }
+
+        @Override
+        public String getColumnText(Object element, int columnIndex) {
+            if (!(element instanceof Variant)) {
+                return "";
+            }
+            Variant data = (Variant) element;
+            switch (columnIndex) {
+            case 0:
+                return data.getId();
+            case 1:
+                return config.getVariantToLicenseMap().get(data.getId());
+            default:
+                return "";
+            }
+        }
+    }
+
+    /**
+     * Supports the table with cell editing.
+     */
+    private final class TableEditingSupport extends EditingSupport {       
+
+        @SuppressWarnings("deprecation")
+        private TableEditingSupport(ColumnViewer viewer) {
+            super(viewer);
+            cellEditor = new ComboBoxViewerCellEditor((Composite) getViewer().getControl(), SWT.NONE);
+            cellEditor.setLabelProvider(new LabelProvider());
+            cellEditor.setContenProvider(new ArrayContentProvider());    
+            cellEditor.setInput(config.getAllLicenses());       
+        }
+
+        @Override
+        protected CellEditor getCellEditor(Object element) {
+            return cellEditor;
+        }
+
+        @Override
+        protected boolean canEdit(Object element) {
+            return true;
+        }
+
+        @Override
+        protected Object getValue(Object element) {
+            if (!(element instanceof Variant)) {
+                return null;
+            }            
+            return config.getVariantToLicenseMap().get(((Variant) element).getId());
+        }
+
+        @Override
+        protected void setValue(Object element, Object value) {
+            if (element instanceof Variant && value instanceof String) {
+                Variant data = (Variant) element;
+                String license = (String) value;                
+                if (!config.addVariantLicensePair(data.getId(), license)) {
+                    MessageDialog.openInformation(PlatformUI.getWorkbench().getDisplay().getActiveShell(), 
+                            "Information", "Every variant needs a different License Constant.");                   
+                }
+                getViewer().update(element, null);
+            }
+        }
+    }    
+    
+    @Override 
+    protected Point getInitialSize() { 
+        return new Point(450, 300); 
+    }
+}
